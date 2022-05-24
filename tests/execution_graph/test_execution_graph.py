96c3e6211e9d5039517515b8d2e1c1a72d1232a0
senbong <senbong@featurebyte.com>
2022-05-24 09:46:03 +0800

    Revise tests

--- tests/execution_graph/test_execution_graph.py
+++ tests/execution_graph/test_execution_graph.py
@@ -1,28 +1,31 @@
+"""
+Unit test for execution graph
+"""
 import pytest
 
-from featurebyte.execution_graph.graph import ExecutionGraph
+from featurebyte.execution_graph.graph import ExecutionGraph, Node
 
 
-class ResettableExecutionGraph(ExecutionGraph):
-    @classmethod
-    def clear_instances(cls):
-        del cls.__class__._instances
-
-
-@pytest.fixture()
+@pytest.fixture(scope="module")
 def execution_graph():
-    yield ResettableExecutionGraph()
-    ResettableExecutionGraph.clear_instances()
+    yield ExecutionGraph()
 
 
-def test_add_operation__duplicated_node(execution_graph):
+@pytest.fixture(scope="module")
+def execution_graph_single_node(execution_graph):
     node_input = execution_graph.add_operation(node_type="input", node_params={}, input_nodes=[])
     assert execution_graph.to_dict() == {
         "nodes": {"input_1": {"type": "input", "parameters": {}}},
         "edges": {},
     }
+    assert node_input == Node(id="input_1", type="input", parameters={})
+    yield execution_graph, node_input
 
-    node_proj_a_first = execution_graph.add_operation(
+
+@pytest.fixture(scope="module")
+def execution_graph_two_nodes(execution_graph_single_node):
+    execution_graph, node_input = execution_graph_single_node
+    node_proj = execution_graph.add_operation(
         node_type="project", node_params={"columns": ["a"]}, input_nodes=[node_input]
     )
     expected_graph = {
@@ -33,9 +36,86 @@ def test_add_operation__duplicated_node(execution_graph):
         "edges": {"input_1": ["project_1"]},
     }
     assert execution_graph.to_dict() == expected_graph
+    yield execution_graph, node_input, node_proj
 
-    node_proj_a_second = execution_graph.add_operation(
+
+@pytest.fixture(scope="module")
+def execution_graph_three_nodes(execution_graph_two_nodes):
+    execution_graph, node_input, node_proj = execution_graph_two_nodes
+    node_eq = execution_graph.add_operation(
+        node_type="equal", node_params={"value": 1}, input_nodes=[node_proj]
+    )
+    expected_graph = {
+        "nodes": {
+            "input_1": {"type": "input", "parameters": {}},
+            "project_1": {"type": "project", "parameters": {"columns": ["a"]}},
+            "equal_1": {"type": "equal", "parameters": {"value": 1}},
+        },
+        "edges": {
+            "input_1": ["project_1"],
+            "project_1": ["equal_1"],
+        },
+    }
+    assert execution_graph.to_dict() == expected_graph
+    yield execution_graph, node_input, node_proj, node_eq
+
+
+@pytest.fixture(scope="module")
+def execution_graph_four_nodes(execution_graph_three_nodes):
+    execution_graph, node_input, node_proj, node_eq = execution_graph_three_nodes
+    node_filter = execution_graph.add_operation(
+        node_type="filter", node_params={}, input_nodes=[node_input, node_eq]
+    )
+    expected_graph = {
+        "nodes": {
+            "input_1": {"type": "input", "parameters": {}},
+            "project_1": {"type": "project", "parameters": {"columns": ["a"]}},
+            "equal_1": {"type": "equal", "parameters": {"value": 1}},
+            "filter_1": {"type": "filter", "parameters": {}},
+        },
+        "edges": {
+            "input_1": ["project_1", "filter_1"],
+            "project_1": ["equal_1"],
+            "equal_1": ["filter_1"],
+        },
+    }
+    assert execution_graph.to_dict() == expected_graph
+    yield execution_graph, node_input, node_proj, node_eq, node_filter
+
+
+def test_add_operation__add_duplicated_node_on_two_nodes_graph(execution_graph_two_nodes):
+    execution_graph, node_input, node_proj = execution_graph_two_nodes
+    node_duplicated = execution_graph.add_operation(
         node_type="project", node_params={"columns": ["a"]}, input_nodes=[node_input]
     )
+    expected_graph = {
+        "nodes": {
+            "input_1": {"type": "input", "parameters": {}},
+            "project_1": {"type": "project", "parameters": {"columns": ["a"]}},
+        },
+        "edges": {"input_1": ["project_1"]},
+    }
     assert execution_graph.to_dict() == expected_graph
-    assert node_proj_a_first == node_proj_a_second
+    assert node_duplicated == node_proj
+
+
+def test_add_operation__add_duplicated_node_on_four_nodes_graph(execution_graph_four_nodes):
+    execution_graph, _, node_proj, node_eq, _ = execution_graph_four_nodes
+    node_duplicated = execution_graph.add_operation(
+        node_type="equal", node_params={"value": 1}, input_nodes=[node_proj]
+    )
+    expected_graph = {
+        "nodes": {
+            "input_1": {"type": "input", "parameters": {}},
+            "project_1": {"type": "project", "parameters": {"columns": ["a"]}},
+            "equal_1": {"type": "equal", "parameters": {"value": 1}},
+            "filter_1": {"type": "filter", "parameters": {}},
+        },
+        "edges": {
+            "input_1": ["project_1", "filter_1"],
+            "project_1": ["equal_1"],
+            "equal_1": ["filter_1"],
+        },
+    }
+    assert execution_graph.to_dict() == expected_graph
+    assert node_duplicated == node_eq
