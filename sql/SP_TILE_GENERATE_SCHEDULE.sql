70fe4a5e898e8e00c1657f8839d660758815bed2
yanhuisg <yanhui@featurebyte.com>
2022-05-26 17:06:02 +0800

    stored procedure to generate monitoring records

--- sql/SP_TILE_GENERATE_SCHEDULE.sql
+++ sql/SP_TILE_GENERATE_SCHEDULE.sql
@@ -1,11 +1,11 @@
-create or replace procedure SP_TILE_GENERATE_SCHEDULE(FEATURE_NAME varchar, WINDOW_END_MINUTE float, BLINDSPOT_SECONDS float, FREQUENCY_MINUTE float, SQL varchar, COLUMN_NAMES varchar, TYPE varchar)
+create or replace procedure SP_TILE_GENERATE_SCHEDULE(FEATURE_NAME varchar, WINDOW_END_MINUTE float, BLINDSPOT_RESIDUE_SECONDS float, FREQUENCY_MINUTE float, SQL varchar, COLUMN_NAMES varchar, TYPE varchar, MONITOR_PERIODS float)
 returns string
 language javascript
 as
 $$
-    var debug = "Debug";
+    var debug = "Debug"
 
-    sleep_seconds = BLINDSPOT_SECONDS % 60
+    sleep_seconds = BLINDSPOT_RESIDUE_SECONDS % 60
     snowflake.execute(
         {
             sqlText: `call system$wait(${sleep_seconds})`
@@ -17,6 +17,7 @@ $$
     var frequency_min = FREQUENCY_MINUTE
     
     var end_ts = new Date()
+    //var end_ts = new Date(Date.parse('2022-05-26 00:28:00 UTC'))
     if (tile_type === "OFFLINE") {
         // offline schedule 
         frequency_min = 1440
@@ -25,6 +26,9 @@ $$
     
     var start_ts = new Date(end_ts.getTime())
     start_ts.setMinutes(start_ts.getMinutes() - frequency_min)
+
+    var monitor_start_ts = new Date(end_ts.getTime())
+    monitor_start_ts.setMinutes(monitor_start_ts.getMinutes() - frequency_min * MONITOR_PERIODS)
     
     end_ts_str = end_ts.toISOString().split("T")
     end_ts_str = end_ts_str[0] + " " + end_ts_str[1].slice(0,5)+":00"
@@ -34,16 +38,30 @@ $$
     start_ts_str = start_ts_str[0] + " " + start_ts_str[1].slice(0,5)+":00"
     debug = debug + " - start_ts_str: " + start_ts_str
 
-    var table_name = FEATURE_NAME.toUpperCase() + "_TILE" + "_" + tile_type
-    var tile_create_sql = `call SP_TILE_GENERATE('${SQL}', ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}, '${COLUMN_NAMES}', '${start_ts_str}', '${end_ts_str}', '${table_name}')`
-    
+    monitor_start_ts_str = monitor_start_ts.toISOString().split("T")
+    monitor_start_ts_str = monitor_start_ts_str[0] + " " + monitor_start_ts_str[1].slice(0,5)+":00"
+    debug = debug + " - monitor_start_ts_str: " + monitor_start_ts_str
+
+    var table_name = FEATURE_NAME.toUpperCase() + "_TILE_" + tile_type
+    var input_sql = SQL.replace("FB_START_TS", "\\'"+start_ts_str+"\\'").replace("FB_END_TS", "\\'"+end_ts_str+"\\'")
+    var generate_stored_proc = `call SP_TILE_GENERATE('${input_sql}', ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}, '${COLUMN_NAMES}', '${table_name}')`
     var result = snowflake.execute(
         {
-            sqlText: tile_create_sql
+            sqlText: generate_stored_proc
         }
     )
     result.next()
-    debug = debug + " - SP_TILE_GENERATE: " + result.getColumnValue(1) 
+    debug = debug + " - SP_TILE_GENERATE: " + result.getColumnValue(1)
+    
+    var monitor_input_sql = SQL.replace("FB_START_TS", "\\'"+monitor_start_ts_str+"\\'").replace("FB_END_TS", "\\'"+end_ts_str+"\\'")
+    var monitor_stored_proc = `call SP_TILE_MONITOR('${monitor_input_sql}', ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}, '${COLUMN_NAMES}', '${table_name}')`
+    result = snowflake.execute(
+        {
+            sqlText: monitor_stored_proc
+        }
+    )
+    result.next()
+    debug = debug + " - SP_TILE_MONITOR: " + result.getColumnValue(1)
 
     return debug
 $$;
---
+++ sql/SP_TILE_MONITOR.sql
@@ -0,0 +1,63 @@
+create or replace procedure SP_TILE_MONITOR(MONITOR_SQL varchar, WINDOW_END_MINUTE float, FREQUENCY_MINUTE float, COLUMN_NAMES varchar, TABLE_NAME varchar)
+returns string
+language javascript
+as
+$$
+    var debug = "Debug"
+
+    var col_list = COLUMN_NAMES.split(",").filter(item => item.toUpperCase() !== "TILE_START_TS")
+    col_list_str = col_list.join(',')
+    debug = debug + " - col_list_str: " + col_list_str
+    
+    //replace SQL template with start and end date strings for tile generation sql 
+    var new_tile_sql = `
+        select 
+            F_TIMESTAMP_TO_INDEX(TILE_START_TS, ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}) as INDEX, ${col_list_str}
+        from (${MONITOR_SQL})
+    `
+
+    var compare_sql = `
+        select t.*, m.VALUE as NEW_VALUE, sysdate() as CREATED_AT
+        from ${TABLE_NAME} t, (${new_tile_sql}) m
+        where t.INDEX = m.INDEX 
+        and t.VALUE != m.VALUE
+    `
+
+    var monitor_table_name = TABLE_NAME + '_MONITOR'
+    var table_exist_sql = `SELECT exists (SELECT * FROM information_schema.tables WHERE table_name = '${monitor_table_name}')`
+    var result = snowflake.execute(
+        {
+            sqlText: table_exist_sql
+        }
+    )
+    result.next()
+    var table_exist = result.getColumnValue(1)
+    debug = debug + " - monitor_table_exist: " + table_exist
+
+
+    if (table_exist === false) {
+
+        var create_sql = `create table ${monitor_table_name} as ${compare_sql}`
+        snowflake.execute(
+            {
+                sqlText: create_sql
+            }
+        )
+        debug = debug + " - inside create monitor table"
+
+    } else {
+
+        var insert_sql = `
+            insert into ${monitor_table_name} ${compare_sql}
+        ` 
+        snowflake.execute(
+            {
+                sqlText: insert_sql
+            }
+        ) 
+        debug = debug + " - inside insert monitor records"
+
+    }
+
+    return debug
+$$;
