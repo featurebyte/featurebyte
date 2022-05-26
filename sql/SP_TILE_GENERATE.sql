c00755cf369a08019764cbad6b3c048e42b5303c
yanhuisg <yanhui@featurebyte.com>
2022-05-26 14:17:47 +0800

    add column names as parameter to tile generation stored proc

--- sql/SP_TILE_GENERATE.sql
+++ sql/SP_TILE_GENERATE.sql
@@ -1,11 +1,11 @@
-create or replace procedure SP_TILE_GENERATE(SQL varchar, START_TS VARCHAR, END_TS VARCHAR, TABLE_NAME varchar, WINDOW_END_MINUTE float, FREQUENCY_MINUTE float)
+create or replace procedure SP_TILE_GENERATE(SQL varchar, WINDOW_END_MINUTE float, FREQUENCY_MINUTE float, COLUMN_NAMES VARCHAR, START_TS VARCHAR, END_TS VARCHAR, TABLE_NAME varchar)
 returns string
 language javascript
 as
 $$
     var debug = "Debug"
 
-    //Check whether tile table for the feature already exists
+    //check whether tile table for the feature already exists
     var tile_exist_sql = `SELECT exists (SELECT * FROM information_schema.tables WHERE table_name = '${TABLE_NAME}')`
     var result = snowflake.execute(
         {
@@ -16,20 +16,20 @@ $$
     var tile_exist = result.getColumnValue(1)
     debug = debug + " - tile_exist: " + tile_exist
 
+    col_list = COLUMN_NAMES.split(",").filter(item => item.toUpperCase() !== "TILE_START_TS")
+    col_list_str = col_list.join(',')
+    debug = debug + " - col_list_str: " + col_list_str
     
-    //replace SQL template with start and end date strings for tile generation sql
-    //TEMPORARY for testing
-    var tile_sql = SQL
-    /*
-    var tile_sql = SQL.replace("<start_ts>", START_TS).replace("<end_ts>", END_TS)
+    //replace SQL template with start and end date strings for tile generation sql    
+    var tile_sql = SQL.replace("FB_START_TS", START_TS).replace("FB_END_TS", END_TS)
     tile_sql = `
         select 
-            F_TIMESTAMP_TO_INDEX(event_timestamp, ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}) as INDEX, *
+            F_TIMESTAMP_TO_INDEX(TILE_START_TS, ${WINDOW_END_MINUTE}, ${FREQUENCY_MINUTE}) as INDEX, ${col_list_str}
         from (${tile_sql})
     `
-    */
 
-    if (tile_exist == false) {
+    if (tile_exist === false) {
+
         //feature tile table does not exist, create the table with the input tile sql
         var tile_create_sql = `create table ${TABLE_NAME} as ${tile_sql}`
         snowflake.execute(
@@ -38,16 +38,23 @@ $$
             }
         )
         debug = debug + " - inside create tile table"
+
     } else {
+
         //feature tile table already exists, insert tile records with the input tile sql
-        //var tile_insert_sql = `insert into ${TABLE_NAME} ${tile_sql}`  
+        insert_cols_str = ""
+        for (element of col_list) {
+          insert_cols_str = insert_cols_str + "b." + element + ","
+        }
+        insert_cols_str = insert_cols_str.slice(0, -1)
+
         var tile_insert_sql = `
             merge into ${TABLE_NAME} a using (${tile_sql}) b
                 on a.INDEX = b.INDEX
                 when matched then 
                     update set a.VALUE = b.VALUE
                 when not matched then 
-                    insert (index, event_timestamp, value) values (b.index, b.event_timestamp, b.value)
+                    insert (INDEX, ${col_list_str}) values (b.INDEX, ${insert_cols_str})
         ` 
         snowflake.execute(
             {
@@ -55,6 +62,7 @@ $$
             }
         ) 
         debug = debug + " - inside insert tile records"
+
     }
 
     return debug
