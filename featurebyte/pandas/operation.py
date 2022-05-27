5fd2215ece40ac14fa50b28a325337e618461648
senbong <senbong.gee@gmail.com>
2022-05-27 23:10:38 +0800

    Revise

--- featurebyte/pandas/operation.py
+++ featurebyte/pandas/operation.py
@@ -23,6 +23,11 @@ class OpsMixin:
     def pytype_dbtype_map(self):
         """
         Supported python builtin scalar type to database type mapping
+
+        Returns
+        -------
+        dict
+            Mapping from supported builtin type to DB type
         """
         return {
             bool: DBVarType.BOOL,
@@ -35,6 +40,11 @@ class OpsMixin:
     def dbtype_pytype_map(self):
         """
         Supported database type mapping to python builtin scalar type
+
+        Returns
+        -------
+        dict
+            Mapping from DB type to supported builtin type
         """
         return {val: key for key, val in self.pytype_dbtype_map.items()}
 
@@ -44,6 +54,27 @@ class OpsMixin:
     ) -> Node:
         """
         Add filter node into the graph & return the node
+
+        Parameters
+        ----------
+        item: DataFrame | Series
+            object to be filtered
+        mask: Series
+            mask used to filter the item object
+        node_output_type: NodeOutputType
+            note output type
+
+        Returns
+        -------
+        Node
+            Filter node
+
+        Raises
+        ------
+        TypeError
+            if mask Series is not boolean type
+        ValueError
+            if the row index between item object & mask are not aligned
         """
         if mask.var_type != DBVarType.BOOL:
             raise TypeError("Only boolean Series filtering is supported!")
--- tests/pandas/test_series.py
+++ tests/pandas/test_series.py
@@ -166,6 +166,7 @@ def test_relational_operators__series_other(bool_series, int_series, float_serie
     """
     Test relational operators with other as Series type
     """
+    # pylint: disable=R0124
     # series_bool_eq = bool_series == bool_series
     # series_int_ne = int_series != int_series
     series_float_lt = float_series < float_series
