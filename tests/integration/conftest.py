8123c01413c9cfe1cdc739fd82828e2357733eae
senbong <senbong.gee@gmail.com>
2022-06-03 23:06:19 +0800

    Update test

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -10,10 +10,10 @@ import pandas as pd
 import pytest
 
 
-@pytest.fixture(name="sqlite_db_filename")
-def sqlite_db_file():
+@pytest.fixture(name="transaction_data")
+def transaction_dataframe():
     """
-    Create SQLite database file with data for testing
+    Simulated transaction Dataframe
     """
     row_number = 100
     rng = np.random.RandomState(1234)
@@ -29,9 +29,18 @@ def sqlite_db_file():
     data["created_at"] += rng.randint(1, 100, row_number).cumsum() * pd.Timedelta(seconds=1)
     data["created_at"] = data["created_at"].astype(int)
     data["cust_id"] = data["cust_id"].cumsum()
+    data["cust_id"] = data["cust_id"].sample(frac=1.0).reset_index(drop=True)
+    data["session_id"] = data["session_id"].sample(frac=1.0).reset_index(drop=True)
+    yield data
 
+
+@pytest.fixture(name="sqlite_db_filename")
+def sqlite_db_file(transaction_data):
+    """
+    Create SQLite database file with data for testing
+    """
     with tempfile.NamedTemporaryFile() as file_handle:
         connection = sqlite3.connect(file_handle.name)
-        data.to_sql(name="browsing_raw", con=connection, index=False)
+        transaction_data.to_sql(name="browsing_raw", con=connection, index=False)
         connection.commit()
         yield file_handle.name
--- tests/integration/test_session.py
+++ tests/integration/test_session.py
@@ -1,11 +1,13 @@
 """
 This module contains session to EventView integration tests
 """
+import pandas as pd
+
 from featurebyte.core.event_view import EventView
 from featurebyte.session.sqlite import SQLiteSession
 
 
-def test_query_object_operation_on_sqlite_source(sqlite_db_filename):
+def test_query_object_operation_on_sqlite_source(sqlite_db_filename, transaction_data):
     """
     Test loading event view from sqlite source
     """
@@ -16,4 +18,13 @@ def test_query_object_operation_on_sqlite_source(sqlite_db_filename):
     assert event_view.columns == ["created_at", "cust_id", "product_action", "session_id"]
 
     event_view["cust_id_x_session_id"] = event_view["cust_id"] * event_view["session_id"]
-    event_view["lucky_customer"] = (event_view["cust_id_x_session_id"] / 1000) > 146.0
+    event_view["lucky_customer"] = (event_view["cust_id_x_session_id"] / 1000) > 140.0
+
+    # construct expected results
+    expected = transaction_data.copy()
+    expected["cust_id_x_session_id"] = expected["cust_id"] * expected["session_id"]
+    expected["lucky_customer"] = (expected["cust_id_x_session_id"] / 1000) > 140.0
+
+    # check agreement
+    output = event_view.preview(limit=expected.shape[0])
+    pd.testing.assert_frame_equal(output, expected[output.columns], check_dtype=False)
