c318dc7bef518c44932ffc9243d4e3fd9e13bfd5
senbong <senbong.gee@gmail.com>
2022-06-06 09:42:26 +0800

    Update tests

--- tests/integration/test_session.py
+++ tests/integration/test_session.py
@@ -18,13 +18,13 @@ def test_query_object_operation_on_sqlite_source(sqlite_session, transaction_dat
     )
     assert event_view.columns == ["created_at", "cust_id", "product_action", "session_id"]
 
-    event_view["cust_id_x_session_id"] = event_view["cust_id"] * event_view["session_id"]
-    event_view["lucky_customer"] = (event_view["cust_id_x_session_id"] / 1000) > 140.0
+    event_view["cust_id_x_session_id"] = event_view["cust_id"] * event_view["session_id"] / 1000.0
+    event_view["lucky_customer"] = event_view["cust_id_x_session_id"] > 140.0
 
     # construct expected results
     expected = transaction_data.copy()
-    expected["cust_id_x_session_id"] = expected["cust_id"] * expected["session_id"]
-    expected["lucky_customer"] = ((expected["cust_id_x_session_id"] / 1000) > 140.0).astype(int)
+    expected["cust_id_x_session_id"] = (expected["cust_id"] * expected["session_id"]) / 1000.0
+    expected["lucky_customer"] = (expected["cust_id_x_session_id"] > 140.0).astype(int)
 
     # check agreement
     output = event_view.preview(limit=expected.shape[0])
@@ -56,3 +56,17 @@ def test_query_object_operation_on_snowflake_source(snowflake_session, transacti
         table_name='"FB_SIMULATE"."PUBLIC"."TEST_TABLE"',
         timestamp_column="CREATED_AT",
     )
+    assert event_view.columns == ["CREATED_AT", "CUST_ID", "PRODUCT_ACTION", "SESSION_ID"]
+
+    event_view["LUCKY_CUSTOMER"] = (
+        event_view["CUST_ID"] * event_view["SESSION_ID"] / 1000.0
+    ) > 140.0
+
+    # construct expected results
+    expected = transaction_data_upper_case.copy()
+    expected["CUST_ID_X_SESSION_ID"] = (expected["CUST_ID"] * expected["SESSION_ID"]) / 1000.0
+    expected["LUCKY_CUSTOMER"] = (expected["CUST_ID_X_SESSION_ID"] > 140.0).astype(int)
+
+    # check agreement
+    output = event_view.preview(limit=expected.shape[0])
+    pd.testing.assert_frame_equal(output, expected[output.columns], check_dtype=False)
