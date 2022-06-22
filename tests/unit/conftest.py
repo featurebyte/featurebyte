ce98dd761aa6139fd1eac90162ce633246e40f23
Yung Siang Liau <yungsiang@featurebyte.com>
2022-06-23 01:20:03 +0800

    Add tests

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,12 +1,15 @@
 """
 Common test fixtures used across unit test directories
 """
+from collections import namedtuple
+
 import pytest
 
+from featurebyte.api.event_view import EventView
 from featurebyte.core.frame import Frame
 from featurebyte.enum import DBVarType
 from featurebyte.query_graph.enum import NodeOutputType, NodeType
-from featurebyte.query_graph.graph import GlobalQueryGraph, GlobalQueryGraphState
+from featurebyte.query_graph.graph import GlobalQueryGraph, GlobalQueryGraphState, Node
 
 
 @pytest.fixture(name="graph")
@@ -45,3 +48,52 @@ def dataframe_fixture(graph):
         column_lineage_map={col: (node.name,) for col in column_var_type_map},
         row_index_lineage=(node.name,),
     )
+
+
+@pytest.fixture(name="session")
+def fake_session():
+    """
+    Fake database session for testing
+    """
+    FakeSession = namedtuple("FakeSession", ["database_metadata", "source_type"])
+    database_metadata = {
+        '"trans"': {
+            "cust_id": DBVarType.INT,
+            "session_id": DBVarType.INT,
+            "event_type": DBVarType.VARCHAR,
+            "value": DBVarType.FLOAT,
+            "created_at": DBVarType.INT,
+        }
+    }
+    yield FakeSession(database_metadata=database_metadata, source_type="sqlite")
+
+
+@pytest.fixture(name="event_view")
+def event_view_fixture(session, graph):
+    """
+    EventView fixture
+    """
+    event_view = EventView.from_session(
+        session=session,
+        table_name='"trans"',
+        timestamp_column="created_at",
+        entity_identifiers=["cust_id"],
+    )
+    assert isinstance(event_view, EventView)
+    expected_inception_node = Node(
+        name="input_1",
+        type=NodeType.INPUT,
+        parameters={
+            "columns": ["cust_id", "session_id", "event_type", "value", "created_at"],
+            "timestamp": "created_at",
+            "entity_identifiers": ["cust_id"],
+            "dbtable": '"trans"',
+        },
+        output_type=NodeOutputType.FRAME,
+    )
+    assert event_view.graph.dict() == graph.dict()
+    assert event_view.protected_columns == {"created_at", "cust_id"}
+    assert event_view.inception_node == expected_inception_node
+    assert event_view.timestamp_column == "created_at"
+    assert event_view.entity_identifiers == ["cust_id"]
+    yield event_view
--- tests/unit/query_graph/test_tiling.py
+++ tests/unit/query_graph/test_tiling.py
@@ -1,9 +1,17 @@
 """
 Tests for featurebyte.query_graph.tiling
 """
+import copy
+
 import pytest
 
-from featurebyte.query_graph.tiling import AggFunc, TileSpec, get_aggregator
+from featurebyte.query_graph.graph import GlobalQueryGraph
+from featurebyte.query_graph.tiling import (
+    AggFunc,
+    TileSpec,
+    get_aggregator,
+    get_tile_table_identifier,
+)
 
 
 @pytest.mark.parametrize(
@@ -51,3 +59,107 @@ def test_tiling_aggregators(agg_func, expected_tile_specs, expected_merge_expr):
     merge_expr = agg.merge()
     assert tile_specs == expected_tile_specs
     assert merge_expr == expected_merge_expr
+
+
+def test_tile_table_id__agg_parameters(event_view):
+
+    feature_names = ["sum_30m", "sum_2h", "sum_1d"]
+    base_kwargs = dict(
+        value_column="value",
+        method="sum",
+        windows=["30m", "2h", "1d"],
+        blind_spot="10m",
+        frequency="30m",
+        time_modulo_frequency="5m",
+        feature_names=feature_names,
+    )
+
+    kwargs = copy.deepcopy(base_kwargs)
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b600_73745c67c6aadbb5bec07085c9df5601e25dd7a0"
+
+    features = event_view.groupby(["event_type"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b600_3c7e197e40e46d702c2fb2f042ac09f8ccc9359b"
+
+    kwargs = copy.deepcopy(base_kwargs)
+    features = event_view.groupby(["event_type"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b600_3c7e197e40e46d702c2fb2f042ac09f8ccc9359b"
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["method"] = "max"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "max_f1800_m300_b600_fb8eca2ad73a377833efcd8e22bb36e4fc0a98b2"
+
+
+def test_tile_table_id__job_setting(event_view):
+
+    feature_names = ["sum_30m", "sum_2h", "sum_1d"]
+    base_kwargs = dict(
+        value_column="value",
+        method="sum",
+        windows=["30m", "2h", "1d"],
+        blind_spot="10m",
+        frequency="30m",
+        time_modulo_frequency="5m",
+        feature_names=feature_names,
+    )
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["frequency"] = "10m"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f600_m300_b600_8b10bcaa2752efbf46a902c716717442ae6b54f8"
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["time_modulo_frequency"] = "10m"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m600_b600_75ee56cfcf23b8a9c4f6c054bafa1ecd76774d97"
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["blind_spot"] = "20m"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b1200_bab672a8bd456b382654cd6dc018f89e16b63922"
+
+
+def test_tile_table_id__transformations(event_view):
+
+    feature_names = ["sum_30m", "sum_2h", "sum_1d"]
+    base_kwargs = dict(
+        value_column="value",
+        method="sum",
+        windows=["30m", "2h", "1d"],
+        blind_spot="10m",
+        frequency="30m",
+        time_modulo_frequency="5m",
+        feature_names=feature_names,
+    )
+
+    event_view["value_10"] = event_view["value"] * 10
+    event_view["value_100"] = event_view["value"] * 100
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["value_column"] = "value_10"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b600_d1718c9dd7b88997626cfbbfe491bc9af9b4d064"
+
+    kwargs = copy.deepcopy(base_kwargs)
+    kwargs["value_column"] = "value_100"
+    features = event_view.groupby(["cust_id"]).aggregate(**kwargs)
+    graph, node = GlobalQueryGraph().prune(features.node, set(feature_names))
+    tile_id = get_tile_table_identifier(graph, node)
+    assert tile_id == "sum_f1800_m300_b600_a2725c23f0cb003d5391ea04b1e1d99f4a61e3e2"
