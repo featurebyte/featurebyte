dfa55f79f03c984ca9ec780c7317dadae7e6fbd8
senbong <senbong.gee@gmail.com>
2022-07-07 21:36:39 +0800

    Implement entity update

--- featurebyte/api/entity.py
+++ featurebyte/api/entity.py
@@ -3,10 +3,18 @@ Entity class
 """
 from __future__ import annotations
 
+from typing import Any
+
 from http import HTTPStatus
 
+from requests.models import Response
+
 from featurebyte.config import Configurations
-from featurebyte.exception import DuplicatedRecordException, RecordCreationException
+from featurebyte.exception import (
+    DuplicatedRecordException,
+    RecordCreationException,
+    RecordUpdateException,
+)
 from featurebyte.models.entity import EntityModel
 
 
@@ -40,7 +48,7 @@ class Entity(EntityModel):
             if response.status_code == HTTPStatus.CONFLICT:
                 raise DuplicatedRecordException(response=response)
             raise RecordCreationException(response=response)
-        super().__init__(name=name, serving_names=[serving_name])
+        super().__init__(**self._response_to_dict(response))
 
     @property
     def serving_name(self) -> str:
@@ -52,3 +60,28 @@ class Entity(EntityModel):
         str
         """
         return self.serving_names[0]
+
+    @staticmethod
+    def _response_to_dict(response: Response) -> dict[str, Any]:
+        """
+        Convert API response to dictionary format
+
+        Parameters
+        ----------
+        response: Response
+        """
+        response_dict = response.json()
+        response_dict["_id"] = response_dict.pop("id")
+        return response_dict
+
+    def update_name(self, name) -> None:
+        """
+        Change entity name
+        """
+        client = Configurations().get_client()
+        response = client.patch(f"/entity/{self.id}", json={"name": name})
+        if response.status_code != HTTPStatus.OK:
+            if response.status_code == HTTPStatus.CONFLICT:
+                raise DuplicatedRecordException(response=response)
+            raise RecordUpdateException(response=response)
+        super().__init__(**self._response_to_dict(response))
--- tests/unit/api/test_entity.py
+++ tests/unit/api/test_entity.py
@@ -7,12 +7,19 @@ from featurebyte.api.entity import Entity
 from featurebyte.exception import DuplicatedRecordException
 
 
-def test_entity_creation(mock_config_path_env, mock_get_persistent):
+@pytest.fixture(name="entity")
+def entity_fixture(mock_config_path_env, mock_get_persistent):
+    """
+    Entity fixture
+    """
+    _ = mock_config_path_env, mock_get_persistent
+    yield Entity(name="customer", serving_name="cust_id")
+
+
+def test_entity_creation(entity):
     """
     Test entity creation
     """
-    _ = mock_config_path_env, mock_get_persistent
-    entity = Entity(name="customer", serving_name="cust_id")
     assert entity.name == "customer"
     assert entity.serving_name == "cust_id"
     assert entity.name_history == []
@@ -24,3 +31,21 @@ def test_entity_creation(mock_config_path_env, mock_get_persistent):
     with pytest.raises(DuplicatedRecordException) as exc:
         Entity(name="Customer", serving_name="cust_id")
     assert exc.value.response.json() == {"detail": 'Entity serving name "cust_id" already exists.'}
+
+
+def test_entity_update_name(entity):
+    """
+    Test update entity name
+    """
+    assert entity.name_history == []
+    entity_id = entity.id
+    entity.update_name("Customer")
+    assert entity.id == entity_id
+    assert entity.name_history == ["customer"]
+
+    # create another entity
+    Entity(name="product", serving_name="product_id")
+
+    with pytest.raises(DuplicatedRecordException) as exc:
+        entity.update_name("product")
+    assert exc.value.response.json() == {"detail": 'Entity name "product" already exists.'}
