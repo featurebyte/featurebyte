bf9bcaec4bb8136c6410d5afe8fe764958e01c6d
Yung Siang Liau <yungsiang@featurebyte.com>
2022-07-21 00:52:37 +0800

    Apply new options regardless of key_path

--- featurebyte/persistent/git.py
+++ featurebyte/persistent/git.py
@@ -105,13 +105,14 @@ class GitDB(Persistent):
         # up by removing the temp directory that stores the control file in GitDB.__del__()
         self._control_path_dir = tempfile.mkdtemp()
         control_path_file = os.path.join(self._control_path_dir, str(uuid.uuid4())[:8])
-        ssh_options = (
-            f"-o ControlMaster=auto -o ControlPath={control_path_file} -o ControlPersist=600"
+        ssh_cmd = f"ssh -i {key_path}" if key_path else "ssh"
+        ssh_cmd += (
+            f" -o ControlMaster=auto -o ControlPath={control_path_file} -o ControlPersist=600"
         )
 
         self._repo = repo = Repo(path=self._local_path)
         self._branch = branch
-        self._ssh_cmd = f"ssh -i {key_path} {ssh_options}" if key_path else "ssh"
+        self._ssh_cmd = ssh_cmd
         self._origin: Optional[Remote] = None
         self._working_tree_dir: str = str(repo.working_tree_dir)
         self._collection_to_doc_name_func_map: dict[str, DocNameFuncType] = {}
