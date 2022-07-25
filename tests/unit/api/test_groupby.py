ce68f1b5865722815388b88c4c455202bf532a06
Yung Siang Liau <yungsiang@featurebyte.com>
2022-07-25 16:50:00 +0800

    Add test

--- tests/unit/api/test_groupby.py
+++ tests/unit/api/test_groupby.py
@@ -201,3 +201,37 @@ def test_groupby__default_feature_job_setting(snowflake_event_data):
         "value_by": None,
         "serving_names": ["cust_id"],
     }
+
+
+def test_groupby__category(snowflake_event_view):
+    """
+    Test category parameter is captured properly
+    """
+    Entity.create(name="customer", serving_name="cust_id")
+    snowflake_event_view.cust_id.as_entity("customer")
+    feature_group = snowflake_event_view.groupby("cust_id", category="col_int").aggregate(
+        value_column="col_float",
+        method="sum",
+        windows=["30m", "1h", "2h"],
+        feature_names=["feat_30m", "feat_1h", "feat_2h"],
+        feature_job_setting=dict(blind_spot="1m30s", frequency="6m", time_modulo_frequency="3m"),
+    )
+    # check node params
+    feature = feature_group["feat_30m"]
+    feature_node_name = feature.node.name
+    groupby_node_name = feature.graph.backward_edges[feature_node_name][0]
+    groupby_node = feature.graph.get_node_by_name(groupby_node_name)
+    assert groupby_node.parameters == {
+        "keys": ["cust_id"],
+        "parent": "col_float",
+        "agg_func": "sum",
+        "windows": ["30m", "1h", "2h"],
+        "names": ["feat_30m", "feat_1h", "feat_2h"],
+        "blind_spot": 90,
+        "frequency": 360,
+        "time_modulo_frequency": 180,
+        "tile_id": "sum_f360_m180_b90_45058a98f2f07813bc673622af9c945b2ceaa16d",
+        "timestamp": "event_timestamp",
+        "value_by": "col_int",
+        "serving_names": ["cust_id"],
+    }
