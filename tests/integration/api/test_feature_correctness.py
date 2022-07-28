7941911500e0ef34da0ff7e5d683be11149202b9
Yung Siang Liau <yungsiang@featurebyte.com>
2022-07-28 17:52:06 +0800

    Refactor test

--- tests/integration/api/test_feature_correctness.py
+++ tests/integration/api/test_feature_correctness.py
@@ -23,6 +23,9 @@ def calculate_feature_ground_truth(
     time_modulo_frequency,
     blind_spot,
 ):
+    """
+    Reference implementation for feature in python that is as simple as possible
+    """
     last_job_index = (get_epoch_seconds(point_in_time) - time_modulo_frequency) // frequency
     last_job_epoch_seconds = last_job_index * frequency + time_modulo_frequency
 
@@ -63,6 +66,26 @@ def training_events(transaction_data_upper_case):
     return df
 
 
+def get_expected_feature_values(training_events, feature_name, **kwargs):
+
+    expected_output = defaultdict(list)
+
+    for _, row in training_events.iterrows():
+        entity_value = row[kwargs["entity_column_name"]]
+        point_in_time = row["POINT_IN_TIME"]
+        val = calculate_feature_ground_truth(
+            point_in_time=point_in_time,
+            entity_value=entity_value,
+            **kwargs,
+        )
+        expected_output["POINT_IN_TIME"].append(point_in_time)
+        expected_output[kwargs["entity_column_name"]].append(entity_value)
+        expected_output[feature_name].append(val)
+
+    df_expected = pd.DataFrame(expected_output)
+    return df_expected
+
+
 def test_aggregation(transaction_data_upper_case, training_events, event_data, config):
 
     event_view = EventView.from_event_data(event_data)
@@ -72,43 +95,34 @@ def test_aggregation(transaction_data_upper_case, training_events, event_data, c
         time_modulo_frequency=feature_job_setting.time_modulo_frequency,
         blind_spot=feature_job_setting.blind_spot,
     )
-    feature_group = event_view.groupby("USER_ID").aggregate(
-        "AMOUNT",
+    variable_column_name = "AMOUNT"
+    entity_column_name = "USER_ID"
+    window_size = 3600 * 24
+    feature_group = event_view.groupby(entity_column_name).aggregate(
+        variable_column_name,
         "avg",
         windows=["24h"],
         feature_names=["AVG_24h"],
     )
     feature_list = FeatureList([feature_group])
 
-    expected_output = defaultdict(list)
     event_timestamp_column_name = "EVENT_TIMESTAMP"
-    variable_column_name = "AMOUNT"
-    entity_column_name = "USER_ID"
     agg_func = lambda x: x.mean()
-    window_size = 3600 * 24
     feature_name = feature_group.feature_names[0]
 
-    for _, row in training_events.iterrows():
-        entity_value = row[entity_column_name]
-        point_in_time = row["POINT_IN_TIME"]
-        val = calculate_feature_ground_truth(
-            df=transaction_data_upper_case,
-            point_in_time=point_in_time,
-            event_timestamp_column_name=event_timestamp_column_name,
-            entity_column_name=entity_column_name,
-            entity_value=entity_value,
-            variable_column_name=variable_column_name,
-            agg_func=agg_func,
-            window_size=window_size,
-            frequency=frequency,
-            time_modulo_frequency=time_modulo_frequency,
-            blind_spot=blind_spot,
-        )
-        expected_output["POINT_IN_TIME"].append(point_in_time)
-        expected_output[entity_column_name].append(entity_value)
-        expected_output[feature_name].append(val)
-
-    df_expected = pd.DataFrame(expected_output)
+    df_expected = get_expected_feature_values(
+        training_events,
+        feature_name,
+        df=transaction_data_upper_case,
+        entity_column_name=entity_column_name,
+        event_timestamp_column_name=event_timestamp_column_name,
+        variable_column_name=variable_column_name,
+        agg_func=agg_func,
+        window_size=window_size,
+        frequency=frequency,
+        time_modulo_frequency=time_modulo_frequency,
+        blind_spot=blind_spot,
+    )
     df_historical_features = feature_list.get_historical_features(
         training_events, credentials=config.credentials, serving_names_mapping={"UID": "USER_ID"}
     )
