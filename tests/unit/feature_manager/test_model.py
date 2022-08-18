d442414fde238a9c10b541fae788b59a653320ce
Yung Siang Liau <yungsiang@featurebyte.com>
2022-08-18 11:37:32 +0800

    Fix feature model tests

--- tests/unit/feature_manager/test_model.py
+++ tests/unit/feature_manager/test_model.py
@@ -68,7 +68,7 @@ def test_extended_feature_model__agg_per_category_feature(
           TO_TIMESTAMP(DATE_PART(EPOCH_SECOND, CAST(__FB_START_DATE AS TIMESTAMP)) + tile_index * 1800) AS __FB_TILE_START_DATE_COLUMN,
           "cust_id",
           "col_int",
-          SUM("col_float") AS value
+          SUM("col_float") AS value_sum_b2cfe14613c88a50946d1b99fa0ae3ca5d89849d
         FROM (
             SELECT
               *,
@@ -105,8 +105,9 @@ def test_extended_feature_model__agg_per_category_feature(
             frequency_minute=30,
             tile_sql=expected_sql,
             entity_column_names=["cust_id", "col_int"],
-            value_column_names=["value"],
-            tile_id="sum_f1800_m300_b600_b2cfe14613c88a50946d1b99fa0ae3ca5d89849d",
+            value_column_names=["value_sum_b2cfe14613c88a50946d1b99fa0ae3ca5d89849d"],
+            tile_id="sf_table_f1800_m300_b600_24dd90a763a6368c29540f4d8151f25da40456f3",
+            aggregation_id="sum_b2cfe14613c88a50946d1b99fa0ae3ca5d89849d",
             category_column_name="col_int",
         )
     ]
