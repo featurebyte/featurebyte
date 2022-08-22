64941930a41520727e3c868a1dd0614416e08c6a
Yung Siang Liau <yungsiang@featurebyte.com>
2022-08-22 23:32:27 +0800

    Break tie in most_frequent lexicographically

--- featurebyte/sql/snowflake/F_COUNT_DICT_MOST_FREQUENT.sql
+++ featurebyte/sql/snowflake/F_COUNT_DICT_MOST_FREQUENT.sql
@@ -6,6 +6,16 @@ $$
   if (!COUNTS) {
     return null;
   }
-  return Object.keys(COUNTS).reduce(function(a, b){ return COUNTS[a] > COUNTS[b] ? a : b });
+  var most_frequent_key = null;
+  var most_frequent_count = 0;
+  for (const k in COUNTS) {
+    if (COUNTS[k] > most_frequent_count) {
+      most_frequent_count = COUNTS[k];
+      most_frequent_key = k;
+    } else if (COUNTS[k] == most_frequent_count && k < most_frequent_key) {
+      most_frequent_key = k;
+    }
+  }
+  return most_frequent_key;
 $$
 ;
