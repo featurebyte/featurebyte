54e7ded2185935b2f990c4de7a909000b0394ddd
Yung Siang Liau <yungsiang@featurebyte.com>
2022-08-24 11:26:37 +0800

    Fix test

--- tests/unit/core/accessor/test_count_dict.py
+++ tests/unit/core/accessor/test_count_dict.py
@@ -8,15 +8,28 @@ from featurebyte.query_graph.enum import NodeOutputType, NodeType
 
 
 @pytest.mark.parametrize(
-    "method, expected_var_type, expected_parameters",
+    "method, method_kwargs, expected_var_type, expected_parameters",
     [
-        ("entropy", DBVarType.FLOAT, {"transform_type": "entropy"}),
-        ("most_frequent", DBVarType.VARCHAR, {"transform_type": "most_frequent"}),
-        ("nunique", DBVarType.FLOAT, {"transform_type": "unique_count"}),
+        ("entropy", {}, DBVarType.FLOAT, {"transform_type": "entropy"}),
+        ("most_frequent", {}, DBVarType.VARCHAR, {"transform_type": "most_frequent"}),
+        (
+            "unique_count",
+            {},
+            DBVarType.FLOAT,
+            {"transform_type": "unique_count", "include_missing": True},
+        ),
+        (
+            "unique_count",
+            {"include_missing": False},
+            DBVarType.FLOAT,
+            {"transform_type": "unique_count", "include_missing": False},
+        ),
     ],
 )
-def test_transformation(count_per_category_feature, method, expected_var_type, expected_parameters):
-    new_feature = getattr(count_per_category_feature.cd, method)()
+def test_transformation(
+    count_per_category_feature, method, method_kwargs, expected_var_type, expected_parameters
+):
+    new_feature = getattr(count_per_category_feature.cd, method)(**method_kwargs)
     assert new_feature.node.output_type == NodeOutputType.SERIES
     assert new_feature.var_type == expected_var_type
     assert new_feature.node.type == NodeType.COUNT_DICT_TRANSFORM
