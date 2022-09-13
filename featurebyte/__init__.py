e9a3fff790fadce12baa292d5c3c9a0801d6d411
Yung Siang Liau <yungsiang@featurebyte.com>
2022-09-13 18:26:52 +0800

    [DEV-493] Import API classes and functions at top level (#262)
    * Import API classes and functions at top level
    
    * Import Feature from the right module
    
    * Avoid importing featurebyte in snowflake.py

--- featurebyte/__init__.py
+++ featurebyte/__init__.py
@@ -1,6 +1,16 @@
 """Python Library for FeatureOps"""
 from importlib import metadata as importlib_metadata
 
+from featurebyte.api.entity import Entity
+from featurebyte.api.event_data import EventData
+from featurebyte.api.event_view import EventView
+from featurebyte.api.feature import Feature
+from featurebyte.api.feature_list import FeatureGroup, FeatureList
+from featurebyte.api.feature_store import FeatureStore
+from featurebyte.core.timedelta import to_timedelta
+from featurebyte.enum import AggFunc, SourceType
+from featurebyte.models.feature_store import SnowflakeDetails
+
 
 def get_version() -> str:
     """
@@ -18,3 +28,18 @@ def get_version() -> str:
 
 
 version: str = get_version()
+
+
+__all__ = [
+    "Entity",
+    "EventData",
+    "EventView",
+    "Feature",
+    "FeatureGroup",
+    "FeatureList",
+    "FeatureStore",
+    "to_timedelta",
+    "AggFunc",
+    "SourceType",
+    "SnowflakeDetails",
+]
