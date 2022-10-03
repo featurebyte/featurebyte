9fcf1300af99e2541efaf48ef7c4749566a0a375
Kenny Chua <kenny@featurebyte.com>
2022-10-03 17:32:34 +0800

    handle invalid credentials in session (#310)

--- tests/unit/routes/test_feature_store.py
+++ tests/unit/routes/test_feature_store.py
@@ -7,6 +7,7 @@ from unittest.mock import patch
 import pytest
 from bson.objectid import ObjectId
 
+from featurebyte.exception import CredentialsError
 from tests.unit.routes.base import BaseApiTestSuite
 
 
@@ -95,6 +96,23 @@ class TestFeatureStoreApi(BaseApiTestSuite):
             response = test_api_client.post(f"{self.base_route}/database", json=feature_store)
         assert response.status_code == HTTPStatus.OK
         assert response.json() == databases
+
+    def test_list_databases__401(self, test_api_client_persistent, create_success_response):
+        """
+        Test list databases with invalid credentials
+        """
+        test_api_client, _ = test_api_client_persistent
+        assert create_success_response.status_code == HTTPStatus.CREATED
+        feature_store = create_success_response.json()
+
+        credentials_error = CredentialsError("Invalid credentials provided.")
+        with patch(
+            "featurebyte.core.generic.ExtendedFeatureStoreModel.get_session"
+        ) as mock_get_session:
+            mock_get_session.return_value.list_databases.side_effect = credentials_error
+            response = test_api_client.post(f"{self.base_route}/database", json=feature_store)
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {"detail": str(credentials_error)}
 
     def test_list_schemas__422(self, test_api_client_persistent, create_success_response):
         """
