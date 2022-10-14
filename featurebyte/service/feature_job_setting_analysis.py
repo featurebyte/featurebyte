ee66cfb0b37edd473fe5a9b39680013ad2eedac0
Kenny Chua <kenny@featurebyte.com>
2022-10-14 18:35:30 +0800

    [DEV-434] bump freeware version, update fixtures (#331)
    * bump freeware version, update fixtures
    
    * fix test

--- featurebyte/service/feature_job_setting_analysis.py
+++ featurebyte/service/feature_job_setting_analysis.py
@@ -7,6 +7,7 @@ from typing import Any, Optional
 
 from bson.objectid import ObjectId
 
+from featurebyte.exception import DocumentError
 from featurebyte.models.base import FeatureByteBaseDocumentModel, FeatureByteBaseModel
 from featurebyte.models.event_data import EventDataModel
 from featurebyte.models.feature_job_setting_analysis import FeatureJobSettingAnalysisModel
@@ -19,6 +20,7 @@ from featurebyte.schema.worker.task.feature_job_setting_analysis import (
     FeatureJobSettingAnalysisTaskPayload,
 )
 from featurebyte.service.base_document import BaseDocumentService
+from featurebyte.service.event_data import EventDataService
 from featurebyte.service.task_manager import AbstractTaskManager, TaskId
 
 
@@ -50,6 +52,11 @@ class FeatureJobSettingAnalysisService(BaseDocumentService[FeatureJobSettingAnal
         Returns
         -------
         TaskId
+
+        Raises
+        ------
+        DocumentError
+            Creation date column is not available for the event data
         """
         # check any conflict with existing documents
         output_document_id = data.id or ObjectId()
@@ -58,10 +65,16 @@ class FeatureJobSettingAnalysisService(BaseDocumentService[FeatureJobSettingAnal
         )
 
         # check that event data exists
-        _ = await self._get_document(
+        event_data_service = EventDataService(
+            user=self.user,
+            persistent=self.persistent,
+        )
+        event_data = await event_data_service.get_document(
             document_id=data.event_data_id,
             collection_name=EventDataModel.collection_name(),
         )
+        if not event_data.record_creation_date_column:
+            raise DocumentError("Creation date column is not available for the event data.")
 
         payload = FeatureJobSettingAnalysisTaskPayload(
             **data.dict(), user_id=self.user.id, output_document_id=output_document_id
