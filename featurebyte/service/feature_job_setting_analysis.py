ce02d0080fe045025d7b3f2707ab5ca203ab54b0
senbong <senbong@featurebyte.com>
2022-10-21 11:43:22 +0800

    Revise service/feature_job_setting_analysis.py

--- featurebyte/service/feature_job_setting_analysis.py
+++ featurebyte/service/feature_job_setting_analysis.py
@@ -3,8 +3,6 @@ FeatureJobSettingAnalysisService class
 """
 from __future__ import annotations
 
-from typing import Any
-
 from bson.objectid import ObjectId
 
 from featurebyte.exception import DocumentError
@@ -32,11 +30,6 @@ class FeatureJobSettingAnalysisService(
     """
 
     document_class = FeatureJobSettingAnalysisModel
-
-    async def create_document(
-        self, data: FeatureByteBaseModel, get_credential: Any = None
-    ) -> FeatureJobSettingAnalysisModel:
-        raise NotImplementedError
 
     async def create_document_creation_task(
         self, data: FeatureJobSettingAnalysisCreate, task_manager: AbstractTaskManager
