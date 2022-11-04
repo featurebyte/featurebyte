f215dbd901ed5c1424187bc3f463caff001279f2
Jevon Yeoh <jevon@featurebyte.com>
2022-11-04 11:06:38 +0800

    session/base: rm filter in register functions and procedures
    We recently added some logic to optimize the initialization flow
    that is gated by a version number. As such, we don't need to optimize
    this check anymore here anymore. This also allows us to update
    stored functions and procedures when we update them, as we would
    previously only write these functions if they were net new.

--- featurebyte/session/base.py
+++ featurebyte/session/base.py
@@ -517,9 +517,7 @@ class BaseSchemaInitializer(ABC):
         functions : list[dict[str, Any]]
             List of functions to register
         """
-        existing = self._normalize_casings(await self.list_functions())
-        items = [item for item in functions if item["identifier"] not in existing]
-        await self._register_sql_objects(items)
+        await self._register_sql_objects(list(functions))
 
     async def register_missing_procedures(self, procedures: list[dict[str, Any]]) -> None:
         """Register procedures defined in the snowflake sql directory
@@ -529,9 +527,7 @@ class BaseSchemaInitializer(ABC):
         procedures: list[dict[str, Any]]
             List of procedures to register
         """
-        existing = self._normalize_casings(await self.list_procedures())
-        items = [item for item in procedures if item["identifier"] not in existing]
-        await self._register_sql_objects(items)
+        await self._register_sql_objects(list(procedures))
 
     async def create_missing_tables(self, tables: list[dict[str, Any]]) -> None:
         """Create tables defined in snowflake sql directory
