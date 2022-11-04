8e982c4a6b2eb5631c1859c62b0addcf40563b5e
Yung Siang Liau <yungsiang@featurebyte.com>
2022-11-05 00:48:07 +0800

    Add event_data_id in ItemDataModel

--- featurebyte/models/item_data.py
+++ featurebyte/models/item_data.py
@@ -9,6 +9,7 @@ from typing import Any, Optional
 from pydantic import Field, StrictStr, validator
 
 from featurebyte.enum import DBVarType, TableDataType
+from featurebyte.models.base import PydanticObjectId
 from featurebyte.models.feature_store import DataModel
 
 
@@ -17,28 +18,31 @@ class ItemDataModel(DataModel):
     Model for ItemData entity
 
     id: PydanticObjectId
-        EventData id of the object
+        Id of the object
     name : str
-        Name of the EventData
+        Name of the ItemData
     tabular_source : TabularSource
         Data warehouse connection information & table name tuple
     columns_info: List[ColumnInfo]
-        List of event data columns
+        List of ItemData columns
     status: DataStatus
         Status of the ItemData
     event_id_column: str
         Event ID column name
     item_id_column: str
         Item ID column name
+    event_data_id: PydanticObjectId
+        Id of the associated EventData
     created_at : Optional[datetime]
-        Datetime when the EventData was first saved or published
+        Datetime when the ItemData was first saved or published
     updated_at: Optional[datetime]
-        Datetime when the EventData object was last updated
+        Datetime when the ItemData object was last updated
     """
 
     type: TableDataType = Field(TableDataType.ITEM_DATA, const=True)
     event_id_column: StrictStr
     item_id_column: StrictStr
+    event_data_id: PydanticObjectId
 
     @validator("record_creation_date_column")
     @classmethod
--- featurebyte/schema/item_data.py
+++ featurebyte/schema/item_data.py
@@ -5,6 +5,7 @@ from typing import List
 
 from pydantic import StrictStr
 
+from featurebyte.models.base import PydanticObjectId
 from featurebyte.models.item_data import ItemDataModel
 from featurebyte.schema.common.base import PaginationMixin
 from featurebyte.schema.data import DataCreate, DataUpdate
@@ -17,6 +18,7 @@ class ItemDataCreate(DataCreate):
 
     event_id_column: StrictStr
     item_id_column: StrictStr
+    event_data_id: PydanticObjectId
 
 
 class ItemDataList(PaginationMixin):
