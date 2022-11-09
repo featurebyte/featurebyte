1794b73255c2bf91545937437d049ff35241c334
Jevon Yeoh <jevon@featurebyte.com>
2022-11-09 13:59:21 +0800

    scd_data/controller: init controller

--- featurebyte/enum.py
+++ featurebyte/enum.py
@@ -206,3 +206,5 @@ class SemanticType(StrEnum):
     EVENT_ID = "event_id"
     ITEM_ID = "item_id"
     DIMENSION_ID = "dimension_id"
+    SCD_NATURAL_KEY_ID = "scd_natural_key_id"
+    SCD_SURROGATE_KEY_ID = "scd_surrogate_key_id"
