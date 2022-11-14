1926e6ee2f92f5f8cee8d60a20bf81880c0bec59
senbong <senbong@featurebyte.com>
2022-11-14 14:05:00 +0800

    Fix bugs in migration

--- featurebyte/migration/service/feature_list_namespace.py
+++ featurebyte/migration/service/feature_list_namespace.py
@@ -15,21 +15,19 @@ class FeatureListNamespaceMigrationService(FeatureListNamespaceService, Migratio
     async def change_field_name_from_event_data_ids_to_tabular_data_ids(self) -> None:
         """Change field name from event_data_ids to tabular_data_ids"""
         # sample first 10 records before migration
-        sample_docs_before, total_before = await self.persistent.find(
-            collection_name="feature_list_namespace", query_filter={}, page_size=10
+        query_filter, page_size = {}, 10
+        _, total_before = await self.persistent.find(
+            collection_name="feature_list_namespace", query_filter=query_filter, page_size=page_size
         )
-        sample_docs_before_map = {doc["_id"]: doc for doc in sample_docs_before}
 
         # migrate all records and audit records
-        await self.migrate_all_records(query_filter={})
+        await self.migrate_all_records(query_filter=query_filter)
 
         # check the sample records after migration
         sample_docs_after, total_after = await self.persistent.find(
-            collection_name="feature_list_namespace",
-            query_filter={"_id": {"$in": list(sample_docs_before_map)}},
+            collection_name="feature_list_namespace", query_filter=query_filter, page_size=page_size
         )
-        sample_docs_after_map = {doc["_id"]: doc for doc in sample_docs_after}
-        assert total_before == total_after
-        for doc in sample_docs_after_map.values():
+        assert total_before == total_after, (total_before, total_after)
+        for doc in sample_docs_after:
             assert "event_data_ids" not in doc
             assert "tabular_data_ids" in doc
