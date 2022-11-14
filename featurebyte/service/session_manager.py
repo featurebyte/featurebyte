cacdd8eaf1b4648a5821dd04a001285a7d010a33
Jevon Yeoh <jevon@featurebyte.com>
2022-11-14 15:29:30 +0800

    service/session_manager: copy over session manager code from mixin

---
+++ featurebyte/service/session_manager.py
@@ -0,0 +1,52 @@
+"""
+SessionManager service
+"""
+from typing import Any
+
+from pydantic import ValidationError
+
+from featurebyte.exception import CredentialsError
+from featurebyte.models import FeatureStoreModel
+from featurebyte.service.base_service import BaseService
+from featurebyte.session.base import BaseSession
+from featurebyte.session.manager import SessionManager
+
+
+class SessionManagerService(BaseService):
+    """
+    SessionManagerService class is responsible for retrieving a session manager.
+    """
+
+    def get_feature_store_session(
+        self, feature_store: FeatureStoreModel, get_credential: Any
+    ) -> BaseSession:
+        """
+        Get session for feature store
+        Parameters
+        ----------
+        feature_store: FeatureStoreModel
+            ExtendedFeatureStoreModel object
+        get_credential: Any
+            Get credential handler function
+        Returns
+        -------
+        BaseSession
+            BaseSession object
+        Raises
+        ------
+        CredentialsError
+            When the credentials used to access the feature store is missing or invalid
+        """
+        try:
+            session_manager = SessionManager(
+                credentials={
+                    feature_store.name: await get_credential(
+                        user_id=self.user.id, feature_store_name=feature_store.name
+                    )
+                }
+            )
+            return await session_manager.get_session(feature_store)
+        except ValidationError as exc:
+            raise CredentialsError(
+                f'Credential used to access FeatureStore (name: "{feature_store.name}") is missing or invalid.'
+            ) from exc
