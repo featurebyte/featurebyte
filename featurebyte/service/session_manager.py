8d0fa0c608a3f6077999dc3e55a3684b595756ba
Jevon Yeoh <jevon@featurebyte.com>
2022-11-18 01:31:37 +0800

    session_manager: refactor session manager

--- featurebyte/service/session_manager.py
+++ featurebyte/service/session_manager.py
@@ -8,6 +8,7 @@ from pydantic import ValidationError
 from featurebyte.exception import CredentialsError
 from featurebyte.models.feature_store import FeatureStoreModel
 from featurebyte.persistent import Persistent
+from featurebyte.service.session_validator import SessionValidatorService
 from featurebyte.session.base import BaseSession
 from featurebyte.session.manager import SessionManager
 
@@ -22,22 +23,26 @@ class SessionManagerService:
     def __init__(self, user: Any, persistent: Persistent):
         self.user = user
         self.persistent = persistent
+        self.session_validator_service = SessionValidatorService(user, persistent)
 
     async def get_feature_store_session(
         self, feature_store: FeatureStoreModel, get_credential: Any
     ) -> BaseSession:
         """
         Get session for feature store
+
         Parameters
         ----------
         feature_store: FeatureStoreModel
             ExtendedFeatureStoreModel object
         get_credential: Any
             Get credential handler function
+
         Returns
         -------
         BaseSession
             BaseSession object
+
         Raises
         ------
         CredentialsError
@@ -51,7 +56,11 @@ class SessionManagerService:
                     )
                 }
             )
-            return await session_manager.get_session(feature_store)
+            session = await session_manager.get_session(feature_store)
+            await self.session_validator_service.validate_details(
+                feature_store.name, feature_store.type, feature_store.details
+            )
+            return session
         except ValidationError as exc:
             raise CredentialsError(
                 f'Credential used to access FeatureStore (name: "{feature_store.name}") is missing or invalid.'
