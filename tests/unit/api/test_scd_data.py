032a2b436e3888f62b90a7426e33b10c641741c6
Jevon Yeoh <jevon@featurebyte.com>
2022-11-17 13:29:56 +0800

    test_scd_data: extend base test

--- tests/unit/api/test_scd_data.py
+++ tests/unit/api/test_scd_data.py
@@ -5,11 +5,28 @@ from unittest.mock import patch
 
 import pytest
 
-from featurebyte.api.dimension_data import DimensionData
 from featurebyte.api.scd_data import SlowlyChangingData
 from featurebyte.enum import TableDataType
 from featurebyte.exception import DuplicatedRecordException, RecordRetrievalException
 from featurebyte.models.feature_store import DataStatus
+from tests.unit.api.base_data_test import BaseDataTestSuite, DataType
+
+
+class TestEventDataTestSuite(BaseDataTestSuite):
+
+    data_type = DataType.DIMENSION_DATA
+    col = "col_int"
+    expected_columns = {
+        "col_char",
+        "col_float",
+        "col_boolean",
+        "event_timestamp",
+        "col_text",
+        "created_at",
+        "col_binary",
+        "col_int",
+        "cust_id",
+    }
 
 
 @pytest.fixture(name="scd_data_dict")
