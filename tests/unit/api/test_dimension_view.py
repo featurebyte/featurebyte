a0d9d7d2902d09ed80c77a1718070024305f1a0e
Jevon Yeoh <jevon@featurebyte.com>
2022-11-25 14:05:03 +0800

    add tests for dimension and scd view updates

--- tests/unit/api/test_dimension_view.py
+++ tests/unit/api/test_dimension_view.py
@@ -1,7 +1,10 @@
 """
 Unit tests for DimensionView class
 """
+import pytest
+
 from featurebyte.api.dimension_view import DimensionView
+from featurebyte.exception import JoinViewMismatchError
 from tests.unit.api.base_view_test import BaseViewTestSuite, ViewType
 
 
@@ -19,3 +22,17 @@ class TestDimensionView(BaseViewTestSuite):
 
     def getitem_frame_params_assertions(self, row_subset, view_under_test):
         assert row_subset.dimension_data_id_column == view_under_test.dimension_data_id_column
+
+
+def test_validate_join(snowflake_dimension_view, snowflake_scd_view):
+    with pytest.raises(JoinViewMismatchError):
+        snowflake_dimension_view.validate_join(snowflake_scd_view)
+
+    # assert that joining with a dimension view has no issues
+    snowflake_dimension_view.validate_join(snowflake_dimension_view)
+
+
+def test_get_join_column(snowflake_dimension_view):
+    column = snowflake_dimension_view.get_join_column()
+    # col_int is the dimension_data_id_key column name used when creating this view fixture
+    assert column == "col_int"
