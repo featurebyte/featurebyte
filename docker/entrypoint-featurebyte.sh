ca087d73777c01f76e127cc2af0c1e99ad2ee134
Chester <chester@featurebyte.com>
2022-12-05 18:24:05 +0800

    added docs to docker-compose

--- docker/entrypoint-featurebyte.sh
+++ docker/entrypoint-featurebyte.sh
@@ -5,7 +5,7 @@ while ! nc -zvw10 mongo-rs 27021 1>/dev/null 2>&1; do echo "Waiting for upstream
 
 # perform database migration
 echo "Performing migration"
-PYTHONPATH=$PWD python /scripts/migration.py
+PYTHONPATH=$PWD python /scripts/migration.py >/dev/null 2>&1
 
 # Start notebook
 echo "Starting jupyter notebook"
@@ -13,17 +13,38 @@ echo "Storing notebooks in directory: ~/.featurebyte/notebook"
 if [[ ! -d /app/.featurebyte/notebook ]]; then mkdir /app/.featurebyte/notebook; fi
 jupyter-lab \
     --notebook-dir=/app/.featurebyte/notebook \
-    --port=8089 \
+    --port=8090 \
     --NotebookApp.token='' \
     --NotebookApp.password='' \
     --NotebookApp.allow_origin='*' \
     --NotebookApp.ip='0.0.0.0' \
-    --no-browser &
+    --allow-root \
+    --no-browser > /dev/null 2>&1 &
 
 # Start serving docs
 echo "Starting nginx docs server"
 nginx
 
+echo "Featurebyte Beta Server is Running"
+echo "┌────────────────────────┬──────────────────────────┐"
+echo "│Service                 │Connection Details        │"
+echo "├────────────────────────┼──────────────────────────┤"
+echo "│featurebyte-backend     │localhost:8088            │"
+echo "├────────────────────────┼──────────────────────────┤"
+echo "│featurebyte-swagger-docs│localhost:8090            │"
+echo "├────────────────────────┼──────────────────────────┤"
+echo "│jupyterlab-notebook     │localhost:8089            │"
+echo "└────────────────────────┴──────────────────────────┘"
+echo ""
+echo "Persistent Configuration Files"
+echo "┌────────────────────────┬──────────────────────────┐"
+echo "│General Config Location │~/.featurebyte            │"
+echo "├────────────────────────┼──────────────────────────┤"
+echo "│MongoDB Files           │~/.featurebyte/data       │"
+echo "├────────────────────────┴──────────────────────────┤"
+echo "│Jupyter Notebooks       │~/.featurebyte/notebook   │"
+echo "└────────────────────────┴──────────────────────────┘"
+echo ""
+
 # start featurebyte service
-echo "Starting featurebyte server"
-uvicorn featurebyte.app:app --host=$API_HOST --port=$API_PORT --workers=$WORKERS --timeout-keep-alive=300
+uvicorn featurebyte.app:app --host=$API_HOST --port=$API_PORT --workers=$WORKERS --timeout-keep-alive=300 > /dev/null 2>&1
