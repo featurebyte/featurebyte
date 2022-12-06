29d7b16b509429710199d1eb5040606fc901a7a0
Jevon Yeoh <jevon@featurebyte.com>
2022-12-06 18:21:00 +0800

    rm unused import

--- tests/integration/api/test_item_view_operations.py
+++ tests/integration/api/test_item_view_operations.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import pytest
-from pandas._testing import assert_frame_equal
 from pandas.testing import assert_series_equal
 
 from featurebyte.api.dimension_view import DimensionView
