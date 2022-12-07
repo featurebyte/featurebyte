7d3eba3c255485fd8f3ddd7fada592e0f6db45fe
Jevon Yeoh <jevon@featurebyte.com>
2022-12-08 01:19:23 +0800

    utils/credential: move to a class (#506)
    This commit moves the `get_credential` function into an interface.
    This will allow us to have better encapsulation/overriding when
    we need a different implementation (eg. in the SaaS version).
    
    Eventually, we can move away from passing the get_credential
    handler via the request.state.get_credential function, since this
    functionality is not request scoped. This would then simplify
    some of the code paths.

--- featurebyte/utils/credential.py
+++ featurebyte/utils/credential.py
@@ -3,15 +3,56 @@ Utility functions for credential management
 """
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
+
 from bson.objectid import ObjectId
 
 from featurebyte.config import Configurations
 from featurebyte.models.credential import Credential
 
 
+class CredentialProvider(ABC):
+    """
+    CredentialProvider is the base class for users to get credentials.
+    """
+
+    @abstractmethod
+    async def get_credential(
+        self, user_id: ObjectId | None, feature_store_name: str
+    ) -> Credential | None:
+        """
+        Retrieve credentials from some persistent source.
+
+        Parameters
+        ----------
+        user_id: ObjectId | None
+            User ID
+        feature_store_name: str
+            FeatureStore name
+
+        Returns
+        -------
+        Credential
+            Credential for the database source
+        """
+
+
+class ConfigCredentialProvider(CredentialProvider):
+    """
+    ConfigCredentialProvider will retrieve the users credentials from a configuration file.
+    """
+
+    async def get_credential(
+        self, user_id: ObjectId | None, feature_store_name: str
+    ) -> Credential | None:
+        _ = user_id
+        config = Configurations()
+        return config.credentials.get(feature_store_name)
+
+
 async def get_credential(user_id: ObjectId | None, feature_store_name: str) -> Credential | None:
     """
-    Retrieve credential from FeatureStoreModel
+    Delegate to the ConfigCredentialProvider. We will look to deprecate direct access to this function in the future.
 
     Parameters
     ----------
@@ -25,6 +66,5 @@ async def get_credential(user_id: ObjectId | None, feature_store_name: str) -> C
     Credential
         Credential for the database source
     """
-    _ = user_id
-    config = Configurations()
-    return config.credentials.get(feature_store_name)
+    credential_provider = ConfigCredentialProvider()
+    return await credential_provider.get_credential(user_id, feature_store_name)
