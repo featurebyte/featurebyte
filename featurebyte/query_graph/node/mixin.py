c64185f0229ad159e9e3c659c90b4f3bb08c27af
Jevon Yeoh <jevon@featurebyte.com>
2022-12-12 16:00:31 +0800

    pr comments / lint

--- featurebyte/query_graph/node/mixin.py
+++ featurebyte/query_graph/node/mixin.py
@@ -74,7 +74,7 @@ class SeriesOutputNodeOpStructMixin:
                 )
             ]
             node_kwargs["is_time_based"] = any(
-                [input_op_structure.is_time_based for input_op_structure in inputs]
+                input_op_structure.is_time_based for input_op_structure in inputs
             )
 
         return OperationStructure(
@@ -184,14 +184,11 @@ class GroupbyNodeOpStructMixin:
                 node_name=self.name,
                 other_node_names=other_node_names,
             )
-        # Item aggregates are the only one aggregates which are not time based.
-        is_time_based = True
-        if self.type == NodeType.ITEM_GROUPBY:
-            is_time_based = False
 
         return OperationStructure(
             **node_kwargs,
             output_type=self.output_type,
             output_category=output_category,
-            is_time_based=is_time_based,
+            is_time_based=self.type
+            != NodeType.ITEM_GROUPBY,  # Only item aggregates are not time based.
         )
