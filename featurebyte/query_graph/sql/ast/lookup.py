2198b645daf4d3ab66709e26e0e3a35bc2bd19c6
Yung Siang Liau <yungsiang@featurebyte.com>
2022-12-13 09:08:51 +0800

    Add comments and only allow frame as input

--- featurebyte/query_graph/sql/ast/lookup.py
+++ featurebyte/query_graph/sql/ast/lookup.py
@@ -11,7 +11,6 @@ from sqlglot.expressions import Expression, Select
 
 from featurebyte.query_graph.enum import NodeType
 from featurebyte.query_graph.sql.ast.base import SQLNodeContext, TableNode
-from featurebyte.query_graph.sql.ast.generic import Project
 from featurebyte.query_graph.sql.common import SQLType, quoted_identifier
 from featurebyte.query_graph.sql.specs import LookupSpec
 
@@ -20,6 +19,15 @@ from featurebyte.query_graph.sql.specs import LookupSpec
 class Lookup(TableNode):
     """
     LookupNode SQLNode
+
+    This node has two responsibilities:
+
+    1. Serve as the source of lookup features when sql type is AGGREGATION
+    2. Construct post-aggregation expressions involving lookup features when sql type is
+       POST_AGGREGATION
+
+    The heavy lifting of lookup join (dimension lookup or SCD lookup) from the source is not done
+    here but in LookupAggregator.
     """
 
     source_node: TableNode
@@ -32,19 +40,20 @@ class Lookup(TableNode):
     @classmethod
     def build(cls, context: SQLNodeContext) -> Lookup:
 
+        # Should have only one input
         input_sql_nodes = context.input_sql_nodes
         assert len(input_sql_nodes) == 1
-        input_node = input_sql_nodes[0]
 
-        if isinstance(input_node, TableNode):
-            source_node = input_node
-        else:
-            assert isinstance(input_node, Project)
-            source_node = input_node.table_node
+        # That input should be TableNode
+        input_node = input_sql_nodes[0]
+        assert isinstance(input_node, TableNode)
+        source_node = input_node
 
         if context.sql_type == SQLType.AGGREGATION:
             columns_map = source_node.copy().columns_map
         else:
+            assert context.sql_type == SQLType.POST_AGGREGATION
+            # Create LookupSpec which determines the internal aggregated result names
             columns_map = {}
             specs = LookupSpec.from_lookup_query_node(
                 context.query_node, source_expr=cast(Select, source_node.sql)
