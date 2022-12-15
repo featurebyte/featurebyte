de165f98ffe77d342c436e3be2dc5c5944bcfb92
yanhuisg <yanhui@featurebyte.com>
2022-12-15 10:46:15 +0800

    add tests for date util

--- featurebyte/common/date_util.py
+++ featurebyte/common/date_util.py
@@ -3,31 +3,37 @@ Date related common utility function
 """
 from __future__ import annotations
 
-from datetime import datetime, timedelta
-
-import pytz
+from datetime import datetime, timedelta, timezone
 
 from featurebyte.models.tile import TileSpec
 
 
-def timestamp_to_tile_index(input_datetime: datetime, tile_spec: TileSpec) -> int:
+def timestamp_to_tile_index(input_dt: datetime, tile_spec: TileSpec) -> int:
     """
     Convert datetime to tile index
 
     Parameters
     ----------
-    input_datetime: datetime
+    input_dt: datetime
         Input datetime
     tile_spec: TileSpec
         tile spec
+
+    Raises
+    ----------
+    ValueError
+        When input datetime timezone is empty or is not UTC
 
     Returns
     -------
     tile index
     """
+    if not input_dt.tzinfo or input_dt.tzname() != "UTC":
+        raise ValueError("UTC timezone is required")
+
     offset = tile_spec.time_modulo_frequency_second - tile_spec.blind_spot_second
-    adjusted_ts = input_datetime - timedelta(seconds=offset)
-    period_in_seconds = (adjusted_ts - datetime(1970, 1, 1)).total_seconds()
+    adjusted_ts = input_dt - timedelta(seconds=offset)
+    period_in_seconds = (adjusted_ts - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds()
     tile_ind = int(period_in_seconds / (tile_spec.frequency_minute * 60))
     return tile_ind
 
@@ -49,6 +55,6 @@ def tile_index_to_timestamp(tile_index: int, tile_spec: TileSpec) -> datetime:
     """
     offset = tile_spec.time_modulo_frequency_second - tile_spec.blind_spot_second
     period_in_seconds = tile_index * tile_spec.frequency_minute * 60
-    epoch_ts = datetime.fromtimestamp(period_in_seconds).astimezone(pytz.timezone("UTC"))
+    epoch_ts = datetime.fromtimestamp(period_in_seconds).astimezone(timezone.utc)
     adjusted_ts = epoch_ts + timedelta(seconds=offset)
     return adjusted_ts
--- tests/unit/common/test_date_util.py
+++ tests/unit/common/test_date_util.py
@@ -1,22 +1,38 @@
 """
 Tests for functions in date_util.py module
 """
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
+
+import pytest
 
 from featurebyte.common import date_util
 
 
 def test_timestamp_to_tile_index(mock_snowflake_tile):
     """Test convert timestamp to tile_index"""
-    dt = datetime(2022, 1, 1)
+    dt = datetime(2022, 1, 1, tzinfo=timezone.utc)
     tile_ind = date_util.timestamp_to_tile_index(dt, mock_snowflake_tile)
     assert tile_ind == 5469983
 
 
+def test_timestamp_to_tile_index_no_timezone(mock_snowflake_tile):
+    """Test convert timestamp to tile_index"""
+    dt = datetime(2022, 1, 1)
+    with pytest.raises(ValueError) as excinfo:
+        date_util.timestamp_to_tile_index(dt, mock_snowflake_tile)
+    assert "UTC timezone is required" in str(excinfo.value)
+
+
+def test_timestamp_to_tile_index_incorrect_timezone(mock_snowflake_tile):
+    """Test convert timestamp to tile_index"""
+    dt = datetime(2022, 1, 1, tzinfo=timezone(timedelta(hours=8)))
+    with pytest.raises(ValueError) as excinfo:
+        date_util.timestamp_to_tile_index(dt, mock_snowflake_tile)
+    assert "UTC timezone is required" in str(excinfo.value)
+
+
 def test_tile_index_to_timestamp(mock_snowflake_tile):
     """Test convert timestamp to tile_index"""
-    expected_dt = datetime(2021, 12, 31, 23, 58)
+    expected_dt = datetime(2021, 12, 31, 23, 58, tzinfo=timezone.utc)
     derived_dt = date_util.tile_index_to_timestamp(5469983, mock_snowflake_tile)
-    assert expected_dt.strftime("%Y-%m-%dT%H:%M:%S.%fZ") == derived_dt.strftime(
-        "%Y-%m-%dT%H:%M:%S.%fZ"
-    )
+    assert derived_dt == expected_dt
