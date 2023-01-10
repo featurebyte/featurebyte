9b8e4bb60b3206bca33689b366510c578a7d2e7c
Kenny Chua <kenny@featurebyte.com>
2023-01-10 15:43:23 +0800

    use local host for debug

--- featurebyte/app.py
+++ featurebyte/app.py
@@ -138,4 +138,4 @@ app = get_app()
 
 if __name__ == "__main__":
     # for debugging the api service
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
