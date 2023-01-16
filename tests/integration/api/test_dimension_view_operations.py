1be63460694321368473951c3fec36b526feb40f
Jevon Yeoh <jevon@featurebyte.com>
2023-01-16 17:56:21 +0800

    simplify test

--- tests/integration/api/test_dimension_view_operations.py
+++ tests/integration/api/test_dimension_view_operations.py
@@ -10,14 +10,6 @@ from tests.integration.api.feature_preview_utils import (
 )
 
 
-@pytest.fixture(name="item_type_dimension_lookup_feature_and_view")
-def item_type_dimension_lookup_feature_and_view_fixture(dimension_view):
-    """
-    Get item type dimension lookup feature, and dimension view
-    """
-    return dimension_view["item_type"].as_feature("ItemTypeFeature"), dimension_view
-
-
 @pytest.fixture(name="item_type_dimension_lookup_feature")
 def item_type_dimension_lookup_feature_fixture(dimension_view):
     """
@@ -26,11 +18,11 @@ def item_type_dimension_lookup_feature_fixture(dimension_view):
     return dimension_view["item_type"].as_feature("ItemTypeFeature")
 
 
-def test_dimension_lookup_features(item_type_dimension_lookup_feature_and_view):
+def test_dimension_lookup_features(dimension_view):
     """
     Test lookup features from DimensionView
     """
-    feature, dimension_view = item_type_dimension_lookup_feature_and_view
+    feature = dimension_view["item_type"].as_feature("ItemTypeFeature")
 
     # Test single lookup feature
     preview_params = {"POINT_IN_TIME": "2001-11-15 10:00:00", "item_id": "item_42"}
