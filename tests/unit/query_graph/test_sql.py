980934772cc6d4296452684b3e6b5663cfa59396
Jevon Yeoh <jevon@featurebyte.com>
2023-01-20 11:18:29 +0800

    update test

--- tests/unit/query_graph/test_sql.py
+++ tests/unit/query_graph/test_sql.py
@@ -338,8 +338,8 @@ def test_is_in_node(input_node):
     )
     assert (
         node.sql.sql()
-        == "IFF(CAST(ARRAY_CONTAINS(TO_VARIANT(cd_val), ARRAY(1, 2, 3)) IS NULL AS BOOLEAN), FALSE, "
-        "ARRAY_CONTAINS(TO_VARIANT(cd_val), ARRAY(1, 2, 3)))"
+        == f"CASE WHEN CAST(ARRAY_CONTAINS(TO_VARIANT({input_series_expr_str}), {array_expr_str}) IS NULL AS BOOLEAN) "
+        f"THEN FALSE ELSE ARRAY_CONTAINS(TO_VARIANT({input_series_expr_str}), {array_expr_str}) END"
     )
 
 
