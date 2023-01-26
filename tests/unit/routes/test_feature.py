58fc4d7862bf8554417419eb6af322bc4ad962b6
Kenny Chua <kenny@featurebyte.com>
2023-01-26 09:16:29 +0800

    fix lint

--- tests/unit/routes/test_feature.py
+++ tests/unit/routes/test_feature.py
@@ -5,7 +5,7 @@ import textwrap
 from collections import defaultdict
 from datetime import datetime
 from http import HTTPStatus
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 import numpy as np
 import pandas as pd
