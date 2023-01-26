7351eaa5ba2d4c0c4f0f79984920b8f419710ee0
Chester <chester@featurebyte.com>
2023-01-26 12:07:52 +0800

    [DEV-982] Beta sample notebook and introduction bootstrapped (#689)
    * adding notebook samples
    
    * adding samples to beta
    
    * adding creditcard model
    
    * renamed files and formatted markdown
    
    * copying resources directory
    
    * formatting fix

--- docker/entrypoint-featurebyte.sh
+++ docker/entrypoint-featurebyte.sh
@@ -21,28 +21,39 @@ jupyter-lab \
     --NotebookApp.ip='0.0.0.0' \
     --allow-root \
     --no-browser &
+
+JUPYTER_PID=$1
+
+# Soft link featurebyte config file
 ln -s /app/.featurebyte/config.yaml /app/.featurebyte/notebook/config.yaml
 
+echo "Writing additional samples in ~/.featurebyte/notebook/samples"
+mkdir -p /app/.featurebyte/notebook/samples  # This will not replace
+cp -nr /samples/* /app/.featurebyte/notebook/samples/  # Copy without replacement
+
 echo "Featurebyte Beta Server is Running"
-echo "┌────────────────────────┬───────────────────────────┐"
-echo "│Service                 │Connection Details         │"
-echo "├────────────────────────┼───────────────────────────┤"
-echo "│featurebyte-backend     │http://localhost:8088/     │"
-echo "├────────────────────────┼───────────────────────────┤"
-echo "│featurebyte-docs        │http://localhost:8089/     │"
-echo "├────────────────────────┼───────────────────────────┤"
-echo "│jupyterlab-notebook     │http://localhost:8090/     │"
-echo "└────────────────────────┴───────────────────────────┘"
+echo "┌────────────────────────┬─────────────────────────────────────────────────────────────┐"
+echo "│ Service                │ Connection Details                                          │"
+echo "├────────────────────────┼─────────────────────────────────────────────────────────────┤"
+echo "│ featurebyte-backend    │ http://localhost:8088/                                      │"
+echo "├────────────────────────┼─────────────────────────────────────────────────────────────┤"
+echo "│ featurebyte-docs       │ http://localhost:8089/                                      │"
+echo "├────────────────────────┼─────────────────────────────────────────────────────────────┤"
+echo "│ jupyterlab-notebook    │ http://localhost:8090/lab/tree/samples/instructions.ipynb   │"
+echo "└────────────────────────┴─────────────────────────────────────────────────────────────┘"
 echo ""
 echo "Persistent Configuration Files"
-echo "┌────────────────────────┬───────────────────────────┐"
-echo "│General Config Location │~/.featurebyte/config.yaml │"
-echo "├────────────────────────┼───────────────────────────┤"
-echo "│MongoDB Files           │~/.featurebyte/data        │"
-echo "├────────────────────────┴───────────────────────────┤"
-echo "│Jupyter Notebooks       │~/.featurebyte/notebook    │"
-echo "└────────────────────────┴───────────────────────────┘"
+echo "┌─────────────────────────┬────────────────────────────┐"
+echo "│ General Config Location │ ~/.featurebyte/config.yaml │"
+echo "├─────────────────────────┼────────────────────────────┤"
+echo "│ MongoDB Files           │ ~/.featurebyte/data        │"
+echo "├─────────────────────────┼────────────────────────────┤"
+echo "│ Jupyter Notebooks       │ ~/.featurebyte/notebook    │"
+echo "└─────────────────────────┴────────────────────────────┘"
 echo ""
 
-# start featurebyte service
+# start featurebyte service (Long running process, CTRL+C to stop)
 uvicorn featurebyte.app:app --host=$API_HOST --port=$API_PORT --workers=$WORKERS --timeout-keep-alive=300 --log-level=$LOG_LEVEL
+
+# Stop jupyter
+kill -TERM ${JUPYTER_PID}
