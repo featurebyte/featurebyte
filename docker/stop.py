3a2cef9fe96c8c6820172b0ff2856a3c5b420711
Chester <chester@featurebyte.com>
2023-02-02 13:34:30 +0800

    allow python2

--- docker/stop.py
+++ docker/stop.py
@@ -2,8 +2,10 @@ import subprocess
 
 if __name__ == "__main__":
     print("# Stopping featurebyte-beta")
-    subprocess.run("docker compose down".split(" "))
+    p = subprocess.Popen("docker compose down".split(" "))
+    p.wait()
 
     # Docker compose does not delete the container sometimes
     # It is okay if there is an error just making sure the containers are deleted
-    subprocess.run("docker container rm featurebyte-server featurebyte-docs".split(" "))
+    p = subprocess.Popen("docker container rm featurebyte-server featurebyte-docs".split(" "))
+    p.wait()
