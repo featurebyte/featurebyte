d8b2baa1323b0438abd6d02dd5933c8a539a9962
senbong <senbong@featurebyte.com>
2023-02-14 15:55:29 +0800

    Fix lint

--- tests/util/helper.py
+++ tests/util/helper.py
@@ -9,7 +9,6 @@ from featurebyte.api.database_table import AbstractTableData
 from featurebyte.core.generic import QueryObject
 from featurebyte.query_graph.enum import NodeOutputType, NodeType
 from featurebyte.query_graph.graph import QueryGraph
-from featurebyte.query_graph.transform.sdk_code import SDKCodeExtractor
 from featurebyte.query_graph.util import get_aggregation_identifier, get_tile_table_identifier
 
 
