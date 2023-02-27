fb6b010e530ae12a283b3dc7cabd412d53334180
Chester <chester@featurebyte.com>
2023-02-27 10:49:26 +0800

    import error

--- featurebyte/docker/migration.py
+++ featurebyte/docker/migration.py
@@ -9,6 +9,10 @@ from featurebyte.migration.run import run_migration
 from featurebyte.utils.credential import get_credential
 from featurebyte.utils.persistent import get_persistent
 
-logger.info("Running database migration")
-asyncio.run(run_migration(user=User(), persistent=get_persistent(), get_credential=get_credential))
-logger.info("Database migration completed")
+if __name__ == "__main__":
+    logger.info("Running database migration")
+    asyncio.run(
+        run_migration(user=User(), persistent=get_persistent(), get_credential=get_credential)
+    )
+
+    logger.info("Database migration completed")
