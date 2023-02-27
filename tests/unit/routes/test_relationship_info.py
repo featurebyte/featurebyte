4a0e4da69878527ca39cec2d728b4f2cd585ed41
Jevon Yeoh <jevon@featurebyte.com>
2023-02-28 00:24:20 +0800

    fix test

--- tests/unit/routes/test_relationship_info.py
+++ tests/unit/routes/test_relationship_info.py
@@ -1,6 +1,8 @@
 """
 Test relationship info routes
 """
+from bson import ObjectId
+
 from tests.unit.routes.base import BaseWorkspaceApiTestSuite
 
 
@@ -14,3 +16,16 @@ class TestRelationshipInfoApi(BaseWorkspaceApiTestSuite):
     payload = BaseWorkspaceApiTestSuite.load_payload(
         "tests/fixtures/request_payloads/relationship_info.json"
     )
+
+    def multiple_success_payload_generator(self, api_client):
+        """Create multiple payload for setting up create_multiple_success_responses fixture"""
+        _ = api_client
+        for i in range(3):
+            payload = self.payload.copy()
+            payload["_id"] = str(ObjectId())
+            payload["child_id"] = str(ObjectId())
+            payload["parent_id"] = str(ObjectId())
+            payload["child_data_source_id"] = str(ObjectId())
+            payload["updated_by"] = str(ObjectId())
+            payload["name"] = f'{self.payload["name"]}_{i}'
+            yield payload
