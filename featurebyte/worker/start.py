d64706232918231277d595dc8eb60f302e1ae426
Kenny Chua <kenny@featurebyte.com>
2023-03-03 14:48:17 +0800

    address reviews

--- featurebyte/worker/start.py
+++ featurebyte/worker/start.py
@@ -1,30 +1,6 @@
 """
 Start worker
 """
-from typing import Any
-
-from asyncio import get_event_loop
-
-from celery.signals import worker_process_shutdown
-
 from . import celery
-
-
-@worker_process_shutdown.connect
-def process_shutdown(*args: Any, **kwargs: Any) -> None:
-    """
-    Shutdown event loop
-
-    Parameters
-    ----------
-    args: Any
-        Arguments
-    kwargs: Any
-        Keyword arguments
-    """
-    _ = args, kwargs
-    loop = get_event_loop()
-    loop.close()
-
 
 celery.autodiscover_tasks(["featurebyte.worker.task_executor"])
