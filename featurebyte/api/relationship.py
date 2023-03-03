615c52d880e450379b6322468e341801b1e8ed27
Jevon Yeoh <jevon@featurebyte.com>
2023-03-03 13:57:31 +0800

    api/relationship: update docstrings (#846)
    Add docstrings for the new Relationship API object.

--- featurebyte/api/relationship.py
+++ featurebyte/api/relationship.py
@@ -8,6 +8,7 @@ from pydantic import Field
 from typeguard import typechecked
 
 from featurebyte.api.api_object import ApiObject
+from featurebyte.common.doc_util import FBAutoDoc
 from featurebyte.exception import RecordRetrievalException
 from featurebyte.models.base import PydanticObjectId
 from featurebyte.models.relationship import RelationshipInfo, RelationshipType
@@ -18,7 +19,24 @@ class Relationship(ApiObject):
     """
     The relationships class allows users to see explore what types of relationships exist between the various
     entities they have.
+
+    We currently support child-parent relationships that are a many-to-one relationship between entities.
+    These relationships are used to:
+
+    - recommend features from a parent for a use case
+    - recommend features recipes such as similarity between the child and the parent
+    - serve feature list
+
+    Relationships are automatically established when an entity is a primary key (or natural key) of a data and other
+    entities are tagged in this data. The entity that is the primary key (or natural key) is automatically set as
+    the child of the other entities. The data that maps the relationship is tagged in the relationship to facilitate
+    the serving of the features.
     """
+
+    __fbautodoc__ = FBAutoDoc(
+        section=["Relationship"],
+        proxy_class="featurebyte.Relationship",
+    )
 
     # class variables
     _route = "/relationship_info"
@@ -76,7 +94,10 @@ class Relationship(ApiObject):
         cls, include_id: Optional[bool] = True, relationship_type: Optional[Literal[tuple(RelationshipType)]] = None  # type: ignore[misc]
     ) -> pd.DataFrame:
         """
-        List a dataframe of the relationships. This provides a dataframe with:
+        List all relationships that exist in your FeatureByte instance, or filtered by relationship type.
+
+        This provides a dataframe with:
+
         - the relationship id
         - primary entity
         - relate dentity
@@ -98,6 +119,19 @@ class Relationship(ApiObject):
         -------
         pd.DataFrame
             A dataframe containing the relationships
+
+        Examples
+        --------
+        List all relationships
+
+        >>> import featurebyte as fb
+        >>> fb.Relationship.list()  # doctest: +SKIP
+
+
+        List all child-parent relationships
+
+        >>> import featurebyte as fb
+        >>> fb.Relationship.list(relationship_type="child_parent")  # doctest: +SKIP
         """
         list_responses = super().list(include_id=include_id)
         if relationship_type:
@@ -109,12 +143,27 @@ class Relationship(ApiObject):
     @typechecked
     def enable(self, enable: bool) -> None:
         """
-        Enable
+        Update the relationship to enable or disable it.
 
         Parameters
         ----------
         enable: bool
             Whether to enable or disable the relationship
+
+        Examples
+        --------
+        Enable a relationship
+
+        >>> import featurebyte as fb
+        >>> relationship = fb.Relationship.get_by_id(<relationship_id>)  # doctest: +SKIP
+        >>> relationship.enable(True)
+
+
+        Disable a relationship
+
+        >>> import featurebyte as fb
+        >>> relationship = fb.Relationship.get_by_id(<relationship_id>)  # doctest: +SKIP
+        >>> relationship.enable(False)
         """
 
         payload = RelationshipInfoUpdate(
