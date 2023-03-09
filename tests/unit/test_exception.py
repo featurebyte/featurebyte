cf4a949eab716304fb2a234136e1455dba49b1d4
senbong <senbong@featurebyte.com>
2023-03-09 10:47:30 +0800

    Fix response exception bug (#882)
    * Fix response exception bug
    
    * Revise fix

--- tests/unit/test_exception.py
+++ tests/unit/test_exception.py
@@ -3,6 +3,7 @@ Unit tests for feature/exception.py module
 """
 import json
 
+import pytest
 from requests import Response
 
 from featurebyte.exception import ResponseException
@@ -37,3 +38,25 @@ def test_response_exception__plain_text_response():
     assert str(response_exception) == "some error"
     assert response_exception.text == "some error"
     assert response_exception.status_code == 500
+
+
+@pytest.mark.parametrize("detail", ["Some details.", ["Some details."], ["Some", "details."]])
+def test_response_exception__response_detail(detail):
+    """Test response exception for plain text response"""
+    response = Response()
+    response._content = json.dumps({"detail": detail}).encode("utf-8")
+    response_exception = ResponseException(
+        response=response,
+        resolution=(
+            "\nIf the error is related to connection broken, "
+            "try to use a smaller `max_batch_size` parameter."
+        ),
+    )
+    with pytest.raises(ResponseException) as exc:
+        raise response_exception
+
+    expected = (
+        "Some details.\n"
+        "If the error is related to connection broken, try to use a smaller `max_batch_size` parameter."
+    )
+    assert expected in str(exc.value)
