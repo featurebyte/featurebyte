4fd41ce1d675f19022585e06f6f573c37d1ce177
yanhuisg <104881728+yanhuisg@users.noreply.github.com>
2023-03-17 18:26:30 +0800

    [DEV-1244 ] remove feature values for entities that are not in entity universe (#917)
    * remove feature values for entities that are not in entity universe
    
    * updated_at to be timestamp
    
    * invididual update_at for each feature

--- tests/integration/tile/spark/test_spark_schedule_feature_store.py
+++ tests/integration/tile/spark/test_spark_schedule_feature_store.py
@@ -3,6 +3,7 @@ This module contains integration tests for the stored procedure of online featur
 """
 from datetime import datetime
 
+import numpy as np
 import pytest
 
 from featurebyte.sql.spark.tile_schedule_online_store import TileScheduleOnlineStore
@@ -162,3 +163,58 @@ async def test_schedule_update_feature_store__insert_varchar_feature_column(
     result = await session.execute_query(sql)
     assert len(result) == 1
     assert result[feature_name].iloc[0] == "cat1"
+
+
+@pytest.mark.parametrize("source_type", ["spark"], indirect=True)
+@pytest.mark.asyncio
+async def test_schedule_online_feature_store__change_entity_universe(session, tile_task_prep_spark):
+    """
+    Test the stored procedure for updating feature store
+    """
+
+    tile_id, agg_id, feature_store_table_name, feature_name, entity_col_names = tile_task_prep_spark
+    date_ts_str = datetime.now().isoformat()[:-3] + "Z"
+
+    tile_online_store_ins = TileScheduleOnlineStore(
+        spark_session=session,
+        agg_id=agg_id,
+        job_schedule_ts_str=date_ts_str,
+    )
+    await tile_online_store_ins.execute()
+
+    sql = f"SELECT * FROM {feature_store_table_name} order by __FB_TILE_START_DATE_COLUMN"
+    result = await session.execute_query(sql)
+    assert len(result) == 2
+    assert result[feature_name].iloc[0] == 3
+    assert result["PRODUCT_ACTION"].iloc[0] == "view"
+    assert result[feature_name].iloc[1] == 6
+    assert result["PRODUCT_ACTION"].iloc[1] == "view"
+
+    new_select_sql = f"select {entity_col_names}, 100.0 as {feature_name} from TEMP_TABLE ORDER BY __FB_TILE_START_DATE_COLUMN ASC limit 2"
+    update_mapping_sql = f"""
+        UPDATE ONLINE_STORE_MAPPING
+        SET SQL_QUERY = '{new_select_sql}'
+        WHERE TILE_ID = '{tile_id}'
+"""
+    await session.execute_query(update_mapping_sql)
+
+    # sql = f"call SP_TILE_SCHEDULE_ONLINE_STORE('{agg_id}', '{date_ts_str}')"
+    # spark_session.sql(sql)
+    tile_online_store_ins = TileScheduleOnlineStore(
+        spark_session=session,
+        agg_id=agg_id,
+        job_schedule_ts_str=date_ts_str,
+    )
+    await tile_online_store_ins.execute()
+
+    sql = f"SELECT * FROM {feature_store_table_name} ORDER BY UPDATED_AT_{feature_name}"
+    result = await session.execute_query(sql)
+    assert len(result) == 4
+    assert np.isnan(result[feature_name].iloc[0])
+    assert result["PRODUCT_ACTION"].iloc[0] == "view"
+    assert np.isnan(result[feature_name].iloc[1])
+    assert result["PRODUCT_ACTION"].iloc[1] == "view"
+    assert result[feature_name].iloc[2] == 100
+    assert result["PRODUCT_ACTION"].iloc[2] == "action"
+    assert result[feature_name].iloc[3] == 100
+    assert result["PRODUCT_ACTION"].iloc[3] == "action"
