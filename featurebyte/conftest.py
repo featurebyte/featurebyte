05213c0bed5e50f093041d8473070dc27e684396
Jevon Yeoh <jevon@featurebyte.com>
2023-03-22 13:43:22 +0800

    [DEV-1272] api_object: update docs (#948)
    Update docstrings for get, get_by_id, info and save.

--- featurebyte/conftest.py
+++ featurebyte/conftest.py
@@ -15,6 +15,14 @@ def add_imports(doctest_namespace):
     doctest_namespace["fb"] = featurebyte
     doctest_namespace["pd"] = pandas
 
+    # get entity id
+    grocery_customer_entity = featurebyte.Entity.get("grocerycustomer")
+    doctest_namespace["grocery_customer_entity_id"] = grocery_customer_entity.id
+
+    # get feature id
+    feature = featurebyte.Feature.get("InvoiceCount_60days")
+    doctest_namespace["invoice_count_60_days_feature_id"] = feature.id
+
 
 @pytest.fixture(autouse=True)
 def activate_playground_catalog():
