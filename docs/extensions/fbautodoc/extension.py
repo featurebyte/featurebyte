451fabcd4d1badd4ba927d5b63405d283e4783ef
Jevon Yeoh <jevon@featurebyte.com>
2023-03-23 17:55:42 +0800

    fbautodoc/extension: hide table of contents

--- docs/extensions/fbautodoc/extension.py
+++ docs/extensions/fbautodoc/extension.py
@@ -569,6 +569,9 @@ class FBAutoDocProcessor(AutoDocProcessor):
 
             # populate resource path and name as autodoc title
             parent.clear()
+            style_elem = etree.SubElement(parent, "style")
+            style_elem.text = ".md-sidebar.md-sidebar--secondary { display: none; }"
+            # md-sidebar md-sidebar--secondary
             title_elem = etree.SubElement(parent, "h1")
             title_elem.set("class", "autodoc-overflow-wrap-break-word")
 
