2137bce7dba3980235b5e98a613abf8e55c110bb
Yung Siang Liau <yungsiang@featurebyte.com>
2023-03-23 09:30:01 +0800

    Add examples

--- featurebyte/core/accessor/datetime.py
+++ featurebyte/core/accessor/datetime.py
@@ -120,9 +120,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the year component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampYear"] = view["Timestamp"].dt.year
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampYear
+        0 2022-01-03 12:28:58           2022
+        1 2022-01-03 16:32:15           2022
+        2 2022-01-07 16:20:04           2022
+        3 2022-01-10 16:18:32           2022
+        4 2022-01-12 17:36:23           2022
         """
         return self._make_operation("year")
 
@@ -138,9 +146,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the quarter component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampQuarter"] = view["Timestamp"].dt.quarter
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampQuarter
+        0 2022-01-03 12:28:58                 1
+        1 2022-01-03 16:32:15                 1
+        2 2022-01-07 16:20:04                 1
+        3 2022-01-10 16:18:32                 1
+        4 2022-01-12 17:36:23                 1
         """
         return self._make_operation("quarter")
 
@@ -156,9 +172,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the month component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampMonth"] = view["Timestamp"].dt.month
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampMonth
+        0 2022-01-03 12:28:58               1
+        1 2022-01-03 16:32:15               1
+        2 2022-01-07 16:20:04               1
+        3 2022-01-10 16:18:32               1
+        4 2022-01-12 17:36:23               1
         """
         return self._make_operation("month")
 
@@ -174,9 +198,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the week component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampWeek"] = view["Timestamp"].dt.week
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampWeek
+        0 2022-01-03 12:28:58              1
+        1 2022-01-03 16:32:15              1
+        2 2022-01-07 16:20:04              1
+        3 2022-01-10 16:18:32              2
+        4 2022-01-12 17:36:23              2
         """
         return self._make_operation("week")
 
@@ -192,9 +224,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the day component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampDay"] = view["Timestamp"].dt.day
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampDay
+        0 2022-01-03 12:28:58             3
+        1 2022-01-03 16:32:15             3
+        2 2022-01-07 16:20:04             7
+        3 2022-01-10 16:18:32            10
+        4 2022-01-12 17:36:23            12
         """
         return self._make_operation("day")
 
@@ -213,8 +253,14 @@ class DatetimeAccessor:
         Examples
         --------
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
-        >>> view["TimestampDayOfWeek"] = view["Timestamp"].dt.hour
+        >>> view["TimestampDayOfWeek"] = view["Timestamp"].dt.day_of_week
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampDayOfWeek
+        0 2022-01-03 12:28:58                   0
+        1 2022-01-03 16:32:15                   0
+        2 2022-01-07 16:20:04                   4
+        3 2022-01-10 16:18:32                   0
+        4 2022-01-12 17:36:23                   2
         """
         return self._make_operation("day_of_week")
 
@@ -230,9 +276,17 @@ class DatetimeAccessor:
 
         Examples
         --------
+        Compute the hour component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampHour"] = view["Timestamp"].dt.hour
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampHour
+        0 2022-01-03 12:28:58             12
+        1 2022-01-03 16:32:15             16
+        2 2022-01-07 16:20:04             16
+        3 2022-01-10 16:18:32             16
+        4 2022-01-12 17:36:23             17
         """
         return self._make_operation("hour")
 
@@ -272,9 +326,18 @@ class DatetimeAccessor:
 
         Examples
         --------
+
+        Compute the second component of a timestamp column:
+
         >>> view = fb.Table.get("GROCERYINVOICE").get_view()
         >>> view["TimestampSecond"] = view["Timestamp"].dt.second
         >>> view.preview(5).filter(regex="Timestamp")
+                    Timestamp  TimestampSecond
+        0 2022-01-03 12:28:58             58.0
+        1 2022-01-03 16:32:15             15.0
+        2 2022-01-07 16:20:04              4.0
+        3 2022-01-10 16:18:32             32.0
+        4 2022-01-12 17:36:23             23.0
         """
         return self._make_operation("second")
 
