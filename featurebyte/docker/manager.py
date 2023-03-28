9e3429e89fee0b48082821f080ad6fa2ad999b81
Chester <chester@featurebyte.com>
2023-03-28 14:36:50 +0800

    (Backporting) [BUG] fixing windows import (#1016)
    * fixing windows import
    
    * remove the hard dependency from featurebyte to documentation
    
    * added sleep
    
    (cherry picked from commit 5f8ad0865c3738af1b84106a5d20796e09fb6f95)

--- featurebyte/docker/manager.py
+++ featurebyte/docker/manager.py
@@ -4,7 +4,6 @@ Featurebyte tools for managing docker containers
 from typing import Generator, List, Optional
 
 import os
-import pwd
 import sys
 import tempfile
 import time
@@ -107,6 +106,7 @@ def get_docker_client(app_name: ApplicationName) -> Generator[DockerClient, None
     with tempfile.TemporaryDirectory() as temp_dir:
         compose_env_file = os.path.join(temp_dir, ".env")
         if sys.platform != "win32":
+            import pwd  # pylint: disable=import-outside-toplevel
             uid = os.getuid()
             user = pwd.getpwuid(uid)
             with open(compose_env_file, "w", encoding="utf8") as file_obj:
@@ -276,9 +276,9 @@ def start_app(
                 os.environ["FEATUREBYTE_TELEMETRY_ID"] = ":".join(
                     [f"{(uuid.getnode() >> ele) & 0xff:02x}" for ele in range(0, 8 * 6, 8)][::-1]
                 )
+                time.sleep(2)  # environment variable did not get set fast enough for docker compose to obtain
             except Exception:  # pylint: disable=broad-except
                 pass
-
             docker.compose.up(services=get_service_names(app_name), detach=True)
 
             # Wait for all services to be healthy
