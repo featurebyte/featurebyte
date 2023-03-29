da49b8a27d496de55d65168bb5ed2b768fbfab07
senbong <senbong@featurebyte.com>
2023-03-29 16:51:03 +0800

    Update docstring & fix entity synchronization issue

--- featurebyte/api/entity.py
+++ featurebyte/api/entity.py
@@ -7,6 +7,8 @@ from typing import Any, List
 
 from http import HTTPStatus
 
+from bson import ObjectId
+from pydantic import Field
 from typeguard import typechecked
 
 from featurebyte.api.api_object import SavableApiObject
@@ -17,7 +19,7 @@ from featurebyte.models.entity import EntityModel, ParentEntity
 from featurebyte.schema.entity import EntityCreate, EntityUpdate
 
 
-class Entity(EntityModel, SavableApiObject):
+class Entity(SavableApiObject):
     """
     Entity class to represent an entity in FeatureByte.
 
@@ -27,10 +29,7 @@ class Entity(EntityModel, SavableApiObject):
     """
 
     # documentation metadata
-    __fbautodoc__ = FBAutoDoc(
-        section=["Entity"],
-        proxy_class="featurebyte.Entity",
-    )
+    __fbautodoc__ = FBAutoDoc(section=["Entity"], proxy_class="featurebyte.Entity")
 
     # class variables
     _route = "/entity"
@@ -39,21 +38,113 @@ class Entity(EntityModel, SavableApiObject):
     _get_schema = EntityModel
     _list_fields = ["name", "serving_names", "created_at"]
 
+    # pydantic instance variable (internal use)
+    internal_serving_names: List[str] = Field(alias="serving_names")
+
+    # pydantic instance variable
+    saved: bool = Field(
+        default=False,
+        allow_mutation=False,
+        exclude=True,
+        description="Flag to indicate whether the entity is saved in the FeatureByte catalog.",
+    )
+
     def _get_create_payload(self) -> dict[str, Any]:
-        data = EntityCreate(serving_name=self.serving_names[0], **self.json_dict())
+        data = EntityCreate(serving_name=self.serving_name, **self.json_dict())
         return data.json_dict()
+
+    def __getattribute__(self, item) -> Any:
+        """
+        Custom __getattribute__ method to handle the case when name of the entity is updated.
+
+        Parameters
+        ----------
+        item: str
+            Attribute name.
+
+        Returns
+        -------
+        Any
+            Attribute value.
+        """
+        if item == "name":
+            try:
+                return self.cached_model.name
+            except RecordRetrievalException:
+                pass
+        return super().__getattribute__(item)
+
+    @property
+    def serving_names(self) -> List[str]:
+        """
+        An entity's serving names is the name of the unique identifier that is used to identify the entity
+        during a preview or serving request. Typically, the serving name for an entity is the name of the
+        primary key (or natural key) of the table that represents the entity.
+
+        Returns
+        -------
+        List[str]
+            Serving names of the entity.
+
+        Examples
+        --------
+        >>> entity = catalog.get_entity("grocerycustomer")
+        >>> entity.serving_names
+        ['GROCERYCUSTOMERGUID']
+        """
+        try:
+            return self.cached_model.serving_names
+        except RecordRetrievalException:
+            return self.internal_serving_names
 
     @property
     def serving_name(self) -> str:
         """
-        Serving name of the entity.
+        First serving name of the entity serving names. An entity's serving names is the name of the unique
+        identifier that is used to identify the entity during a preview or serving request. Typically, the
+        serving name for an entity is the name of the primary key (or natural key) of the table that
+        represents the entity.
 
         Returns
         -------
         str
-            Serving name of the entity.
+            First serving name of the entity serving names.
         """
         return self.serving_names[0]
+
+    @property
+    def parents(self) -> List[ParentEntity]:
+        """
+        Get the list of parent entities. A parent-child relationship is a hierarchical connection that links one
+        entity (the child) to another entity (the parent). Each child entity key value can have only one parent
+        entity key value, but a parent entity key value can have multiple child entity key values.
+
+        Returns
+        -------
+        List[ParentEntity]
+            List of parent entities.
+
+        Examples
+        --------
+
+        >>> entity = catalog.get_entity("grocerycustomer")
+        >>> entity.parents  # doctest: +ELLIPSIS
+        [ParentEntity(id=ObjectId(...), table_type='scd_table', table_id=ObjectId(...))]
+        """
+        return self.cached_model.parents
+
+    @property
+    def ancestor_ids(self) -> List[ObjectId]:
+        """
+        Get the list of ancestor entity ids. An ancestor entity is an entity that is a parent of the current entity,
+        or a parent of a parent, and so on.
+
+        Returns
+        -------
+        List[ObjectId]
+            List of ancestor entity ids.
+        """
+        return self.cached_model.ancestor_ids
 
     @typechecked
     def update_name(self, name: str) -> None:
@@ -64,6 +155,26 @@ class Entity(EntityModel, SavableApiObject):
         ----------
         name: str
             New entity name.
+
+        Examples
+        --------
+        Update entity name:
+
+        >>> entity = catalog.get_entity(name="grocerycustomer")
+        >>> entity.update_name(name="customer")
+        >>> entity.name
+        'customer'
+        >>> entity.update_name(name="grocerycustomer")
+        >>> entity.name
+        'grocerycustomer'
+
+        Show the history of the entity name:
+
+        >>> entity.name_history  # doctest: +ELLIPSIS
+        [...,
+        {'created_at': ..., 'name': 'grocerycustomer'},
+        {'created_at': ..., 'name': 'customer'},
+        {'created_at': ..., 'name': 'grocerycustomer'}]
         """
         self.update(update_payload={"name": name}, allow_update_local=True)
 
@@ -76,6 +187,14 @@ class Entity(EntityModel, SavableApiObject):
         -------
         list[dict[str, Any]]
             History of the entity name.
+
+        Examples
+        --------
+        Get the history of the entity name:
+
+        >>> entity = catalog.get_entity(name="groceryproduct")
+        >>> entity.name_history  # doctest: +ELLIPSIS
+        [{'created_at': ..., 'name': 'groceryproduct'}]
         """
         return self._get_audit_history(field_name="name")
 
