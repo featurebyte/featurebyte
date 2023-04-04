6d7f06487e38aad869e038eb5ab92ef08c66fc49
Jevon Yeoh <jevon@featurebyte.com>
2023-04-04 15:04:18 +0800

    rm unused

--- featurebyte/common/documentation/gen_ref_pages_docs_builder.py
+++ featurebyte/common/documentation/gen_ref_pages_docs_builder.py
@@ -562,41 +562,6 @@ def populate_nav(nav: Nav, proxied_path_to_markdown_path: Dict[str, str]) -> Nav
     return nav
 
 
-def trim_docstring(docstring: Optional[str]) -> str:
-    """
-    Trim leading indent from a docstring.
-
-    See: https://www.python.org/dev/peps/pep-0257/#handling-docstring-indentation
-    """
-    if not docstring:
-        return ""
-
-    # Convert tabs to spaces (following the normal Python rules)
-    # and split into a list of lines:
-    lines = docstring.expandtabs().splitlines()
-    # Determine minimum indentation (first line doesn't count):
-    indent = 1000
-    for line in lines[1:]:
-        stripped = line.lstrip()
-        if stripped:
-            indent = min(indent, len(line) - len(stripped))
-
-    # Remove indentation (first line is special):
-    trimmed = [lines[0].strip()]
-    if indent < 1000:
-        for line in lines[1:]:
-            trimmed.append(line[indent:].rstrip())
-
-    # Strip off trailing and leading blank lines:
-    while trimmed and not trimmed[-1]:
-        trimmed.pop()
-    while trimmed and not trimmed[0]:
-        trimmed.pop(0)
-
-    # Return a single string:
-    return "\n".join(trimmed)
-
-
 class DocsBuilder:
     """
     DocsBuilder is a class to build the API docs.
