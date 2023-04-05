fb8bb71be14bca10e53d4f11654c5f542282617a
Jevon Yeoh <jevon@featurebyte.com>
2023-04-05 12:02:27 +0800

    fix import

--- tests/unit/api/test_catalog.py
+++ tests/unit/api/test_catalog.py
@@ -19,27 +19,23 @@ from bson import ObjectId
 from pandas.testing import assert_frame_equal
 from pydantic import ValidationError
 
-from featurebyte import (
-    DimensionTable,
-    EventTable,
-    EventView,
-    Feature,
-    FeatureJobSettingAnalysis,
-    FeatureList,
-    FeatureStore,
-    ItemTable,
-    PeriodicTask,
-    Relationship,
-    SCDTable,
-    Table,
-)
 from featurebyte.api.api_object import ApiObject, SavableApiObject
 from featurebyte.api.base_table import TableApiObject, TableListMixin
 from featurebyte.api.catalog import Catalog, update_and_reset_catalog
+from featurebyte.api.dimension_table import DimensionTable
 from featurebyte.api.entity import Entity
-from featurebyte.api.feature import FeatureNamespace
+from featurebyte.api.event_table import EventTable
+from featurebyte.api.event_view import EventView
+from featurebyte.api.feature import Feature, FeatureNamespace
 from featurebyte.api.feature_job import FeatureJobMixin
-from featurebyte.api.feature_list import FeatureListNamespace
+from featurebyte.api.feature_job_setting_analysis import FeatureJobSettingAnalysis
+from featurebyte.api.feature_list import FeatureList, FeatureListNamespace
+from featurebyte.api.feature_store import FeatureStore
+from featurebyte.api.item_table import ItemTable
+from featurebyte.api.periodic_task import PeriodicTask
+from featurebyte.api.relationship import Relationship
+from featurebyte.api.scd_table import SCDTable
+from featurebyte.api.table import Table
 from featurebyte.config import activate_catalog, get_active_catalog_id
 from featurebyte.exception import (
     DuplicatedRecordException,
