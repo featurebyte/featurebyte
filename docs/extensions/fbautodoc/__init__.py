d9aeaae8b1bde0462f49a1f6c7588203bed340cb
Jevon Yeoh <jevon@featurebyte.com>
2023-04-10 15:03:55 +0800

    mv markdown extension into fb code
    Make it easier to keep this consistent with the documentation repo.

--- docs/extensions/fbautodoc/__init__.py
+++ docs/extensions/fbautodoc/__init__.py
@@ -1,4 +1,7 @@
-from .extension import FBAutoDocExtension, makeExtension
+from featurebyte.common.documentation.markdown_extension.extension import (
+    FBAutoDocExtension,
+    makeExtension,
+)
 
 __version__ = "0.2.0"
 __all__ = ["FBAutoDocExtension", "makeExtension"]
