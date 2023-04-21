bb17f827bff8f3d454105cbb6d89736af83583e0
Jevon Yeoh <jevon@featurebyte.com>
2023-04-21 15:32:09 +0800

    docs: add and rearrange a few items, and update docstrings (#1154)
    * [DEV-1549] doc_layout: mv to class method
    
    * [DEV-1550]: add historical features async and status
    
    * [DEV-1552] add table.update_status
    
    * [DEV-1553] add featurejobsettinganalysis#get_by_id
    
    * [DEV-1548] update docstring for featurelist.update_feature_status
    
    * rm activate

--- featurebyte/common/documentation/custom_nav.py
+++ featurebyte/common/documentation/custom_nav.py
@@ -6,7 +6,6 @@ from typing import Any, Iterable, List, Mapping
 from mkdocs_gen_files import Nav  # type: ignore[attr-defined]
 
 from featurebyte.common.documentation.constants import (
-    ACTIVATE,
     ADD_METADATA,
     CATALOG,
     CLASS_METHODS,
@@ -79,7 +78,6 @@ class BetaWave3Nav(Nav):
     _custom_second_level_order = [
         CLASS_METHODS,
         TYPE,
-        ACTIVATE,
         LIST,
         GET,
         GET_VIEW,
