9f2645cfd4cb985e66837b666a0101f50dac6375
senbong <senbong@featurebyte.com>
2023-04-26 18:21:27 +0800

    Add test_info for historical feature table api object

--- tests/unit/api/test_historical_feature_table.py
+++ tests/unit/api/test_historical_feature_table.py
@@ -30,3 +30,24 @@ def test_list():
     assert df["name"].tolist() == ["my_historical_feature_table"]
     assert df["feature_store_name"].tolist() == ["sf_featurestore"]
     assert df["observation_table_name"].tolist() == ["observation_table_from_source_table"]
+
+
+def test_info(historical_feature_table):
+    """Test get historical feature table info"""
+    info_dict = historical_feature_table.info()
+    assert info_dict["table_details"]["table_name"].startswith("HISTORICAL_FEATURE_TABLE_")
+    assert isinstance(info_dict["feature_list_version"], str)
+    assert info_dict == {
+        "name": "my_historical_feature_table",
+        "feature_list_name": "feature_list_for_historical_feature_table",
+        "feature_list_version": info_dict["feature_list_version"],
+        "observation_table_name": "observation_table_from_source_table",
+        "table_details": {
+            "database_name": "sf_database",
+            "schema_name": "sf_schema",
+            "table_name": info_dict["table_details"]["table_name"],
+        },
+        "columns_info": [],
+        "created_at": info_dict["created_at"],
+        "updated_at": None,
+    }
