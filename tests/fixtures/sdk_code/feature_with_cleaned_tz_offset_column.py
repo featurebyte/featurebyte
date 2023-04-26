4d35f3a7766134ce50ee6ae49ee6882a38b794c5
Jevon Yeoh <jevon@featurebyte.com>
2023-04-27 01:28:37 +0800

    update fjs

--- tests/fixtures/sdk_code/feature_with_cleaned_tz_offset_column.py
+++ tests/fixtures/sdk_code/feature_with_cleaned_tz_offset_column.py
@@ -3,6 +3,7 @@ from bson import ObjectId
 from featurebyte import ColumnCleaningOperation
 from featurebyte import EventTable
 from featurebyte import MissingValueImputation
+from featurebyte import FeatureJobSetting
 
 event_table = EventTable.get_by_id(ObjectId("{table_id}"))
 event_view = event_table.get_view(
@@ -27,11 +28,11 @@ grouped = event_view.groupby(
     method="count",
     windows=["7d"],
     feature_names=["timestamp_hour_counts_7d"],
-    feature_job_setting={{
-        "blind_spot": "90s",
-        "frequency": "360s",
-        "time_modulo_frequency": "180s",
-    }},
+    feature_job_setting=FeatureJobSetting(
+        blind_spot="90s",
+        frequency="360s",
+        time_modulo_frequency="180s",
+    ),
     skip_fill_na=True,
 )
 feat = grouped["timestamp_hour_counts_7d"]
