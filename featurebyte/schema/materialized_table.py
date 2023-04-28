6d4fe07927e565499aafa4359fe1769ddbedb14c
Yung Siang Liau <yungsiang@featurebyte.com>
2023-04-28 17:53:08 +0800

    Check in missing file

---
+++ featurebyte/schema/materialized_table.py
@@ -0,0 +1,22 @@
+"""
+Schemas common to materialized table
+"""
+from typing import Any, Dict, Tuple
+
+from pydantic import root_validator
+
+from featurebyte.models.base import FeatureByteBaseDocumentModel
+
+
+class BaseMaterializedTableListRecord(FeatureByteBaseDocumentModel):
+    """
+    This model determines the schema when listing tables.
+    """
+
+    shape: Tuple[int, int]
+
+    @root_validator(pre=True)
+    @classmethod
+    def _extract_common_materialized_table_fields(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        values["shape"] = values["num_rows"], len(values["columns_info"])
+        return values
