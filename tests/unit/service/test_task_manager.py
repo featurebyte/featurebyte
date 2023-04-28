fb7407b0074939d6dc15196357ed2253790a0c13
Kenny Chua <kenny@featurebyte.com>
2023-04-28 17:29:14 +0800

    fix tests

--- tests/unit/service/test_task_manager.py
+++ tests/unit/service/test_task_manager.py
@@ -59,6 +59,8 @@ async def test_task_manager__long_running_tasks(task_manager, celery, user_id, p
                 "catalog_id": str(payload.catalog_id),
                 "output_collection_name": payload.output_collection_name,
                 "task_output_path": payload.task_output_path,
+                "task_type": "io_task",
+                "priority": 0,
             },
         )
 
