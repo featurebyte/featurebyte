536e1bd76a02186bb0fa9c75bdf7f3fee8200486
Yung Siang Liau <yungsiang@featurebyte.com>
2023-05-03 17:12:38 +0800

    Add typechecked for compute_historical_feature_table

--- featurebyte/api/feature_list.py
+++ featurebyte/api/feature_list.py
@@ -1045,6 +1045,7 @@ class FeatureList(
 
         return pd.concat(output, ignore_index=True)
 
+    @typechecked
     def compute_historical_feature_table(
         self,
         observation_table: Union[ObservationTable, pd.DataFrame],
