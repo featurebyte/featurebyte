9d15a895d40f36fd18201be04f86c6a4af22f533
Jevon Yeoh <jevon@featurebyte.com>
2023-05-04 14:28:57 +0800

    update

--- featurebyte/api/source_table.py
+++ featurebyte/api/source_table.py
@@ -330,7 +330,13 @@ class AbstractTableData(ConstructGraphMixin, FeatureByteBaseModel, ABC):
         Examples
         --------
         Sample 3 rows from the table.
-        >>> catalog.get_table("GROCERYPRODUCT").sample(3)
+        >>> catalog.get_table("GROCERYPRODUCT").sample(
+        ...   size=3,
+        ...   seed=111,
+        ...   from_timestamp=datetime(2019, 1, 1),
+        ...   to_timestamp=datetime(2023, 12, 31),
+        ...   after_cleaning=True,
+        ... )
                              GroceryProductGuid ProductGroup
         0  e890c5cb-689b-4caf-8e49-6b97bb9420c0       Épices
         1  5720e4df-2996-4443-a1bc-3d896bf98140         Chat
