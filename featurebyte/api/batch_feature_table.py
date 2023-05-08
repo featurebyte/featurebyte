b6bd9cf2883781acd36d50f6d41bbcc70dea1cc9
Jevon Yeoh <jevon@featurebyte.com>
2023-05-08 12:12:20 +0800

    [DEV-1666] add preview, sample, and describe to docs (#1271) (#1272)
    * [DEV-1666] add preview, sample, and describe to docs (#1271)
    
    * [DEV-1666] add preview, sample, and describe to docs
    
    * [DEV-1679] update docstring for join
    
    * lint
    
    * disable lint

--- featurebyte/api/batch_feature_table.py
+++ featurebyte/api/batch_feature_table.py
@@ -3,6 +3,8 @@ BatchFeatureTable class
 """
 from __future__ import annotations
 
+import pandas as pd
+
 from featurebyte.api.api_object import ApiObject, ForeignKeyMapping
 from featurebyte.api.batch_request_table import BatchRequestTable
 from featurebyte.api.feature_store import FeatureStore
@@ -33,3 +35,56 @@ class BatchFeatureTable(BatchFeatureTableModel, ApiObject, MaterializedTableMixi
         ForeignKeyMapping("feature_store_id", FeatureStore, "feature_store_name"),
         ForeignKeyMapping("batch_request_table_id", BatchRequestTable, "batch_request_table_name"),
     ]
+
+    def preview(self, limit: int = 10) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a selection of rows of the batch feature table.
+
+        Parameters
+        ----------
+        limit: int
+            Maximum number of return rows.
+
+        Returns
+        -------
+        pd.DataFrame
+            Preview rows of the table.
+        """
+        return super().preview(limit=limit)
+
+    def sample(self, size: int = 10, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a random selection of rows of the batch feature table based on a
+        specified size and seed for sampling control.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Sampled rows from the table.
+        """
+        return super().sample(size=size, seed=seed)
+
+    def describe(self, size: int = 0, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns descriptive statistics of the batch feature table.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample. If 0, all rows will be used.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Summary of the table.
+        """
+        return super().describe(size=size, seed=seed)
--- featurebyte/api/batch_request_table.py
+++ featurebyte/api/batch_request_table.py
@@ -1,7 +1,10 @@
 """
 BatchRequestTable class
 """
+# pylint: disable=duplicate-code
 from __future__ import annotations
+
+import pandas as pd
 
 from featurebyte.api.api_object import ApiObject, ForeignKeyMapping
 from featurebyte.api.feature_store import FeatureStore
@@ -31,3 +34,71 @@ class BatchRequestTable(BatchRequestTableModel, ApiObject, MaterializedTableMixi
     _list_foreign_keys = [
         ForeignKeyMapping("feature_store_id", FeatureStore, "feature_store_name"),
     ]
+
+    def to_pandas(self) -> pd.DataFrame:
+        """
+        Converts the batch request table to a pandas dataframe.
+
+        Returns
+        -------
+        pd.DataFrame
+
+        Examples
+        --------
+        >>> batch_request_table = catalog.get_batch_request_table("my_batch_request_table")  # doctest: +SKIP
+        >>> batch_request_table.to_pandas()  # doctest: +SKIP
+        """
+        return super().to_pandas()
+
+    def preview(self, limit: int = 10) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a selection of rows of the batch request table.
+
+        Parameters
+        ----------
+        limit: int
+            Maximum number of return rows.
+
+        Returns
+        -------
+        pd.DataFrame
+            Preview rows of the table.
+        """
+        return super().preview(limit=limit)
+
+    def sample(self, size: int = 10, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a random selection of rows of the batch request table based on a
+        specified size and seed for sampling control.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Sampled rows from the table.
+        """
+        return super().sample(size=size, seed=seed)
+
+    def describe(self, size: int = 0, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns descriptive statistics of the batch request table.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample. If 0, all rows will be used.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Summary of the table.
+        """
+        return super().describe(size=size, seed=seed)
