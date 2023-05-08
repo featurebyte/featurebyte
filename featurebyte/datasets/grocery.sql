c67d7cc315f712b9e3d751bd1356bba988ab848d
Kenny Chua <kenny@featurebyte.com>
2023-05-08 10:49:28 +0800

    update creditcard dataset (#1269)

--- featurebyte/datasets/grocery.sql
+++ featurebyte/datasets/grocery.sql
@@ -10,28 +10,26 @@ USING parquet OPTIONS (
     path '{staging_path}/GroceryCustomer.parquet'
 );
 CREATE TABLE GROCERY.__GROCERYCUSTOMER USING DELTA AS SELECT * FROM temp_table;
-CREATE OR REPLACE VIEW GROCERY.GROCERYCUSTOMER(
-    `RowID`,
-    `GroceryCustomerGuid`,
-    `ValidFrom`,
-    `Gender`,
-    `Title`,
-    `GivenName`,
-    `MiddleInitial`,
-    `Surname`,
-    `StreetAddress`,
-    `City`,
-    `State`,
-    `PostalCode`,
-    `BrowserUserAgent`,
-    `DateOfBirth`,
-    `Latitude`,
-    `Longitude`,
-    `record_available_at`,
-    `CurrentRecord`
-) as
-SELECT`RowID`, `GroceryCustomerGuid`, `ValidFrom`, `Gender`, `Title`, `GivenName`, `MiddleInitial`, `Surname`, `StreetAddress`, `City`, `State`, `PostalCode`, `BrowserUserAgent`, `DateOfBirth`, `Latitude`, `Longitude`, `record_available_at`,
-LAG(`ValidFrom`) OVER (PARTITION BY `GroceryCustomerGuid` ORDER BY `ValidFrom` DESC) IS NULL AS `CurrentRecord`
+CREATE OR REPLACE VIEW GROCERY.GROCERYCUSTOMER AS
+SELECT
+  `RowID`,
+  `GroceryCustomerGuid`,
+  `ValidFrom`,
+  `Gender`,
+  `Title`,
+  `GivenName`,
+  `MiddleInitial`,
+  `Surname`,
+  `StreetAddress`,
+  `City`,
+  `State`,
+  `PostalCode`,
+  `BrowserUserAgent`,
+  `DateOfBirth`,
+  `Latitude`,
+  `Longitude`,
+  `record_available_at`,
+  LAG(`ValidFrom`) OVER (PARTITION BY `GroceryCustomerGuid` ORDER BY `ValidFrom` DESC) IS NULL AS `CurrentRecord`
 FROM GROCERY.__GROCERYCUSTOMER
 WHERE `record_available_at` <= CURRENT_TIMESTAMP();
 
@@ -41,15 +39,15 @@ USING parquet OPTIONS (
     path '{staging_path}/GroceryInvoice.parquet'
 );
 CREATE TABLE GROCERY.__GROCERYINVOICE USING DELTA AS SELECT * FROM temp_table;
-CREATE OR REPLACE VIEW GROCERY.GROCERYINVOICE(
+CREATE OR REPLACE VIEW GROCERY.GROCERYINVOICE AS
+SELECT
     `GroceryInvoiceGuid`,
     `GroceryCustomerGuid`,
     `Timestamp`,
     `tz_offset`,
     `record_available_at`,
     `Amount`
-) as
-SELECT * FROM GROCERY.__GROCERYINVOICE
+FROM GROCERY.__GROCERYINVOICE
 WHERE `record_available_at` <= CURRENT_TIMESTAMP();
 
 -- populate InvoiceItems
@@ -58,7 +56,8 @@ USING parquet OPTIONS (
     path '{staging_path}/InvoiceItems.parquet'
 );
 CREATE TABLE GROCERY.__INVOICEITEMS USING DELTA AS SELECT * FROM temp_table;
-CREATE OR REPLACE VIEW GROCERY.INVOICEITEMS(
+CREATE OR REPLACE VIEW GROCERY.INVOICEITEMS AS
+SELECT
     `GroceryInvoiceItemGuid`,
     `GroceryInvoiceGuid`,
     `GroceryProductGuid`,
@@ -67,8 +66,7 @@ CREATE OR REPLACE VIEW GROCERY.INVOICEITEMS(
     `TotalCost`,
     `Discount`,
     `record_available_at`
-) as
-SELECT * FROM GROCERY.__INVOICEITEMS
+FROM GROCERY.__INVOICEITEMS
 WHERE `record_available_at` <= CURRENT_TIMESTAMP();
 
 -- populate GroceryProduct
