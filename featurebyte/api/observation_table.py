b04f3593e9e5daa96b1e26070908d37835060906
Jevon Yeoh <jevon@featurebyte.com>
2023-05-08 09:36:16 +0800

    [DEV-1666] add preview, sample, and describe to docs (#1271)
    * [DEV-1666] add preview, sample, and describe to docs
    
    * [DEV-1679] update docstring for join
    
    * lint

--- featurebyte/api/observation_table.py
+++ featurebyte/api/observation_table.py
@@ -1,7 +1,10 @@
 """
 ObservationTable class
 """
+# pylint: disable=duplicate-code
 from __future__ import annotations
+
+import pandas as pd
 
 from featurebyte.api.api_object import ApiObject, ForeignKeyMapping
 from featurebyte.api.feature_store import FeatureStore
@@ -31,3 +34,71 @@ class ObservationTable(ObservationTableModel, ApiObject, MaterializedTableMixin)
     _list_foreign_keys = [
         ForeignKeyMapping("feature_store_id", FeatureStore, "feature_store_name"),
     ]
+
+    def to_pandas(self) -> pd.DataFrame:
+        """
+        Converts the observation table to a pandas dataframe.
+
+        Returns
+        -------
+        pd.DataFrame
+
+        Examples
+        --------
+        >>> observation_table = catalog.get_observation_table("observation_table_name")  # doctest: +SKIP
+        >>> observation_table.to_pandas()  # doctest: +SKIP
+        """
+        return super().to_pandas()
+
+    def preview(self, limit: int = 10) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a selection of rows of the observation table.
+
+        Parameters
+        ----------
+        limit: int
+            Maximum number of return rows.
+
+        Returns
+        -------
+        pd.DataFrame
+            Preview rows of the table.
+        """
+        return super().preview(limit=limit)
+
+    def sample(self, size: int = 10, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns a DataFrame that contains a random selection of rows of the observation table based on a
+        specified size and seed for sampling control.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Sampled rows from the table.
+        """
+        return super().sample(size=size, seed=seed)
+
+    def describe(self, size: int = 0, seed: int = 1234) -> pd.DataFrame:
+        """
+        Returns descriptive statistics of the observation table.
+
+        Parameters
+        ----------
+        size: int
+            Maximum number of rows to sample. If 0, all rows will be used.
+        seed: int
+            Seed to use for random sampling.
+
+        Returns
+        -------
+        pd.DataFrame
+            Summary of the table.
+        """
+        return super().describe(size=size, seed=seed)
