c8de8cf29fef290f16e813b1c0dd8d0aaa97d3c1
Jevon Yeoh <jevon@featurebyte.com>
2023-05-08 14:40:38 +0800

    rm preview

--- featurebyte/core/accessor/feature_datetime.py
+++ featurebyte/core/accessor/feature_datetime.py
@@ -86,13 +86,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampYear"] = feature["Timestamp"].dt.year  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampYear
-        0 2022-01-03 12:28:58           2022
-        1 2022-01-03 16:32:15           2022
-        2 2022-01-07 16:20:04           2022
-        3 2022-01-10 16:18:32           2022
-        4 2022-01-12 17:36:23           2022
         """
         return super().year  # type: ignore[return-value]
 
@@ -112,13 +105,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampQuarter"] = feature["Timestamp"].dt.quarter  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampQuarter
-        0 2022-01-03 12:28:58                 1
-        1 2022-01-03 16:32:15                 1
-        2 2022-01-07 16:20:04                 1
-        3 2022-01-10 16:18:32                 1
-        4 2022-01-12 17:36:23                 1
         """
         return super().quarter  # type: ignore[return-value]
 
@@ -138,13 +124,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampMonth"] = feature["Timestamp"].dt.month  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampMonth
-        0 2022-01-03 12:28:58               1
-        1 2022-01-03 16:32:15               1
-        2 2022-01-07 16:20:04               1
-        3 2022-01-10 16:18:32               1
-        4 2022-01-12 17:36:23               1
         """
         return super().month  # type: ignore[return-value]
 
@@ -164,13 +143,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampWeek"] = feature["Timestamp"].dt.week  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampWeek
-        0 2022-01-03 12:28:58              1
-        1 2022-01-03 16:32:15              1
-        2 2022-01-07 16:20:04              1
-        3 2022-01-10 16:18:32              2
-        4 2022-01-12 17:36:23              2
         """
         return super().week  # type: ignore[return-value]
 
@@ -193,13 +165,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampDay"] = feature["Timestamp"].dt.day  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampDay
-        0 2022-01-03 12:28:58             3
-        1 2022-01-03 16:32:15             3
-        2 2022-01-07 16:20:04             7
-        3 2022-01-10 16:18:32            10
-        4 2022-01-12 17:36:23            12
 
 
         Compute the interval since the previous event in terms of days:
@@ -207,13 +172,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["PreviousTimestamp"] = feature["Timestamp"].lag("GroceryCustomerGuid")  # doctest: +SKIP
         >>> feature["DaysSincePreviousTimestamp"] = (feature["Timestamp"] - feature["PreviousTimestamp"]).dt.day  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp|Customer")  # doctest: +SKIP
-                            GroceryCustomerGuid           Timestamp   PreviousTimestamp  DaysSincePreviousTimestamp
-        0  007a07da-1525-49be-94d1-fc7251f46a66 2022-01-07 12:02:17                 NaT                         NaN
-        1  007a07da-1525-49be-94d1-fc7251f46a66 2022-01-11 19:46:41 2022-01-07 12:02:17                    4.322500
-        2  007a07da-1525-49be-94d1-fc7251f46a66 2022-02-04 13:06:35 2022-01-11 19:46:41                   23.722153
-        3  007a07da-1525-49be-94d1-fc7251f46a66 2022-03-09 18:51:16 2022-02-04 13:06:35                   33.239363
-        4  007a07da-1525-49be-94d1-fc7251f46a66 2022-03-15 15:08:34 2022-03-09 18:51:16                    5.845347
         """
         return super().day  # type: ignore[return-value]
 
@@ -233,13 +191,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
         --------
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampDayOfWeek"] = feature["Timestamp"].dt.day_of_week  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampDayOfWeek
-        0 2022-01-03 12:28:58                   0
-        1 2022-01-03 16:32:15                   0
-        2 2022-01-07 16:20:04                   4
-        3 2022-01-10 16:18:32                   0
-        4 2022-01-12 17:36:23                   2
         """
         return super().day_of_week  # type: ignore[return-value]
 
@@ -262,13 +213,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampHour"] = feature["Timestamp"].dt.hour  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampHour
-        0 2022-01-03 12:28:58             12
-        1 2022-01-03 16:32:15             16
-        2 2022-01-07 16:20:04             16
-        3 2022-01-10 16:18:32             16
-        4 2022-01-12 17:36:23             17
         """
         return super().hour  # type: ignore[return-value]
 
@@ -291,13 +235,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampMinute"] = feature["Timestamp"].dt.minute  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampMinute
-        0 2022-01-03 12:28:58               28
-        1 2022-01-03 16:32:15               32
-        2 2022-01-07 16:20:04               20
-        3 2022-01-10 16:18:32               18
-        4 2022-01-12 17:36:23               36
         """
         return super().minute  # type: ignore[return-value]
 
@@ -321,13 +258,6 @@ class FeatureDatetimeAccessor(DatetimeAccessor):
 
         >>> feature = catalog.get_feature("InvoiceCount_60days")
         >>> feature["TimestampSecond"] = feature["Timestamp"].dt.second  # doctest: +SKIP
-        >>> feature.preview(5).filter(regex="Timestamp")  # doctest: +SKIP
-                    Timestamp  TimestampSecond
-        0 2022-01-03 12:28:58             58.0
-        1 2022-01-03 16:32:15             15.0
-        2 2022-01-07 16:20:04              4.0
-        3 2022-01-10 16:18:32             32.0
-        4 2022-01-12 17:36:23             23.0
         """
         return super().second  # type: ignore[return-value]
 
