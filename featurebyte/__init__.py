bc6baf8a7693293927a510b4caeace2ecec9e93d
Jevon Yeoh <jevon@featurebyte.com>
2023-05-15 11:43:20 +0800

    mv and add list_unsaved_features

--- featurebyte/__init__.py
+++ featurebyte/__init__.py
@@ -1,7 +1,6 @@
 """Python Library for FeatureOps"""
 from typing import Any, List, Optional
 
-import inspect
 import sys
 from http import HTTPStatus
 
@@ -51,6 +50,7 @@ from featurebyte.exception import (
     InvalidSettingsError,
     RecordRetrievalException,
 )
+from featurebyte.feature_utility import list_unsaved_features
 from featurebyte.logging import get_logger
 from featurebyte.models.credential import (
     AccessTokenCredential,
@@ -366,97 +366,6 @@ def list_deployments(
     return output_df.drop(columns=["id"])
 
 
-def list_unsaved_features() -> pd.DataFrame:
-    """
-    List all unsaved features in the current session.
-
-    Returns
-    -------
-    pd.DataFrame
-        List of unsaved features
-
-    Examples
-    --------
-    >>> customer_gender = catalog.get_view("GROCERYCUSTOMER")["Gender"].as_feature(
-    ...     feature_name="Customer Gender"
-    ... )
-    >>> fb.list_unsaved_features()[["variable_name", "name", "catalog", "active_catalog"]]
-         variable_name             name  catalog  active_catalog
-    0  customer_gender  Customer Gender  grocery            True
-    """
-    processed_variables = set()
-    unsaved_features = []
-    client = Configurations().get_client()
-
-    def _is_saved(feature: Feature) -> bool:
-        """
-        Check if a feature is saved.
-
-        Parameters
-        ----------
-        feature: Feature
-            Feature to check
-
-        Returns
-        -------
-        bool
-        """
-        response = client.get(
-            url=f"/feature/{feature.id}", headers={"active-catalog-id": str(feature.catalog_id)}
-        )
-        if response.status_code == HTTPStatus.OK:
-            return True
-        return False
-
-    # get list of frame info from the current call stack
-    call_stack = inspect.stack()
-    # skip the first frame, which is the current function, to get the caller's frame
-    caller_frame_info = call_stack[1]
-    # check caller's local variables first, then global variables for unsaved features
-    caller_variables = [caller_frame_info.frame.f_locals, caller_frame_info.frame.f_globals]
-    for variables in caller_variables:
-        for var_name, var_obj in variables.items():
-            # global variable may be overriden by local variable
-            if var_name in processed_variables:
-                continue
-            if isinstance(var_obj, Feature) and not _is_saved(var_obj):
-                unsaved_features.append(
-                    {
-                        "object_id": str(var_obj.id),
-                        "variable_name": var_name,
-                        "name": var_obj.name,
-                        "catalog_id": var_obj.catalog_id,
-                    }
-                )
-            elif isinstance(var_obj, BaseFeatureGroup):
-                for name, feature in var_obj.feature_objects.items():
-                    if not _is_saved(feature):
-                        unsaved_features.append(
-                            {
-                                "object_id": str(feature.id),
-                                "variable_name": f'{var_name}["{name}"]',
-                                "name": feature.name,
-                                "catalog_id": feature.catalog_id,
-                            }
-                        )
-            processed_variables.add(var_name)
-
-    if unsaved_features:
-        catalogs = Catalog.list(include_id=True)
-        return (
-            pd.DataFrame(unsaved_features)
-            .merge(
-                catalogs.rename({"name": "catalog", "active": "active_catalog"}, axis=1),
-                left_on="catalog_id",
-                right_on="id",
-                how="left",
-            )[["object_id", "variable_name", "name", "catalog", "active_catalog"]]
-            .sort_values("object_id")
-            .reset_index(drop=True)
-        )
-    return pd.DataFrame(columns=["object_id", "variable_name", "name", "catalog", "active_catalog"])
-
-
 __all__ = [
     # API objects
     "BatchFeatureTable",
@@ -520,6 +429,8 @@ __all__ = [
     "start",
     "stop",
     "playground",
+    # utility
+    "list_unsaved_features",
 ]
 
 
