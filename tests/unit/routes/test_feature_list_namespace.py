a9063906a01e7e17c83d6017d6f7275f07e000fe
senbong <senbong@featurebyte.com>
2023-05-17 14:30:48 +0800

    Fix failed tests

--- tests/unit/routes/test_feature_list_namespace.py
+++ tests/unit/routes/test_feature_list_namespace.py
@@ -47,7 +47,9 @@ class TestFeatureListNamespaceApi(BaseCatalogApiTestSuite):
         self, test_api_client_persistent, user_id
     ):  # pylint: disable=arguments-differ
         """Post route success response object"""
-        _, persistent = test_api_client_persistent
+        api_client, persistent = test_api_client_persistent
+        self._save_entity(api_client, DEFAULT_CATALOG_ID)
+
         user = Mock()
         user.id = user_id
         feature_list_namespace_service = FeatureListNamespaceService(
@@ -85,7 +87,6 @@ class TestFeatureListNamespaceApi(BaseCatalogApiTestSuite):
         """Setup for get_info route testing"""
         api_object_filename_pairs = [
             ("feature_store", "feature_store"),
-            ("entity", "entity"),
             ("event_table", "event_table"),
             ("feature", "feature_sum_2h"),
             ("feature", "feature_sum_30m"),
@@ -95,12 +96,22 @@ class TestFeatureListNamespaceApi(BaseCatalogApiTestSuite):
             response = api_client.post(f"/{api_object}", json=payload)
             assert response.status_code == HTTPStatus.CREATED
 
+    def _save_entity(self, api_client, catalog_id):
+        """Save entity"""
+        payload = self.load_payload("tests/fixtures/request_payloads/entity.json")
+        response = api_client.post(
+            f"/entity", json=payload, headers={"active-catalog-id": str(catalog_id)}
+        )
+        assert response.status_code == HTTPStatus.CREATED
+
     @pytest_asyncio.fixture
     async def create_multiple_success_responses(
         self, test_api_client_persistent, user_id
     ):  # pylint: disable=arguments-differ
         """Post multiple success responses"""
         test_api_client, persistent = test_api_client_persistent
+        self._save_entity(test_api_client, catalog_id=DEFAULT_CATALOG_ID)
+
         user = Mock()
         user.id = user_id
         feature_list_namespace_service = FeatureListNamespaceService(
@@ -295,7 +306,9 @@ class TestFeatureListNamespaceApi(BaseCatalogApiTestSuite):
         self, test_api_client_persistent, user_id, catalog_id
     ):  # pylint: disable=arguments-differ
         """Post route success response object"""
-        _, persistent = test_api_client_persistent
+        test_api_client, persistent = test_api_client_persistent
+        self._save_entity(test_api_client, catalog_id)
+
         user = Mock()
         user.id = user_id
         feature_list_namespace_service = FeatureListNamespaceService(
