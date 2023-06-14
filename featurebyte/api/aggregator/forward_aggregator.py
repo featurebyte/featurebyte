6446541750ef0ce0caf3fc30ed89f407dfd75da5
Jevon Yeoh <jevon@featurebyte.com>
2023-06-14 16:35:16 +0800

    add target

--- featurebyte/api/aggregator/forward_aggregator.py
+++ featurebyte/api/aggregator/forward_aggregator.py
@@ -1,13 +1,16 @@
 """
 Forward aggregator module
 """
-from typing import Any, List, Optional, Type
+from typing import Any, List, Optional, Type, cast
 
-from featurebyte import ChangeView, EventView, ItemView
+from featurebyte import AggFunc, ChangeView, EventView, ItemView
 from featurebyte.api.aggregator.base_aggregator import BaseAggregator
+from featurebyte.api.target import Target
 from featurebyte.api.view import View
 from featurebyte.common.model_util import parse_duration_string
+from featurebyte.query_graph.node.agg_func import construct_agg_func
 from featurebyte.query_graph.node.generic import GroupByNode
+from featurebyte.query_graph.transform.reconstruction import add_pruning_sensitive_operation
 
 
 class ForwardAggregator(BaseAggregator):
@@ -23,17 +26,19 @@ class ForwardAggregator(BaseAggregator):
     def aggregation_method_name(self) -> str:
         return "forward_aggregate"
 
-    def aggregate_over(
+    def forward_aggregate(
         self,
         value_column: Optional[str] = None,
         method: Optional[str] = None,
         horizon: Optional[str] = None,
         target_name: Optional[str] = None,
-    ) -> None:
+    ) -> Target:
+        # Validation
         self._validate_parameters(
             value_column=value_column, method=method, horizon=horizon, target_name=target_name
         )
 
+        # Create new node
         node_params = self._prepare_node_parameters(
             value_column=value_column, method=method, horizon=horizon, target_name=target_name
         )
@@ -43,24 +48,14 @@ class ForwardAggregator(BaseAggregator):
             node_params=node_params,
             input_node=self.view.node,
         )
-        assert isinstance(feature_names, list)
-        assert method is not None
-        agg_method = construct_agg_func(agg_func=cast(AggFunc, method))
 
-        items = []
-        for feature_name in feature_names:
-            feature = self._project_feature_from_groupby_node(
-                agg_method=agg_method,
-                feature_name=feature_name,
-                groupby_node=groupby_node,
-                method=method,
-                value_column=value_column,
-                fill_value=fill_value,
-                skip_fill_na=skip_fill_na,
-            )
-            items.append(feature)
-        feature_group = FeatureGroup(items)
-        return feature_group
+        # Build and return Target
+        return Target(
+            name=target_name,
+            entity_ids=self.entity_ids,
+            horizon=horizon,
+            blind_spot="",
+        )
 
     def _prepare_node_parameters(
         self,
