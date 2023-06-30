c4526f4826ebb3124834f110d918f8403259e968
Jevon Yeoh <jevon@featurebyte.com>
2023-06-30 11:17:49 +0800

    Try

--- tests/integration/service/test_working_schema_service.py
+++ tests/integration/service/test_working_schema_service.py
@@ -54,7 +54,7 @@ def migration_service_fixture(app_container, get_cred):
     """
     Fixture for DataWarehouseMigrationServiceV8
     """
-    service = app_container.data_warehouse_migration_service_v8
+    service = app_container.data_warehouse_migration_service_v_8
     service.set_credential_callback(get_cred)
     service.set_celery(get_celery())
     return service
