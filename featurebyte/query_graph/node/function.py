00ae84d07df68f83f3df395818313cb7bde4e03a
senbong <senbong@featurebyte.com>
2023-06-30 13:14:49 +0800

    Address comment

--- featurebyte/query_graph/node/function.py
+++ featurebyte/query_graph/node/function.py
@@ -2,7 +2,10 @@
 This module contains generic function related node classes
 """
 # DO NOT include "from __future__ import annotations" as it will trigger issue for pydantic model nested definition
-from typing import Any, Dict, List, Literal, Optional, Sequence, Tuple
+from typing import Any, Dict, List, Literal, Optional, Sequence, Tuple, Union
+from typing_extensions import Annotated
+
+from abc import abstractmethod  # pylint: disable=wrong-import-order
 
 from pydantic import BaseModel, Field
 
@@ -22,6 +25,8 @@ from featurebyte.query_graph.node.metadata.sdk_code import (
     CodeGenerationConfig,
     CodeGenerationContext,
     ExpressionStr,
+    InfoDict,
+    RightHandSide,
     StatementT,
     ValueStr,
     VariableNameGenerator,
@@ -29,13 +34,86 @@ from featurebyte.query_graph.node.metadata.sdk_code import (
 )
 
 
-class FunctionParameterInput(BaseModel):
-    """FunctionParameterInput class"""
+class BaseFunctionParameterInput(BaseModel):
+    """BaseFunctionParameterInput class"""
+
+    dtype: DBVarType
+
+    @abstractmethod
+    def get_column_args(self) -> List[Optional[str]]:
+        """
+        Get column arguments (column name or None) for the function parameter input.
+        If the input is a value, return empty list.
+
+        Returns
+        _______
+        List[Optional[str]]
+        """
+
+    @abstractmethod
+    def get_sdk_function_argument(
+        self, node_inputs: List[VarNameExpressionInfo], node_input_index: int
+    ) -> Tuple[RightHandSide, int]:
+        """
+        Get SDK function argument for the function parameter input.
+
+        Parameters
+        ----------
+        node_inputs : List[VarNameExpressionInfo]
+            List of node inputs.
+        node_input_index : int
+            The index of the node input that the function parameter input is associated with.
+
+        Returns
+        -------
+        Tuple[RightHandSide, int]
+            Tuple of SDK function argument and the updated node input index.
+        """
+
+
+class ValueFunctionParameterInput(BaseFunctionParameterInput):
+    """ValueFunctionParameterInput class"""
 
     value: Optional[Any]
-    dtype: DBVarType
+    input_form: Literal[FunctionParameterInputForm.VALUE] = Field(
+        FunctionParameterInputForm.VALUE, const=True
+    )
+
+    def get_column_args(self) -> List[Optional[str]]:
+        return []
+
+    def get_sdk_function_argument(
+        self, node_inputs: List[VarNameExpressionInfo], node_input_index: int
+    ) -> Tuple[RightHandSide, int]:
+        # do not increment node_input_index as this is a value input, it should not increment the node_input_index
+        return ValueStr.create(self.value), node_input_index
+
+
+class ColumnFunctionParameterInput(BaseFunctionParameterInput):
+    """ColumnFunctionParameterInput class"""
+
     column_name: Optional[str]
-    input_form: FunctionParameterInputForm
+    input_form: Literal[FunctionParameterInputForm.COLUMN] = Field(
+        FunctionParameterInputForm.COLUMN, const=True
+    )
+
+    def get_column_args(self) -> List[Optional[str]]:
+        return [self.column_name]
+
+    def get_sdk_function_argument(
+        self, node_inputs: List[VarNameExpressionInfo], node_input_index: int
+    ) -> Tuple[RightHandSide, int]:
+        # InfoDict is not expected here as it should be used only in (ConditionNode - AssignNode) structure
+        value = node_inputs[node_input_index]
+        assert not isinstance(value, InfoDict), "Unexpected InfoDict type"
+        # increment node_input_index as this is a column input, it should increment the node_input_index
+        return value, node_input_index + 1
+
+
+FunctionParameterInput = Annotated[
+    Union[ValueFunctionParameterInput, ColumnFunctionParameterInput],
+    Field(discriminator="input_form"),
+]
 
 
 class GenericFunctionNodeParameters(BaseModel):
@@ -56,8 +134,7 @@ class GenericFunctionNode(BaseSeriesOutputNode):
     def _get_column_function_args(self) -> List[Optional[str]]:
         column_input_args = []
         for func_arg in self.parameters.function_parameters:
-            if func_arg.input_form == FunctionParameterInputForm.COLUMN:
-                column_input_args.append(func_arg.column_name)
+            column_input_args.extend(func_arg.get_column_args())
         return column_input_args
 
     @property
@@ -138,13 +215,12 @@ class GenericFunctionNode(BaseSeriesOutputNode):
         context: CodeGenerationContext,
     ) -> Tuple[List[StatementT], VarNameExpressionInfo]:
         function_parameters: List[Any] = []
-        node_input_count = 0
+        node_input_index = 0
         for func_param in self.parameters.function_parameters:
-            if func_param.input_form == FunctionParameterInputForm.COLUMN:
-                function_parameters.append(node_inputs[node_input_count])
-                node_input_count += 1
-            else:
-                function_parameters.append(ValueStr.create(func_param.value))
+            func_param_val, node_input_index = func_param.get_sdk_function_argument(
+                node_inputs, node_input_index
+            )
+            function_parameters.append(func_param_val)
 
         function_parameters = [str(arg) for arg in function_parameters]
         expression = f"{self.parameters.function_name}({', '.join(function_parameters)})"
