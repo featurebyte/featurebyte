986dfb7a360ae086df951eeba3965658184a21e4
Yung Siang Liau <yungsiang@featurebyte.com>
2023-07-07 01:12:19 +0800

    Avoid using SHOW COLUMNS for spark 3.2.4 support

--- featurebyte/sql/base.py
+++ featurebyte/sql/base.py
@@ -69,40 +69,6 @@ class BaseSqlModel(BaseModel):
 
         return f"{left_expr} <=> {right_expr}"
 
-    @property
-    def schema_column_name(self) -> str:
-        """
-        Column name for schema based on session type
-
-        Returns
-        -------
-            column name for schema
-        """
-        if isinstance(self._session, SnowflakeSession):
-            return "column_name"
-
-        return "col_name"
-
-    async def get_table_columns(self, table_name: str) -> List[str]:
-        """
-        Get column names for a table. The column names are returned in upper case.
-
-        Parameters
-        ----------
-        table_name: str
-            input table name
-
-        Returns
-        -------
-            list of column names in upper case
-        """
-        cols_df = await self._session.execute_query(f"SHOW COLUMNS IN {table_name}")
-        cols = []
-        if cols_df is not None:
-            for _, row in cols_df.iterrows():
-                cols.append(row[self.schema_column_name].upper())
-        return cols
-
     async def table_exists(self, table_name: str) -> bool:
         """
         Check if table exists
