982f0401c42a786267bee4c84d7f384c3d8f93a1
Jevon Yeoh <jevon@featurebyte.com>
2023-07-11 02:03:04 +0800

    rename

--- tests/integration/api/test_target_correctness.py
+++ tests/integration/api/test_target_correctness.py
@@ -184,7 +184,7 @@ def test_forward_aggregate(
         )
 
         # Transform and sample to get a smaller sample dataframe just for preview
-        transformed_expected_values = transform_and_sample_observation_set(expected_values)
+        preview_expected_values = transform_and_sample_observation_set(expected_values)
 
         # Build actual Target preview results
         target = event_view.groupby(entity_column_name).forward_aggregate(
@@ -193,10 +193,10 @@ def test_forward_aggregate(
             horizon=target_parameter.horizon,
             target_name=target_parameter.target_name,
         )
-        results = target.preview(transformed_expected_values[["POINT_IN_TIME", "üser id"]])
+        results = target.preview(preview_expected_values[["POINT_IN_TIME", "üser id"]])
 
         # Compare actual preview, against sampled results
-        fb_assert_frame_equal(results, transformed_expected_values)
+        fb_assert_frame_equal(results, preview_expected_values)
 
         # Build full materialized Target
         df_targets = target.compute_target(
