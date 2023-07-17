c4335ee95b73021d239d844e7726d7098858712b
Jevon Yeoh <jevon@featurebyte.com>
2023-07-17 16:20:55 +0800

    refactor pydantic overrides

--- featurebyte/common/documentation/pydantic_field_docs.py
+++ featurebyte/common/documentation/pydantic_field_docs.py
@@ -6,6 +6,7 @@ from typing import Dict
 CATALOG_ID = "catalog_id"
 CREATED_AT = "created_at"
 DTYPE = "dtype"
+ENTITY_IDS = "entity_ids"
 FEATURE_STORE = "feature_store"
 ID = "id"
 NAME = "name"
@@ -32,6 +33,10 @@ def _get_catalog_id_docstring_override(class_name: str) -> str:
     return f"Returns the unique identifier (ID) of the Catalog that is associated with the {class_name} object."
 
 
+def _get_dtype_docstring_override(class_name: str) -> str:
+    return f"Returns the data type of the {class_name}."
+
+
 def _get_doc_overrides(class_name: str) -> Dict[str, str]:
     """
     Get the docstring overrides for a class.
@@ -54,6 +59,28 @@ def _get_doc_overrides(class_name: str) -> Dict[str, str]:
     }
 
 
+def _get_target_or_feature_overrides(class_name: str) -> Dict[str, str]:
+    """
+    Get the docstring overrides for a Target or Feature class.
+
+    Parameters
+    ----------
+    class_name : str
+        The name of the class.
+
+    Returns
+    -------
+    Dict[str, str]
+        The docstring overrides for the class.
+    """
+    return {
+        **_get_doc_overrides(class_name),
+        CATALOG_ID: _get_catalog_id_docstring_override(class_name),
+        DTYPE: _get_dtype_docstring_override(class_name),
+        ENTITY_IDS: f"Returns a list of entity IDs that are linked to the {class_name}.",
+    }
+
+
 pydantic_field_doc_overrides = {
     "BatchFeatureTable": _get_doc_overrides("BatchFeatureTable"),
     "BatchRequestTable": _get_doc_overrides("BatchRequestTable"),
@@ -63,22 +90,18 @@ pydantic_field_doc_overrides = {
         "disguised_values list.",
     },
     "Entity": _get_doc_overrides("Entity"),
+    "Target": _get_target_or_feature_overrides("Target"),
     "Feature": {
-        CATALOG_ID: _get_catalog_id_docstring_override("Feature"),
-        CREATED_AT: _get_created_at_docstring_override("Feature"),
-        DTYPE: "Returns the data type of the Feature.",
-        "entity_ids": "Returns a list of entity IDs that are linked to the feature.",
+        **_get_target_or_feature_overrides("Feature"),
         "feature_list_ids": "Returns a list of IDs of feature lists that include the specified feature version.",
         "feature_namespace_id": "Returns the unique identifier (ID) of the feature namespace of the specified feature. "
         "All feature versions of a feature have the same feature namespace ID.",
-        ID: _get_id_docstring_override("Feature"),
         NAME: "The 'name' property of a Feature object serves to identify the feature namespace and can be used to "
         "retrieve or set its name. To save a Feature object derived from other Feature objects to a catalog, a "
         "unique name must be assigned to it using the property before saving it. The namespace of Lookup "
         "Features or Aggregate Features are defined during their declaration. The property can be used to "
         "modify it before saving it. Once the feature is saved, its name cannot be changed. Therefore, it is "
         "essential to give the Feature object a descriptive and meaningful name before saving it.",
-        UPDATED_AT: _get_updated_at_docstring_override("Feature"),
     },
     "FeatureList": {
         **_get_doc_overrides("FeatureList"),
@@ -120,7 +143,7 @@ pydantic_field_doc_overrides = {
         "imputed_value": "The value that will be used to replace any value that falls outside a specified range.",
     },
     "ViewColumn": {
-        DTYPE: "Returns the data type of the view column.",
+        DTYPE: _get_dtype_docstring_override("ViewColumn"),
         FEATURE_STORE: "Provides information about the feature store that the view column is connected to.",
         NAME: _get_name_docstring_override("ViewColumn"),
     },
