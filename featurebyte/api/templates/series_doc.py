c915f530ac9b735884b7e8f1bece3c1030b432ce
Jevon Yeoh <jevon@featurebyte.com>
2023-07-20 10:32:10 +0800

    [chore][DEV-1952] target: update docstrings for a couple of series fns (#1543)

---
+++ featurebyte/api/templates/series_doc.py
@@ -0,0 +1,29 @@
+"""
+Series docs templates module
+"""
+
+ISNULL_DOC = """
+Returns a boolean {class_name} indicating whether each element is missing.
+
+Returns
+-------
+{class_name}
+    {class_name} with boolean values
+
+Examples
+--------
+{examples}
+"""
+
+NOTNULL_DOC = """
+Returns a boolean {class_name} indicating whether each element is not null.
+
+Returns
+-------
+{class_name}
+    {class_name} with boolean values
+
+Examples
+--------
+{examples}
+"""
