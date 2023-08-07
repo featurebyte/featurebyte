0f0e672921159b1429831aac213a2167627b6a9d
Steve Millet <steve.millet@featurebyte.com>
2023-08-07 12:42:47 -0700

    Use the notebook name as test id in the notebook tests
    It can be hard to figure out which notebook failed during a test run
    on GitHubn with the auto generated ids. This uses the notebook filename
    as the id to make it easier to debug.

--- tests/notebooks/test_notebooks.py
+++ tests/notebooks/test_notebooks.py
@@ -1,4 +1,9 @@
+import pathlib
 import subprocess
+
+import pytest
+
+NOTEBOOKS_PATH = pathlib.Path("./notebooks")
 
 
 def notebook_runner(notebook_path):
@@ -6,16 +11,29 @@ def notebook_runner(notebook_path):
     return subprocess.run(jupyter_cmd, shell=True)
 
 
+def idfn(param):
+    """Return the name of the notebook being tested as the id"""
+    if isinstance(param, pathlib.PosixPath):
+        return str(param).split("/")[1]
+
+
+@pytest.mark.parametrize(
+    "quick_start_notebooks", list(NOTEBOOKS_PATH.glob("quick*.ipynb")), ids=idfn
+)
 def test_quick_start_notebooks(quick_start_notebooks):
     result = notebook_runner(quick_start_notebooks)
     assert result.returncode == 0
 
 
+@pytest.mark.parametrize("deep_dive_notebooks", list(NOTEBOOKS_PATH.glob("deep*.ipynb")), ids=idfn)
 def test_deep_dive_notebooks(deep_dive_notebooks):
     result = notebook_runner(deep_dive_notebooks)
     assert result.returncode == 0
 
 
+@pytest.mark.parametrize(
+    "playground_notebooks", list(NOTEBOOKS_PATH.glob("playground*.ipynb")), ids=idfn
+)
 def test_playground_notebooks(playground_notebooks):
     result = notebook_runner(playground_notebooks)
     assert result.returncode == 0
