578843e60543475eabf41624649d46f5b9eaf8de
Jevon Yeoh <jevon@featurebyte.com>
2023-08-07 11:16:00 +0800

    add test

--- tests/integration/api/test_lookup_target_operations.py
+++ tests/integration/api/test_lookup_target_operations.py
@@ -1,9 +1,12 @@
 """
 Lookup target operations
 """
+from typing import Any
+
 import pandas as pd
 import pytest
 
+from featurebyte.api.target import Target
 from tests.integration.api.feature_preview_utils import (
     convert_preview_param_dict_to_feature_preview_resp,
 )
@@ -69,31 +72,76 @@ def test_item_view_lookup_features(item_table, expected_joined_event_item_datafr
     )
 
 
+def _target_with_offset_test_helper(
+    target: Target,
+    target_name: str,
+    lookup_column_name: str,
+    df: pd.DataFrame,
+    timestamp_column_name: str,
+    primary_key_serving_name: str,
+    primary_key_value: Any,
+    primary_key_column: str,
+    offset_duration: str,
+):
+    # Preview
+    point_in_time = "2001-11-15 10:00:00"
+    preview_params = {
+        "POINT_IN_TIME": point_in_time,
+        primary_key_serving_name: primary_key_value,
+    }
+    preview_output = target.preview(pd.DataFrame([preview_params])).iloc[0].to_dict()
+
+    # Compare with expected result
+    mask = (
+        pd.to_datetime(df[timestamp_column_name], utc=True).dt.tz_localize(None)
+        <= (pd.to_datetime(point_in_time) + pd.Timedelta(offset_duration))
+    ) & (df[primary_key_column] == primary_key_value)
+    expected_row = df[mask].sort_values(timestamp_column_name).iloc[-1]
+    assert preview_output[target_name] == expected_row[lookup_column_name]
+
+
 @pytest.mark.parametrize("source_type", ["databricks", "snowflake", "spark"], indirect=True)
 def test_scd_lookup_target_with_offset(config, scd_table, scd_dataframe):
     """
     Test creating lookup target from a SCDView with offset
     """
-    # SCD lookup target
     offset = "90d"
     scd_view = scd_table.get_view()
-    scd_lookup_target = scd_view["User Status"].as_target(
-        "Current User Status Offset 90d", offset=offset
+    target_name = "Current User Status Offset 90d"
+    scd_lookup_target = scd_view["User Status"].as_target(target_name, offset=offset)
+    _target_with_offset_test_helper(
+        scd_lookup_target,
+        target_name,
+        "User Status",
+        scd_dataframe,
+        "Effective Timestamp",
+        "üser id",
+        1,
+        "User ID",
+        offset,
     )
 
-    # Preview
-    point_in_time = "2001-11-15 10:00:00"
-    user_id = 1
-    preview_params = {
-        "POINT_IN_TIME": point_in_time,
-        "üser id": user_id,
-    }
-    preview_output = scd_lookup_target.preview(pd.DataFrame([preview_params])).iloc[0].to_dict()
 
-    # Compare with expected result
-    mask = (
-        pd.to_datetime(scd_dataframe["Effective Timestamp"], utc=True).dt.tz_localize(None)
-        <= (pd.to_datetime(point_in_time) + pd.Timedelta(offset))
-    ) & (scd_dataframe["User ID"] == user_id)
-    expected_row = scd_dataframe[mask].sort_values("Effective Timestamp").iloc[-1]
-    assert preview_output["Current User Status Offset 90d"] == expected_row["User Status"]
+@pytest.mark.parametrize("source_type", ["databricks", "snowflake", "spark"], indirect=True)
+def test_event_lookup_target_with_offset(config, event_table, transaction_data_upper_case):
+    """
+    Test creating event target from a event view with offset
+    """
+    # Event lookup target
+    offset = "90d"
+    view = event_table.get_view()
+    lookup_column_name = "ÀMOUNT"
+    target_name = "lookup_target"
+    timestamp_column = "ËVENT_TIMESTAMP"
+    event_target = view[lookup_column_name].as_target(target_name, offset=offset)
+    _target_with_offset_test_helper(
+        event_target,
+        target_name,
+        lookup_column_name,
+        transaction_data_upper_case,
+        timestamp_column,
+        "order_id",
+        "T42",
+        "TRANSACTION_ID",
+        offset,
+    )
