7522f0de6191060978e9025d9ea2d6eb2506bc6b
Jevon Yeoh <jevon@featurebyte.com>
2023-08-15 14:53:20 +0800

    [DEV-2068] aggregator: add guardrails for vector aggregation  (#1644)
    * add vector aggregate guardrails for dtype and aggfunc type
    
    * add changelog
    
    * reuse
    
    * centralize unit test
    
    * format
    
    * simplify if

---
+++ featurebyte/api/aggregator/vector_validator.py
@@ -0,0 +1,41 @@
+"""
+Vector aggregate validator
+"""
+from typing import List, Optional
+
+from featurebyte.enum import AggFunc, DBVarType
+from featurebyte.query_graph.model.column_info import ColumnInfo
+
+VECTOR_AGGREGATE_SUPPORTED_FUNCTIONS = {AggFunc.MAX, AggFunc.AVG}
+
+
+def validate_vector_aggregate_parameters(
+    columns_info: List[ColumnInfo], value_column: Optional[str], method: Optional[str]
+) -> None:
+    """
+    Validate vector aggregate parameters
+
+    Parameters
+    ----------
+    columns_info: List[ColumnInfo]
+        List of column info
+    value_column: Optional[str]
+        Column to be aggregated
+    method: Optional[str]
+        Aggregation method
+
+    Raises
+    ------
+    ValueError
+        If the method is not supported for vector aggregate operations
+    """
+    # Check whether the value_column's type is an array
+    for info in columns_info:
+        # If the type of the value_column is not an array, can return. If it is, break out of the loop and
+        # check the method.
+        if info.name == value_column and info.dtype != DBVarType.ARRAY:
+            return
+
+    # If it's an array, check whether the method is supported. We currently only support MAX and AVG operations.
+    if method in VECTOR_AGGREGATE_SUPPORTED_FUNCTIONS:
+        raise ValueError(f"Method {method} is not supported for vector aggregate operations.")
