43147ca06be5c8b22c953bf712277b7640b9d273
Jevon Yeoh <jevon@featurebyte.com>
2023-08-18 16:08:54 +0800

    scaffold out item tests

--- tests/integration/api/test_vector_aggregation_operations.py
+++ tests/integration/api/test_vector_aggregation_operations.py
@@ -24,12 +24,10 @@ VECTOR_VALUE_INT_COL = "VECTOR_VALUE_INT"
 VECTOR_VALUE_DIFF_LENGTH_COL = "VECTOR_VALUE_DIFFERENT_LENGTH"
 
 
-@pytest.fixture(name="data_with_array", scope="session")
-def data_with_array_fixture():
+def _update_df(df: pd.DataFrame, event_timestamp_col: str) -> pd.DataFrame:
     """
-    Simulated data with an array column
+    Helper method to update df
     """
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "fixtures", "vector_data.csv"))
     # Manually add the vectors on since it's easier than shoving it into a csv
     # The first two rows will belong to CUST_ID 1, and the last three rows will belong to CUST_ID 3
     df[VECTOR_VALUE_FLOAT_COL] = [
@@ -53,8 +51,28 @@ def data_with_array_fixture():
         [4, 8],
         [],
     ]
-    df["EVENT_TIMESTAMP"] = pd.to_datetime(df["EVENT_TIMESTAMP"].astype(str))
-    yield df
+    df[event_timestamp_col] = pd.to_datetime(df[event_timestamp_col].astype(str))
+    return df
+
+
+@pytest.fixture(name="item_data_with_array", scope="session")
+def item_data_with_array_fixture():
+    """
+    Simulated data with an array column
+    """
+    df = pd.read_csv(
+        os.path.join(os.path.dirname(__file__), "fixtures", "vector_data_item_data.csv")
+    )
+    yield _update_df(df, "EVENT_TIMESTAMP_ITEM")
+
+
+@pytest.fixture(name="event_data_with_array", scope="session")
+def event_data_with_array_fixture():
+    """
+    Simulated data with an array column
+    """
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "fixtures", "vector_data.csv"))
+    yield _update_df(df, "EVENT_TIMESTAMP")
 
 
 def get_expected_dataframe(
@@ -83,27 +101,16 @@ def get_expected_dataframe(
     return out
 
 
-def test_random(data_with_array):
-    """
-    Test random
-    """
-    assert data_with_array.shape[0] == 5
-    expected_dataframe = get_expected_dataframe(
-        data_with_array, ["CUST_ID", "USER_ID"], "VECTOR_VALUE_FLOAT", AggFunc.AVG
-    )
-    assert expected_dataframe.shape[0] == 3
-
-
 @pytest_asyncio.fixture(name="event_table_with_array_column", scope="session")
 async def register_table_with_array_column(
-    data_with_array, session, data_source, catalog, user_entity, customer_entity
+    event_data_with_array, session, data_source, catalog, user_entity, customer_entity, order_entity
 ):
     """
     Register a table with an array column
     """
-    _ = catalog, user_entity, customer_entity
+    _ = catalog, user_entity, customer_entity, order_entity
     table_name = "event_table_with_vector"
-    await session.register_table(table_name, data_with_array, temporary=False)
+    await session.register_table(table_name, event_data_with_array, temporary=False)
 
     database_table = data_source.get_source_table(
         database_name=session.database_name,
@@ -122,22 +129,60 @@ async def register_table_with_array_column(
     )
     event_table["USER_ID"].as_entity("User")
     event_table["CUST_ID"].as_entity("Customer")
+    event_table["EVENT_ID"].as_entity("Order")
     return event_table
+
+
+@pytest_asyncio.fixture(name="item_table_with_array_column", scope="session")
+async def register_item_table_with_array_column(
+    event_table_with_array_column, item_data_with_array, session, data_source, catalog, item_entity
+):
+    """
+    Register a table with an array column
+    """
+    _ = item_entity
+    table_name = "item_table_with_vector"
+    await session.register_table(table_name, item_data_with_array, temporary=False)
+
+    database_table = data_source.get_source_table(
+        database_name=session.database_name,
+        schema_name=session.schema_name,
+        table_name=table_name,
+    )
+    item_table = database_table.create_item_table(
+        name=table_name,
+        event_id_column="EVENT_ID",
+        item_id_column="ITEM_ID",
+        event_table_name=event_table_with_array_column.name,
+    )
+    # event_table.update_default_feature_job_setting(
+    #     feature_job_setting=FeatureJobSetting(
+    #         blind_spot="30m", frequency="1h", time_modulo_frequency="30m"
+    #     )
+    # )
+    item_table["USER_ID_ITEM"].as_entity("User")
+    item_table["CUST_ID_ITEM"].as_entity("Customer")
+    item_table["ITEM_ID"].as_entity("Item")
+    item_table["EVENT_ID"].as_entity("Order")
+    return item_table
+
+
+TEST_CASES = [
+    (AggFunc.MAX, "[3.0,3.0,3.0]", VECTOR_VALUE_FLOAT_COL),
+    (AggFunc.AVG, "[2.0,2.0,2.0]", VECTOR_VALUE_FLOAT_COL),
+    (AggFunc.SUM, "[4.0,4.0,4.0]", VECTOR_VALUE_FLOAT_COL),
+    (AggFunc.MAX, "[3,3,3]", VECTOR_VALUE_INT_COL),
+    # (AggFunc.AVG, "[2.0,2.0,2.0]", "VECTOR_VALUE_INT"),  # TODO: fix
+    # (AggFunc.SUM, "[4.0,4.0,4.0]", "VECTOR_VALUE_INT"),  # TODO: fix
+]
 
 
 @pytest.mark.parametrize("source_type", ["spark"], indirect=True)
 @pytest.mark.parametrize(
     "agg_func,expected_results,vector_value_column",
-    [
-        (AggFunc.MAX, "[3.0,3.0,3.0]", VECTOR_VALUE_FLOAT_COL),
-        (AggFunc.AVG, "[2.0,2.0,2.0]", VECTOR_VALUE_FLOAT_COL),
-        (AggFunc.SUM, "[4.0,4.0,4.0]", VECTOR_VALUE_FLOAT_COL),
-        (AggFunc.MAX, "[3,3,3]", VECTOR_VALUE_INT_COL),
-        # (AggFunc.AVG, "[2.0,2.0,2.0]", "VECTOR_VALUE_INT"),  # TODO: fix
-        # (AggFunc.SUM, "[4.0,4.0,4.0]", "VECTOR_VALUE_INT"),  # TODO: fix
-    ],
+    TEST_CASES,
 )
-def test_vector_aggregation_operations(
+def test_vector_aggregation_operations__aggregate_over(
     event_table_with_array_column, agg_func, expected_results, vector_value_column
 ):
     """
@@ -163,12 +208,41 @@ def test_vector_aggregation_operations(
 
 
 @pytest.mark.parametrize("source_type", ["spark"], indirect=True)
+@pytest.mark.parametrize(
+    "agg_func,expected_results,vector_value_column",
+    TEST_CASES,
+)
+def test_vector_aggregation_operations__aggregate(
+    item_table_with_array_column, agg_func, expected_results, vector_value_column
+):
+    """
+    Test vector aggregation operations
+    """
+    item_view = item_table_with_array_column.get_view()
+    feature_name = "vector_agg"
+    feature = item_view.groupby("EVENT_ID").aggregate(
+        value_column=vector_value_column,
+        method=agg_func,
+        feature_name=feature_name,
+        skip_fill_na=True,
+    )
+
+    preview_params = {"POINT_IN_TIME": "2022-06-06 00:58:00", "order_id": "1"}
+    feature_preview = feature.preview(pd.DataFrame([preview_params]))
+    assert feature_preview.shape[0] == 1
+    assert feature_preview.iloc[0].to_dict() == {
+        feature_name: expected_results,
+        **convert_preview_param_dict_to_feature_preview_resp(preview_params),
+    }
+
+
+@pytest.mark.parametrize("source_type", ["spark"], indirect=True)
 @pytest.mark.parametrize("cust_id", [1, 3])
 def test_vector_aggregation_operations_fails_for_vectors_of_different_lengths(
     event_table_with_array_column, cust_id
 ):
     """
-    Test vector aggregation operations
+    Test vector aggregation operations fails for vectors of different lengths
     """
     event_view = event_table_with_array_column.get_view()
     feature_name = "vector_agg"
