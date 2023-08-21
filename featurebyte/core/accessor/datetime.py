fe24e2b4c43c7239c3727e9a384133fb2d90c870
Yung Siang Liau <yungsiang@featurebyte.com>
2023-08-22 00:48:44 +0800

    Fix logic to identify timezone offset column

--- featurebyte/core/accessor/datetime.py
+++ featurebyte/core/accessor/datetime.py
@@ -646,12 +646,55 @@ class DatetimeAccessor:
                     break
 
             elif isinstance(opstruct_column, DerivedDataColumn):
-                for column in opstruct_column.columns:
-                    if column.table_id == table_id and column.name == offset_column_name:
-                        offset_column_name_in_frame = opstruct_column.name
-                        break
+                if cls._is_valid_timezone_offset_column_if_derived_column(
+                    opstruct_column, table_id, offset_column_name, frame
+                ):
+                    offset_column_name_in_frame = opstruct_column.name
+                    # break
 
         if offset_column_name_in_frame is not None:
             return frame[offset_column_name_in_frame]  # type: ignore
 
         return None
+
+    @classmethod
+    def _is_valid_timezone_offset_column_if_derived_column(
+        cls,
+        opstruct_column: DerivedDataColumn,
+        table_id: PydanticObjectId,
+        offset_column_name: str,
+        frame: Frame,
+    ):
+        """
+        Check whether a DerivedDataColumn is a valid timezone offset column
+
+        Parameters
+        ----------
+        opstruct_column: DerivedDataColumn
+            The DerivedDataColumn to check
+        table_id: PydanticObjectId
+            Table identifier of the EventTable
+        offset_column_name: str
+            Name of the offset column as defined in the EventTable. The column might not necessarily
+            have the same name in the frame (e.g. due to join suffix).
+        frame: Frame
+            The parent object of the datetime series
+        """
+        is_derived_from_timezone_offset_column = False
+        for column in opstruct_column.columns:
+            if column.table_id == table_id and column.name == offset_column_name:
+                is_derived_from_timezone_offset_column = True
+                break
+
+        if is_derived_from_timezone_offset_column:
+            allowed_node_types = {NodeType.INPUT, NodeType.GRAPH}
+            for node_name in opstruct_column.node_names:
+                is_node_type_allowed = any(
+                    node_name.startswith(allowed_node_type)
+                    for allowed_node_type in allowed_node_types
+                )
+                if not is_node_type_allowed:
+                    return False
+            return True
+
+        return False
