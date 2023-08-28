3263696dcae24abcd3cb382b9ca1b052c59f0133
Jevon Yeoh <jevon@featurebyte.com>
2023-08-28 14:51:01 +0800

    [DEV-2083] vector_agg/snowflake: add average udtf

--- featurebyte/session/snowflake.py
+++ featurebyte/session/snowflake.py
@@ -317,7 +317,7 @@ class SnowflakeSchemaInitializer(BaseSchemaInitializer):
 
     @property
     def current_working_schema_version(self) -> int:
-        return 25
+        return 26
 
     async def create_schema(self) -> None:
         create_schema_query = f'CREATE SCHEMA "{self.session.schema_name}"'
--- tests/integration/udf/test_vector_aggregate.py
+++ tests/integration/udf/test_vector_aggregate.py
@@ -24,13 +24,15 @@ async def setup_test_data_fixture(session):
         (2, [4, 4, 10]),
         (2, [1, 12, 2]),
     ]
-    create_table_query = "CREATE OR REPLACE TABLE test_table (id_col number, array_col ARRAY);"
+    create_table_query = (
+        "CREATE OR REPLACE TABLE test_table (id_col number, array_col ARRAY, count number);"
+    )
     insert_values = []
     for id_col, array_col in input_data:
         insert_values.append(f"({id_col}, '{array_col}')")
     insert_query = f"""
         INSERT INTO test_table
-          SELECT $1, PARSE_JSON($2)
+          SELECT $1, PARSE_JSON($2), 1
           FROM VALUES
           {", ".join(insert_values)}
           ;
@@ -99,13 +101,33 @@ async def test_vector_aggregate_sum(setup_test_data, session):
 
 @pytest.mark.parametrize("source_type", ["snowflake"], indirect=True)
 @pytest.mark.asyncio
+async def test_vector_aggregate_simple_avg(setup_test_data, session):
+    """
+    Test vector aggregate avg
+    """
+    _ = setup_test_data
+    # Run query
+    query = _get_query("VECTOR_AGGREGATE_SIMPLE_AVERAGE")
+    df = await session.execute_query(query)
+
+    # Assert expected results
+    results = [[4, 4, 6], [4, 8, 7]]
+    expected_df = pd.DataFrame({"ID_COL": [1, 2], "OUT": _get_formatted_output(results)})
+    fb_assert_frame_equal(df, expected_df, sort_by_columns=["ID_COL"])
+
+
+@pytest.mark.parametrize("source_type", ["snowflake"], indirect=True)
+@pytest.mark.asyncio
 async def test_vector_aggregate_avg(setup_test_data, session):
     """
     Test vector aggregate avg
     """
     _ = setup_test_data
     # Run query
-    query = _get_query("VECTOR_AGGREGATE_AVG")
+    query = """
+    SELECT a.id_col, b.VECTOR_AGG_RESULT AS OUT
+    FROM test_table AS a, table(VECTOR_AGGREGATE_AVG(ARRAY_COL, COUNT) OVER (partition by id_col)) AS b
+    """
     df = await session.execute_query(query)
 
     # Assert expected results
