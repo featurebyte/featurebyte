9b7f03888fc2200a0b740be37bb421c9bd0cc9ac
Kenny Chua <kenny@featurebyte.com>
2023-08-29 16:56:05 +0800

    [chore] support profile registration using python client (#1690) [0.4] (#1691)
    * support profile registration using python client (#1690)
    
    * fix bug

--- featurebyte/__init__.py
+++ featurebyte/__init__.py
@@ -1,11 +1,10 @@
 """Python Library for FeatureOps"""
-from typing import Any, Dict, List, Optional
+from typing import Any, List, Optional
 
 import os
 import shutil
 import sys
 from http import HTTPStatus
-from pathlib import Path
 
 import pandas as pd
 import yaml
@@ -171,19 +170,27 @@ def use_profile(profile: str) -> None:
             pass
 
 
-def register_tutorial_api_token(api_token: str) -> None:
+def register_profile(profile_name: str, api_url: str, api_token: Optional[str] = None) -> None:
     """
-    Register tutorial api token, and use the tutorial profile. This will write a new tutorial profile to the
-    configuration file.
+    Register a profile for connecting to a FeatureByte service in the configuration file.
+    If the profile already exists, it will be updated.
 
     Parameters
     ----------
+    profile_name: str
+        Profile name
+    api_url: str
+        Featurebye API Service URL
     api_token: str
         Tutorial api token
 
     Examples
     --------
-    >>> fb.register_tutorial_api_token("your_api_token")  # doctest: +SKIP
+    >>> fb.register_profile(  # doctest: +SKIP
+    ...     profile_name="tutorial",
+    ...     api_url="https://tutorials.featurebyte.com/api/v1",
+    ...     api_token="your_api_token"
+    ... )
     """
     # Read configuration file
     config_file_path = Configurations().config_file_path
@@ -191,24 +198,20 @@ def register_tutorial_api_token(api_token: str) -> None:
         loaded_config = yaml.safe_load(file_obj)
 
     profiles: List[Any] = loaded_config["profile"]
-    tutorial_profile_name = "tutorial"
     # Update API token in existing profile if it's there
-    tutorial_url = "https://tutorials.featurebyte.com/api/v1"
     for profile in profiles:
-        if profile["name"] == tutorial_profile_name:
-            if "api_token" in profile:
-                updated_profile = profile["api_token"] != api_token
-            else:
-                updated_profile = True
-            profile["api_token"] = api_token
-            profile["api_url"] = tutorial_url
+        if profile["name"] == profile_name:
+            updated_profile = profile["api_url"] != api_url or profile.get("api_token") != api_token
+            if updated_profile:
+                profile["api_url"] = api_url
+                profile["api_token"] = api_token
             break
     # Add tutorial profile if it's not already there
     else:
         profiles.append(
             {
-                "name": tutorial_profile_name,
-                "api_url": tutorial_url,
+                "name": profile_name,
+                "api_url": api_url,
                 "api_token": api_token,
             }
         )
@@ -226,8 +229,29 @@ def register_tutorial_api_token(api_token: str) -> None:
         with open(config_file_path, "w", encoding="utf-8") as file_obj:
             file_obj.write(yaml_str)
 
-    # Use the tutorial profile
-    use_profile(tutorial_profile_name)
+    # Use the profile
+    use_profile(profile_name)
+
+
+def register_tutorial_api_token(api_token: str) -> None:
+    """
+    Register tutorial api token, and use the tutorial profile. This will write a new tutorial profile to the
+    configuration file.
+
+    Parameters
+    ----------
+    api_token: str
+        Tutorial api token
+
+    Examples
+    --------
+    >>> fb.register_tutorial_api_token("your_api_token")  # doctest: +SKIP
+    """
+    register_profile(
+        profile_name="tutorial",
+        api_url="https://tutorials.featurebyte.com/api/v1",
+        api_token=api_token,
+    )
 
 
 def list_credentials(
--- tests/unit/test_init.py
+++ tests/unit/test_init.py
@@ -1,6 +1,8 @@
 """
 Test init module functions
 """
+from typing import Optional
+
 from unittest import mock
 
 import pytest
@@ -25,6 +27,27 @@ def _assert_tutorial_profile_with_api_token(api_token: str) -> None:
 
     # Verify that the active profile is now the tutorial profile
     assert config.profile.name == "tutorial"
+
+
+def _assert_profile_with_details(
+    profile_name: str, api_url: str, api_token: Optional[str] = None
+) -> None:
+    """
+    Assert that there's a tutorial profile with the given api token
+    """
+    # Reload configs and verify that there's now a profile with tutorial
+    config = Configurations()
+    has_profile = False
+    for profile in config.profiles:
+        if profile.name == profile_name:
+            assert not has_profile  # check that there's only one profile with the given name
+            has_profile = True
+            assert profile.api_url == api_url
+            assert profile.api_token == api_token
+    assert has_profile
+
+    # Verify that the active profile is now the given profile
+    assert config.profile.name == profile_name
 
 
 @pytest.fixture(name="noop_check_sdk_versions")
@@ -67,6 +90,57 @@ def test_register_tutorial_api_token(noop_check_sdk_versions, noop_log_env_summa
         # Re-register with different API token
         fb.register_tutorial_api_token("test2")
         _assert_tutorial_profile_with_api_token("test2")
+    finally:
+        # Reset back to original profile
+        config.use_profile(original_profile_name)
+
+
+def test_register_profile(noop_check_sdk_versions, noop_log_env_summary, config):
+    """
+    Test register_profile function
+    """
+    _ = noop_check_sdk_versions, noop_log_env_summary
+    profile_name = "tutorial"
+    # Verify that there's no profile with tutorial
+    for profile in config.profiles:
+        assert profile.name != profile_name
+    # Verify that the active profile is not tutorial
+    original_profile_name = config.profile.name
+    assert original_profile_name != profile_name
+
+    try:
+        # Register tutorial api token
+        fb.register_profile(
+            profile_name="test",
+            api_url="https://tutorials.featurebyte.com/api/v1",
+            api_token="test",
+        )
+        _assert_profile_with_details(
+            profile_name="test",
+            api_url="https://tutorials.featurebyte.com/api/v1",
+            api_token="test",
+        )
+
+        # Re-register with different details
+        fb.register_profile(
+            profile_name="test2",
+            api_url="https://test.featurebyte.com/api/v1",
+        )
+        _assert_profile_with_details(
+            profile_name="test2",
+            api_url="https://test.featurebyte.com/api/v1",
+        )
+
+        # Update existing profile with different details
+        fb.register_profile(
+            profile_name="test2",
+            api_url="https://test2.featurebyte.com/api/v1",
+        )
+        _assert_profile_with_details(
+            profile_name="test2",
+            api_url="https://test2.featurebyte.com/api/v1",
+        )
+
     finally:
         # Reset back to original profile
         config.use_profile(original_profile_name)
