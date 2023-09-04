04bb9760d9d4b2ff234dc92962c7861f1a255cf4
Yung Siang Liau <yungsiang@featurebyte.com>
2023-09-04 17:42:31 +0800

    [DEV-2216] Fix conversion of date type to double in spark (#1713)
    This fixes the conversion of date type to double in spark. Previously, the
    conversion produced null value if the type is not timestamp.

--- tests/unit/query_graph/sql/adapter/test_spark.py
+++ tests/unit/query_graph/sql/adapter/test_spark.py
@@ -2,6 +2,8 @@
 Test spark adapter module
 """
 
+from sqlglot import expressions
+
 from featurebyte.query_graph.sql.adapter import SparkAdapter
 from tests.unit.query_graph.sql.adapter.base_adapter_test import BaseAdapterTest
 
@@ -12,3 +14,24 @@ class TestSparkAdapter(BaseAdapterTest):
     """
 
     adapter = SparkAdapter()
+
+    def test_dateadd_seconds(self):
+        """
+        Test dateadd_second method for spark adapter
+        """
+        out = self.adapter.dateadd_second(
+            expressions.Literal.number(100), expressions.Identifier(this="my_timestamp")
+        )
+        expected = "CAST(CAST(CAST(my_timestamp AS TIMESTAMP) AS DOUBLE) + 100 AS TIMESTAMP)"
+        assert out.sql() == expected
+
+    def test_datediff_microseond(self):
+        """
+        Test datediff_microsecond method for spark adapter
+        """
+        out = self.adapter.datediff_microsecond(
+            expressions.Identifier(this="t1"),
+            expressions.Identifier(this="t2"),
+        )
+        expected = "(CAST(CAST(t2 AS TIMESTAMP) AS DOUBLE) * 1000000.0 - CAST(CAST(t1 AS TIMESTAMP) AS DOUBLE) * 1000000.0)"
+        assert out.sql() == expected
