64b0b56ef86163490c5c28808e860bc7cef40a14
Jevon Yeoh <jevon@featurebyte.com>
2023-09-04 17:57:48 +0800

    run test in both spark and sf

--- tests/integration/api/test_vector_aggregation_operations.py
+++ tests/integration/api/test_vector_aggregation_operations.py
@@ -382,7 +382,7 @@ def test_vector_aggregation_operations_fails_for_vectors_of_different_lengths(
         feature.preview(pd.DataFrame([preview_params]))
 
 
-@pytest.mark.parametrize("source_type", ["spark"], indirect=True)
+@pytest.mark.parametrize("source_type", ["spark", "snowflake"], indirect=True)
 def test_vector_cosine_similarity(item_table_with_array_column):
     """
     Test vector cosine similarity
