c777389d6116cc687369a49cdb8cb1944012e66a
Kenny Chua <kenny@featurebyte.com>
2023-09-11 17:23:20 +0800

    [chore] Add request retry for api client (#1733)
    * add request retry for api client
    
    * extend timeout to 3 min

--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -3,6 +3,7 @@ Test config parser
 """
 import os
 import tempfile
+import time
 from pathlib import Path
 from unittest.mock import Mock, patch
 from uuid import uuid4
@@ -213,6 +214,7 @@ def test_client_redirection(mock_check_sdk_versions):
             "GET",
             "https://app1.featurebyte.com/api/v1/user/me",
             allow_redirects=False,
+            timeout=180,
             headers={},
         )
 
@@ -278,3 +280,17 @@ def test_websocket_reconnect():
             ]
             with pytest.raises(WebSocketBadStatusException):
                 ws_client.receive_bytes()
+
+
+@pytest.mark.no_mock_api_client
+def test_client_retry():
+    """
+    Test getting client
+    """
+    # expect client to retry and take more than 1 second to report error
+    client = APIClient(api_url="http://localhost:9999", api_token=None)
+    t0 = time.time()
+    with pytest.raises(InvalidSettingsError):
+        client.get("/")
+    t1 = time.time()
+    assert (t1 - t0) > 1
