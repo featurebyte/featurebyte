eb86e032a4a838efbbd7fff5e193dd6b748d0891
Jevon Yeoh <jevon@featurebyte.com>
2023-09-18 20:05:23 +0800

    [chore] snowflake/vector_aggregate_avg: cast count to FLOAT for type consistency (#1750)
    * [chore] snowflake/vector_aggregate_avg: use NUMBER instead of INTEGER type
    
    * try again
    
    * float
    
    * update count to cast as float
    
    * cast to int
    
    * undo
    
    * cast to float
    
    * test
    
    * use float instead
    
    * fix test
    
    * snowflake version
    
    * use double

--- featurebyte/query_graph/sql/tiling.py
+++ featurebyte/query_graph/sql/tiling.py
@@ -328,10 +328,12 @@ class VectorAvgAggregator(OrderIndependentAggregator):
         sum_expression = expressions.Anonymous(
             this="VECTOR_AGGREGATE_SUM", expressions=[quoted_identifier(col.name)]
         )
+        count = expressions.Count(this=expressions.Star())
+        cast_as_double = expressions.Cast(this=count, to=expressions.DataType.build("DOUBLE"))
         return [
             self.construct_array_tile_spec(sum_expression, f"sum_list_value_{agg_id}", AggFunc.SUM),
             self.construct_numeric_tile_spec(
-                expressions.Count(this=expressions.Star()), f"count_value_{agg_id}", AggFunc.COUNT
+                cast_as_double, f"count_value_{agg_id}", AggFunc.COUNT
             ),
         ]
 
--- tests/integration/udf/test_vector_aggregate.py
+++ tests/integration/udf/test_vector_aggregate.py
@@ -16,15 +16,15 @@ async def setup_test_data_fixture(session):
     """
     # Prepare test data
     input_data = [
-        (1, [4, 5, 6]),
-        (1, [1, 3, 3]),
-        (1, [7, 4, 9]),
-        (2, [7, 8, 9]),
-        (2, [4, 4, 10]),
-        (2, [1, 12, 2]),
+        (1, [4, 5, 6.0]),
+        (1, [1, 3, 3.0]),
+        (1, [7, 4, 9.0]),
+        (2, [7, 8, 9.0]),
+        (2, [4, 4, 10.0]),
+        (2, [1, 12, 2.0]),
     ]
     create_table_query = (
-        "CREATE OR REPLACE TABLE test_table (id_col number, array_col ARRAY, count number);"
+        "CREATE OR REPLACE TABLE test_table (id_col number, array_col ARRAY, count float);"
     )
     insert_values = []
     for id_col, array_col in input_data:
