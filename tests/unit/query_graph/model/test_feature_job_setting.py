4eb2e9f54fcd89997bd9d67b99c778cbfc46402a
senbong <senbong@featurebyte.com>
2023-09-26 18:08:32 +0800

    [chore] Revise `TableIdFeatureJobSetting` (#1776)
    * Revise TableIdFeatureJobSetting
    
    * Fix lint

--- tests/unit/query_graph/model/test_feature_job_setting.py
+++ tests/unit/query_graph/model/test_feature_job_setting.py
@@ -1,7 +1,10 @@
 """
 Test module for feature job setting
 """
+from bson import ObjectId
+
 from featurebyte import FeatureJobSetting
+from featurebyte.query_graph.model.feature_job_setting import TableIdFeatureJobSetting
 
 
 def test_equality_of_feature_job_setting():
@@ -34,3 +37,35 @@ def test_equality_of_feature_job_setting():
         time_modulo_frequency="3600s",
     )
     assert feature_job_setting_4 == feature_job_setting_1
+
+
+def test_table_id_feature_job_setting():
+    """Test table id feature job setting"""
+    table_id = ObjectId()
+    setting1 = TableIdFeatureJobSetting(
+        table_id=table_id,
+        feature_job_setting=FeatureJobSetting(
+            blind_spot="1h",
+            frequency="1d",
+            time_modulo_frequency="1h",
+        ),
+    )
+    setting2 = TableIdFeatureJobSetting(
+        table_id=table_id,
+        feature_job_setting=FeatureJobSetting(
+            blind_spot="3600s",
+            frequency="24h",
+            time_modulo_frequency="60m",
+        ),
+    )
+    assert setting1 == setting2
+
+    # compare with dict
+    assert setting1 == setting2.dict()
+
+    # check that table setting is hashable
+    assert hash(setting1) == hash(setting2)
+
+    # change table id & compare
+    setting2.table_id = ObjectId()
+    assert setting1 != setting2
