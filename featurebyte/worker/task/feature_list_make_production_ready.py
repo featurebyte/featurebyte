92361624338c9c7d1c0b013495e185be519f7a71
Jevon Yeoh <jevon@featurebyte.com>
2023-10-12 23:59:04 +0800

    lint

--- featurebyte/worker/task/feature_list_make_production_ready.py
+++ featurebyte/worker/task/feature_list_make_production_ready.py
@@ -38,9 +38,6 @@ class FeatureListMakeProductionReadyTask(BaseTask[FeatureListMakeProductionReady
         return f'Make all features of feature list "{feature_list_name}" production ready'
 
     async def execute(self, payload: FeatureListMakeProductionReadyTaskPayload) -> Any:
-        """
-        Execute FeatureListMakeProductionReadyTask
-        """
         await self.feature_list_facade_service.make_features_production_ready(
             feature_list_id=payload.feature_list_id,
             ignore_guardrails=payload.ignore_guardrails,
