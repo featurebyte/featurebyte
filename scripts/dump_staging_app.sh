60f117cd9e9b1b195fcb2f98bb8487c63903e5f2
senbong <senbong@featurebyte.com>
2023-10-31 09:43:42 +0800

    Update script

--- scripts/dump_staging_app.sh
+++ scripts/dump_staging_app.sh
@@ -7,6 +7,17 @@ MONGO_DUMP_FILENAME=mongodb-staging.tar.gz
 
 # download staging data & dump it to the mongo
 gsutil cp gs://featurebyte_staging_backup/$MONGO_DUMP_FILENAME
-docker cp $MONGO_DUMP_FILENAME mongo-testrs:/
-docker exec mongo-testrs mongorestore --gzip --drop --uri 'mongodb://localhost:27021,localhost:27022/?replicaSet=rs0' --archive $MONGO_DUMP_FILENAME
-# rm -f $MONGO_DUMP_FILENAME
+docker cp $MONGO_DUMP_FILENAME mongo-rs:/
+docker exec mongo-rs mongorestore --gzip --drop \
+    --nsInclude="featurebyte.*catalog" \
+    --nsInclude="featurebyte.*feature_store" \
+    --nsInclude="featurebyte.*entity" \
+    --nsInclude="featurebyte.*relationship_info" \
+    --nsInclude="featurebyte.*table" \
+    --nsInclude="featurebyte.*feature" \
+    --nsInclude="featurebyte.*feature_namespace" \
+    --nsExclude="featurebyte.feature_ideation_suggested_feature" \
+    --archive=$MONGO_DUMP_FILENAME \
+    --uri 'mongodb://localhost:27021,localhost:27022/?replicaSet=rs0'
+
+rm -f $MONGO_DUMP_FILENAME
