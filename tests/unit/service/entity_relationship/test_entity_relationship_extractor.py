31aeb61a6d168d0598a68dacef52a5c026ce8789
senbong <senbong@featurebyte.com>
2023-10-31 20:31:27 +0800

    [DEV-2518] Update feature list relationships info extraction logic (#1883)
    * Implement extract_primary_entity_descendant_relationship
    
    * Update feature list entity relationship extraction logic
    
    * Address comment
    
    * Update docstring

--- tests/unit/service/entity_relationship/test_entity_relationship_extractor.py
+++ tests/unit/service/entity_relationship/test_entity_relationship_extractor.py
@@ -13,34 +13,6 @@ def relationship_info_service_fixture(app_container):
 
 
 @pytest.mark.asyncio
-async def test_extract_all_relationships(
-    entity_relationship_extractor,
-    relationship_grandpa_father,
-    relationship_granny_father,
-    relationship_father_son,
-    relationship_mother_son,
-    father_entity_id,
-    mother_entity_id,
-):
-    """Test extractor"""
-    extractor = entity_relationship_extractor
-    output = await extractor.extract_all_relationships(entity_ids=[father_entity_id])
-    relationship_ids = set(relationship.id for relationship in output)
-    assert relationship_ids == {
-        relationship_grandpa_father.id,
-        relationship_granny_father.id,
-        relationship_father_son.id,
-        relationship_mother_son.id,
-    }
-
-    output = await extractor.extract_all_relationships(
-        entity_ids=[mother_entity_id],
-    )
-    relationship_ids = set(relationship.id for relationship in output)
-    assert relationship_ids == {relationship_mother_son.id, relationship_father_son.id}
-
-
-@pytest.mark.asyncio
 async def test_extract_relationship_from_primary_entity_case_1(
     entity_relationship_extractor,
     relationship_grandpa_father,
@@ -122,3 +94,89 @@ async def test_extract_relationship_from_primary_entity_case_2(
     )
     relationship_ids = set(relationship.id for relationship in output)
     assert relationship_ids == {relationship_grandpa_father.id}
+
+
+@pytest.mark.asyncio
+async def test_extract_primary_entity_descendant_relationship__case_1(
+    entity_relationship_extractor,
+    relationship_grandpa_father,
+    relationship_granny_father,
+    relationship_father_son,
+    relationship_mother_son,
+    grandpa_entity_id,
+    granny_entity_id,
+    father_entity_id,
+    mother_entity_id,
+    son_entity_id,
+):
+    """Test extract_primary_entity_descendant_relationship (case 1)"""
+    extractor = entity_relationship_extractor
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[grandpa_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_grandpa_father.id, relationship_father_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[granny_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_granny_father.id, relationship_father_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[father_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_father_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[mother_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_mother_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[father_entity_id, mother_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_father_son.id, relationship_mother_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[son_entity_id],
+    )
+    assert output == []
+
+
+@pytest.mark.asyncio
+async def test_extract_primary_entity_descendant_relationship__case_2(
+    entity_relationship_extractor,
+    relationship_grandpa_father,
+    relationship_mother_son,
+    grandpa_entity_id,
+    father_entity_id,
+    mother_entity_id,
+    son_entity_id,
+):
+    """Test extract_primary_entity_descendant_relationship (case 2)"""
+    extractor = entity_relationship_extractor
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[grandpa_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_grandpa_father.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[father_entity_id],
+    )
+    assert output == []
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[mother_entity_id],
+    )
+    relationship_ids = set(relationship.id for relationship in output)
+    assert relationship_ids == {relationship_mother_son.id}
+
+    output = await extractor.extract_primary_entity_descendant_relationship(
+        primary_entity_ids=[son_entity_id],
+    )
+    assert output == []
