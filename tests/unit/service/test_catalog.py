cff86a6d6c5e8bbe6193ecafd45ef63cd06ed221
senbong <senbong@featurebyte.com>
2023-11-01 15:26:42 +0800

    Update test catalog

--- tests/unit/service/test_catalog.py
+++ tests/unit/service/test_catalog.py
@@ -18,11 +18,22 @@ async def test_catalog_get_and_list_raw_query_filter(app_container, catalog):
         await catalog_service.get_document(document_id=catalog.id)
 
     with catalog_service.allow_use_raw_query_filter():
+        # test get
         retrieved_catalog = await catalog_service.get_document(
             document_id=catalog.id, use_raw_query_filter=True
         )
         assert retrieved_catalog.id == catalog.id
 
+        # test list
         output = await catalog_service.list_documents_as_dict(use_raw_query_filter=True)
         assert output["total"] == 1
         assert output["data"][0]["_id"] == catalog.id
+
+        # test list iterator
+        output = [
+            catalog.id
+            async for catalog in catalog_service.list_documents_iterator(
+                query_filter={}, use_raw_query_filter=True
+            )
+        ]
+        assert output == [catalog.id]
