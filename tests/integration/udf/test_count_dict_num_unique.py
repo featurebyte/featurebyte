40ade115e1a5a20ee16c5af4f38d69bf41a23434
Jevon Yeoh <jevon@featurebyte.com>
2023-11-09 14:47:58 +0800

    [chore] udf: add a couple unit test for UDFs

---
+++ tests/integration/udf/test_count_dict_num_unique.py
@@ -0,0 +1,29 @@
+"""
+Tests for count dict num unique UDF
+"""
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("source_type", ["snowflake"], indirect=True)
+@pytest.mark.parametrize(
+    "dictionary, expected",
+    [
+        (None, 0),
+        ({}, 0),
+        ({"a": 1}, 1),
+        ({"a": 1, "b": 1, "c": 1}, 3),
+    ],
+)
+@pytest.mark.asyncio
+async def test_count_dict_num_unique_udf(session, to_object, dictionary, expected):
+    """
+    Test count dict num unique UDF
+    """
+    dictionary_expr = to_object(dictionary)
+    query = f"SELECT F_COUNT_DICT_NUM_UNIQUE({dictionary_expr}) AS OUT"
+    df = await session.execute_query(query)
+    actual = df.iloc[0]["OUT"]
+    if actual is None:
+        actual = np.nan
+    np.testing.assert_allclose(actual, expected, 1e-5)
