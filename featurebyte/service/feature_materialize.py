daea7a6daa3695799a159ea09fc1aa74eb413f2c
Yung Siang Liau <yungsiang@featurebyte.com>
2023-12-07 10:09:19 +0800

    Fix lint issues

--- featurebyte/service/feature_materialize.py
+++ featurebyte/service/feature_materialize.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 
 from typing import AsyncIterator, List, Optional, cast
 
-import textwrap
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
 
