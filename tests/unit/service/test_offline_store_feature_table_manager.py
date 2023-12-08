7399579c08a20c427d0638e8331f085a1611fbb3
Yung Siang Liau <yungsiang@featurebyte.com>
2023-12-08 23:58:52 +0800

    Fix lint issue

--- tests/unit/service/test_offline_store_feature_table_manager.py
+++ tests/unit/service/test_offline_store_feature_table_manager.py
@@ -32,7 +32,7 @@ async def undeploy_feature(feature):
     """
     Helper function to undeploy a single feature
     """
-    deployment = fb.Deployment.get(f"{feature.name}_list")
+    deployment: fb.Deployment = fb.Deployment.get(f"{feature.name}_list")
     deployment.disable()
 
 
