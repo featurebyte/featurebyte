fb233dad0f78485ff71a4cf210737add6f7e492a
Yung Siang Liau <yungsiang@featurebyte.com>
2023-12-24 18:13:41 +0800

    Skip databricks_unity for test_vector_cosine_similarity

--- tests/integration/udf/test_vector_cosine_similarity.py
+++ tests/integration/udf/test_vector_cosine_similarity.py
@@ -4,7 +4,11 @@ Test vector cosine similarity
 import numpy as np
 import pytest
 
+from tests.source_types import SNOWFLAKE_SPARK_DATABRICKS
 
+
+# Note: Currently not applicable to databricks_unity
+@pytest.mark.parametrize("source_type", SNOWFLAKE_SPARK_DATABRICKS, indirect=True)
 @pytest.mark.parametrize(
     "array1, array2, expected",
     [
