3395dde32e8a46fc2661bf4dee3fe7bdc7d00cf3
Yung Siang Liau <yungsiang@featurebyte.com>
2023-12-28 14:38:23 +0800

    Update fixture

--- tests/fixtures/feature_materialize/initialize_new_columns_new_table_databricks.sql
+++ tests/fixtures/feature_materialize/initialize_new_columns_new_table_databricks.sql
@@ -13,7 +13,7 @@ TBLPROPERTIES (
 SELECT
   CAST('2022-01-01T00:00:00' AS TIMESTAMP) AS `__feature_timestamp`,
   `cust_id`,
-  `sum_30m`
+  `sum_30m_V220101`
 FROM `TEMP_FEATURE_TABLE_000000000000000000000000`;
 
 ALTER TABLE `fb_entity_cust_id_fjs_1800_300_600_ttl` ALTER COLUMN `__feature_timestamp` SET NOT NULL;
