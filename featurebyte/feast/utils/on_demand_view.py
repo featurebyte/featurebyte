861bb3b791fa70c025ed735a97845c89baa609a4
senbong <senbong@featurebyte.com>
2024-01-05 11:57:40 +0800

    Fix tests & lint

--- featurebyte/feast/utils/on_demand_view.py
+++ featurebyte/feast/utils/on_demand_view.py
@@ -1,13 +1,10 @@
 """
 On demand feature view related classes and functions.
 """
-from typing import Dict, List, Optional, Union, cast
+from typing import Any, Dict, List, Optional, Union, cast
 
-import importlib
-import os
 from unittest.mock import patch
 
-from bson import ObjectId
 from feast import FeatureView, Field, RequestSource
 from feast.feature_view_projection import FeatureViewProjection
 from feast.on_demand_feature_view import OnDemandFeatureView, on_demand_feature_view
@@ -23,7 +20,6 @@ def create_feast_on_demand_feature_view(
     function_name: str,
     sources: List[Union[FeatureView, RequestSource, FeatureViewProjection]],
     schema: List[Field],
-    on_demand_feature_view_dir: str,
 ) -> OnDemandFeatureView:
     """
     Create a Feast OnDemandFeatureView from a function definition.
@@ -38,29 +34,21 @@ def create_feast_on_demand_feature_view(
         List of FeatureViews and RequestSources to be used as sources for the OnDemandFeatureView
     schema: List[Field]
         List of Fields to be used as schema for the OnDemandFeatureView
-    on_demand_feature_view_dir: str
-        Directory to write the on demand feature view code to
 
     Returns
     -------
     OnDemandFeatureView
         The created OnDemandFeatureView
     """
-    module_name = f"on_demand_feature_view_{ObjectId()}"
-    with open(
-        os.path.join(on_demand_feature_view_dir, f"{module_name}.py"), "w", encoding="utf-8"
-    ) as file_handle:
-        file_handle.write(definition)
+    # get the function from the definition
+    locals_namespace: Dict[str, Any] = {}
+    exec(definition, locals_namespace)  # pylint: disable=exec-used  # nosec
+    func = locals_namespace[function_name]
 
-    module = importlib.import_module(module_name)
-    func = getattr(module, function_name)
-    try:
+    # dill.source.getsource fails to find the source code of the function
+    # since we already have the source code, we can just use that instead
+    with patch("dill.source.getsource", return_value=definition):
         output = on_demand_feature_view(sources=sources, schema=schema)(func)
-    except IOError:
-        # dill.source.getsource fails to find the source code of the function in some environments
-        # since we already have the source code, we can just use that instead
-        with patch("dill.source.getsource", return_value=definition):
-            output = on_demand_feature_view(sources=sources, schema=schema)(func)
 
     return cast(OnDemandFeatureView, output)
 
@@ -76,7 +64,6 @@ class OnDemandFeatureViewConstructor:
         feature_model: FeatureModel,
         name_to_feast_feature_view: Dict[str, FeatureView],
         name_to_feast_request_source: Dict[str, RequestSource],
-        on_demand_feature_view_dir: str,
     ) -> OnDemandFeatureView:
         """
         Create a Feast OnDemandFeatureView from an offline store info.
@@ -89,8 +76,6 @@ class OnDemandFeatureViewConstructor:
             Dict of FeatureView names to FeatureViews to be used as sources for the OnDemandFeatureView
         name_to_feast_request_source: Dict[str, RequestSource]
             Dict of RequestSource names to RequestSources to be used as sources for the OnDemandFeatureView
-        on_demand_feature_view_dir: str
-            Directory to write the on demand feature view code to
 
         Returns
         -------
@@ -158,6 +143,5 @@ class OnDemandFeatureViewConstructor:
                     dtype=to_feast_primitive_type(DBVarType(feature_model.dtype)),
                 )
             ],
-            on_demand_feature_view_dir=on_demand_feature_view_dir,
         )
         return feature_view
