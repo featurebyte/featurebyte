8c2386ff348b6cca0f57f93f42fc03b19e8f0da9
Kenny Chua <kenny@featurebyte.com>
2024-02-02 13:02:26 +0800

    add test coverage for io task executor

--- featurebyte/schema/worker/task/test.py
+++ featurebyte/schema/worker/task/test.py
@@ -9,11 +9,31 @@ from featurebyte.schema.worker.task.base import BaseTaskPayload, TaskType
 
 class TestTaskPayload(BaseTaskPayload):
     """
-    Test Task Payload
+    Test CPU Task Payload
     """
 
     command = WorkerCommand.TEST
     task_type = TaskType.CPU_TASK
+
+    @property
+    def task_output_path(self) -> Optional[str]:
+        """
+        Redirect route used to retrieve the task result
+
+        Returns
+        -------
+        Optional[str]
+        """
+        return None
+
+
+class TestIOTaskPayload(BaseTaskPayload):
+    """
+    Test Task Payload
+    """
+
+    command = WorkerCommand.TEST
+    task_type = TaskType.IO_TASK
 
     @property
     def task_output_path(self) -> Optional[str]:
