044d118841c7f5178ebb38cf8b8be8a04a7e293e
senbong <senbong@featurebyte.com>
2024-02-07 20:50:04 +0800

    Update test

--- tests/unit/worker/task/test_batch_creator.py
+++ tests/unit/worker/task/test_batch_creator.py
@@ -23,5 +23,9 @@ async def test_patch_api_object_cache__async_func(snowflake_event_table):
     # make the first call to the function to populate the cache during the execution of the function
     await check_api_object_cache()
 
+    # check that the cache after the execution of the function
+    assert ApiObject._cache.maxsize == 1024
+    assert ApiObject._cache.ttl == 1
+
     # check that the cache is cleared after the execution of the function
     await check_api_object_cache()
