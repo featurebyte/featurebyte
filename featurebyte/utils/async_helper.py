479287672854bc7785e2ebcda6ef2bd20ce7da59
Kenny Chua <kenny@featurebyte.com>
2024-04-06 02:07:22 +0800

    support maximum concurrency with asyncio gather

---
+++ featurebyte/utils/async_helper.py
@@ -0,0 +1,53 @@
+"""
+Helper functions for asyncio
+"""
+
+from typing import Coroutine
+
+import asyncio
+from asyncio import Future
+
+
+def asyncio_gather(*coros: Coroutine, max_concurrency: int = 0) -> Future:
+    """
+    Run coroutines with a optional concurrency limit
+
+    Parameters
+    ----------
+    coros: Coroutine
+        Coroutines to run
+    max_concurrency: int
+        Maximum number of coroutines to run concurrently
+
+    Returns
+    -------
+    Future
+    """
+
+    if max_concurrency < 1:
+        return asyncio.gather(*coros)
+
+    failed = False
+    tasks = []
+    semaphore = asyncio.Semaphore(max_concurrency)
+
+    # Run coroutines with a semaphore
+    async def _coro(coro):
+        async with semaphore:
+            nonlocal failed, tasks
+            if failed:  # pylint: disable=used-before-assignment
+                # Close unawaited coroutines on failure
+                coro.close()
+                return
+            task = asyncio.create_task(coro)
+            tasks.append(task)
+            try:
+                return await task
+            except Exception:
+                failed = True
+                # Cancel all tasks on failure
+                for task in tasks:
+                    task.cancel()
+                raise
+
+    return asyncio.gather(*(_coro(c) for c in coros))
