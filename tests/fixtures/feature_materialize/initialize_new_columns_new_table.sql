7511e78cc6e3435327d085e357ba7c763862bcc9
Yung Siang Liau <yungsiang@featurebyte.com>
2024-04-15 02:37:04 +0800

    Update feature materialize service for precomputed lookup feature tables

--- tests/fixtures/feature_materialize/initialize_new_columns_new_table.sql
+++ tests/fixtures/feature_materialize/initialize_new_columns_new_table.sql
@@ -64,6 +64,10 @@ SELECT
   CAST("_fb_internal_cust_id_window_w1800_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295" AS DOUBLE) AS "sum_30m_V220101"
 FROM _FB_AGGREGATED AS AGG;
 
+SELECT
+  COUNT(*)
+FROM "cat1_cust_id_30m";
+
 CREATE TABLE "sf_db"."sf_schema"."cat1_cust_id_30m" AS
 SELECT
   CAST('2022-01-01T00:00:00' AS TIMESTAMPNTZ) AS "__feature_timestamp",
