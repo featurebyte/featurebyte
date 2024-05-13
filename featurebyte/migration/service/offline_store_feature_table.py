705462a75dd3ecb3a9eb18c6f33b2689d7a92a10
senbong <senbong@featurebyte.com>
2024-05-13 16:33:17 +0800

    Handle remote attributes & update method name

--- featurebyte/migration/service/offline_store_feature_table.py
+++ featurebyte/migration/service/offline_store_feature_table.py
@@ -31,15 +31,17 @@ class OfflineStoreFeatureTableMigrationServiceV9(BaseMongoCollectionMigration):
         return self.offline_store_feature_table_service  # type: ignore[return-value]
 
     @migrate(version=9, description="Add base_name to offline store feature table records")
-    async def add_primary_entity_ids_and_relationships_info(self) -> None:
+    async def add_base_name_to_offline_store_feature_table_records(self) -> None:
         """Add base_name to offline store feature table records"""
         query_filter = {"precomputed_lookup_feature_table_info": None}
         async for table in self.offline_store_feature_table_service.list_documents_iterator(
-            query_filter=query_filter
+            query_filter=query_filter, populate_remote_attributes=False
         ):
             await self.offline_store_feature_table_service.update_documents(
                 query_filter={"_id": table.id},
                 update={"$set": {"base_name": table.get_basename()}},
             )
-            updated_table = await self.offline_store_feature_table_service.get_document(table.id)
+            updated_table = await self.offline_store_feature_table_service.get_document(
+                table.id, populate_remote_attributes=False
+            )
             assert updated_table.base_name is not None
