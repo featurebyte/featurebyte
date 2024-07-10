d8180a157d6975d72a9a3a0b7c1da7a090c42ca6
Chester <chester@featurebyte.com>
2024-07-10 16:39:07 +0800

    [chore] fixing unit-tests (#2447)

--- tests/unit/migration/test_run.py
+++ tests/unit/migration/test_run.py
@@ -78,6 +78,8 @@ async def test_migrate_method_generator(user, persistent, get_credential, schema
         persistent=persistent,
         get_credential=get_credential,
         celery=get_celery(),
+        storage=get_storage(),
+        temp_storage=get_temp_storage(),
         schema_metadata=schema_metadata,
         include_data_warehouse_migrations=True,
     )
@@ -99,6 +101,8 @@ async def test_migrate_method_generator(user, persistent, get_credential, schema
         persistent=persistent,
         get_credential=get_credential,
         celery=get_celery(),
+        storage=get_storage(),
+        temp_storage=get_temp_storage(),
         schema_metadata=schema_metadata,
         include_data_warehouse_migrations=True,
     )
@@ -125,6 +129,8 @@ async def test_migrate_method_generator__exclude_warehouse(
         persistent=persistent,
         get_credential=get_credential,
         celery=get_celery(),
+        storage=get_storage(),
+        temp_storage=get_temp_storage(),
         schema_metadata=schema_metadata,
         include_data_warehouse_migrations=False,
     )
@@ -198,9 +204,9 @@ async def test_run_migration(
         persistent=persistent,
         get_credential=get_credential,
         celery=get_celery(),
-        include_data_warehouse_migrations=False,
         storage=get_storage(),
         temp_storage=get_temp_storage(),
+        include_data_warehouse_migrations=False,
         redis=Mock(),
     )
 
@@ -212,6 +218,8 @@ async def test_run_migration(
         persistent=persistent,
         get_credential=get_credential,
         celery=get_celery(),
+        storage=get_storage(),
+        temp_storage=get_temp_storage(),
         schema_metadata=schema_metadata,
         include_data_warehouse_migrations=False,
     ):
