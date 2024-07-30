e05deab9a350bec67e59fcda23767f0833358927
Yung Siang Liau <yungsiang@featurebyte.com>
2024-07-30 20:28:42 +0800

    [DEV-3642] Update PreviewService to do value counts without sorting full table (#2497)

--- tests/unit/query_graph/interpreter/test_preview.py
+++ tests/unit/query_graph/interpreter/test_preview.py
@@ -205,6 +205,7 @@ def test_value_counts_sql_no_casting(graph, node_input, update_fixtures):
         column_names=["a", "b"],
         num_rows=50000,
         num_categories_limit=1000,
+        total_num_rows=100000,
     )
     assert len(value_counts_queries.queries) == 2
     for query in value_counts_queries.queries:
