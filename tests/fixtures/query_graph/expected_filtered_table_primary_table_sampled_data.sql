ec6ad40a84ab529881596f480c206c57cfd2cec1
senbong <senbong@featurebyte.com>
2024-08-23 18:26:25 +0800

    Fix fixtures due to logical conflict

--- tests/fixtures/query_graph/expected_filtered_table_primary_table_sampled_data.sql
+++ tests/fixtures/query_graph/expected_filtered_table_primary_table_sampled_data.sql
@@ -41,11 +41,20 @@ FROM (
 ) AS L
 LEFT JOIN (
   SELECT
-    "order_id" AS "order_id",
-    "item_id" AS "item_id",
-    "item_name" AS "item_name",
-    "item_type" AS "item_type"
-  FROM "db"."public"."item_table"
+    "order_id",
+    ANY_VALUE("item_id") AS "item_id",
+    ANY_VALUE("item_name") AS "item_name",
+    ANY_VALUE("item_type") AS "item_type"
+  FROM (
+    SELECT
+      "order_id" AS "order_id",
+      "item_id" AS "item_id",
+      "item_name" AS "item_name",
+      "item_type" AS "item_type"
+    FROM "db"."public"."item_table"
+  )
+  GROUP BY
+    "order_id"
 ) AS R
   ON L."order_id" = R."order_id"
 WHERE
