ff6cea423b395972efe15d07af31866d4d701469
Yung Siang Liau <yungsiang@featurebyte.com>
2024-08-28 09:40:08 +0800

    Add helper function for calling udf in integration tests

--- tests/integration/udf/test_count_dict_num_unique.py
+++ tests/integration/udf/test_count_dict_num_unique.py
@@ -4,9 +4,8 @@ Tests for count dict num unique UDF
 
 import numpy as np
 import pytest
-from sqlglot import expressions
 
-from featurebyte.query_graph.sql.common import get_fully_qualified_function_call, sql_to_string
+from tests.integration.udf.util import execute_query_with_udf
 
 
 @pytest.mark.parametrize(
@@ -24,15 +23,7 @@ async def test_count_dict_num_unique_udf(session, to_object, dictionary, expecte
     Test count dict num unique UDF
     """
     dictionary_expr = to_object(dictionary)
-    udf_expr = get_fully_qualified_function_call(
-        session.database_name, session.schema_name, "F_COUNT_DICT_NUM_UNIQUE", [dictionary_expr]
-    )
-    query = sql_to_string(
-        expressions.select(expressions.alias_(udf_expr, alias="OUT", quoted=False)),
-        session.source_type,
-    )
-    df = await session.execute_query(query)
-    actual = df.iloc[0]["OUT"]
+    actual = await execute_query_with_udf(session, "F_COUNT_DICT_NUM_UNIQUE", [dictionary_expr])
     if actual is None:
         actual = np.nan
     np.testing.assert_allclose(actual, expected, 1e-5)
