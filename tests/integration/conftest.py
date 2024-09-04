f7a924cd876141af59e94612d69da0e04f077917
Yung Siang Liau <yungsiang@featurebyte.com>
2024-09-04 22:08:52 +0800

    Enable lookup feature and target tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -90,8 +90,6 @@ SKIPPED_TESTS = {
         "tests/integration/api/test_features.py",
         "tests/integration/api/test_forward_aggregate_asat.py",
         "tests/integration/api/test_item_view_operations.py",
-        "tests/integration/api/test_lookup_feature_operations.py",
-        "tests/integration/api/test_lookup_target_operations.py",
         "tests/integration/api/test_on_demand_features.py",
         "tests/integration/api/test_serving_parent_features.py",
         "tests/integration/api/test_target_correctness.py",
