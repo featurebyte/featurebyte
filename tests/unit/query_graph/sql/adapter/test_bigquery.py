c94bc0d0af8810c6777f3f4d454a9e46d061fb68
Yung Siang Liau <yungsiang@featurebyte.com>
2024-09-09 16:45:37 +0800

    Check in fixture

---
+++ tests/unit/query_graph/sql/adapter/test_bigquery.py
@@ -0,0 +1,30 @@
+"""
+Tests for BigQueryAdapter
+"""
+
+from sqlglot.expressions import select
+
+from featurebyte.enum import SourceType
+from featurebyte.query_graph.sql.adapter import BigQueryAdapter
+from featurebyte.query_graph.sql.common import sql_to_string
+from tests.util.helper import assert_equal_with_expected_fixture
+
+
+def test_random_sample(update_fixtures):
+    """
+    Test random_sample
+    """
+    output = sql_to_string(
+        BigQueryAdapter.random_sample(
+            select("a", "b").from_("table1"),
+            desired_row_count=100,
+            total_row_count=1000,
+            seed=1234,
+        ),
+        SourceType.BIGQUERY,
+    )
+    assert_equal_with_expected_fixture(
+        output,
+        "tests/fixtures/adapter/bigquery_random_sample.sql",
+        update_fixtures,
+    )
