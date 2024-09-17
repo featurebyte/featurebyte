717a65527f88f399ca78064bb4615263a9e42954
Yung Siang Liau <yungsiang@featurebyte.com>
2024-09-17 10:35:03 +0800

    Fix entity universe for window aggregates from source

--- featurebyte/query_graph/sql/tile_util.py
+++ featurebyte/query_graph/sql/tile_util.py
@@ -100,8 +100,20 @@ def calculate_last_tile_index_expr(
     Expression
     """
     point_in_time_epoch_expr = adapter.to_epoch_seconds(point_in_time_expr)
-    last_tile_index_expr = parse_one(
-        f"FLOOR(({point_in_time_epoch_expr.sql()} - {time_modulo_frequency}) / {frequency})"
+    # f"FLOOR(({point_in_time_epoch_expr.sql()} - {time_modulo_frequency}) / {frequency})"
+    last_tile_index_expr = expressions.Cast(
+        this=expressions.Floor(
+            this=expressions.Div(
+                this=expressions.Paren(
+                    this=expressions.Sub(
+                        this=point_in_time_epoch_expr,
+                        expression=make_literal_value(time_modulo_frequency),
+                    )
+                ),
+                expression=make_literal_value(frequency),
+            ),
+        ),
+        to=expressions.DataType.build("BIGINT"),
     )
     if offset is not None:
         offset_num_tiles = offset // frequency
