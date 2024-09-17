e02ee48aa83d0f25d1a574873df643ba97170e99
Yung Siang Liau <yungsiang@featurebyte.com>
2024-09-17 12:52:23 +0800

    Fix mypy

--- featurebyte/query_graph/sql/tile_util.py
+++ featurebyte/query_graph/sql/tile_util.py
@@ -100,6 +100,7 @@ def calculate_last_tile_index_expr(
     Expression
     """
     point_in_time_epoch_expr = adapter.to_epoch_seconds(point_in_time_expr)
+    last_tile_index_expr: Expression
     last_tile_index_expr = expressions.Cast(
         this=expressions.Floor(
             this=expressions.Div(
