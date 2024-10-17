2540ed0d12039bfd6e47a41f219af8c97b280e02
Yung Siang Liau <yungsiang@featurebyte.com>
2024-10-17 18:17:16 +0800

    [chore] Add error handling and logging in query cache cleanup task (#2640)

--- featurebyte/service/query_cache_cleanup.py
+++ featurebyte/service/query_cache_cleanup.py
@@ -6,12 +6,15 @@ from __future__ import annotations
 
 from bson import ObjectId
 
+from featurebyte.logging import get_logger
 from featurebyte.models.query_cache import QueryCacheType
 from featurebyte.service.feature_store import FeatureStoreService
 from featurebyte.service.query_cache import QueryCacheDocumentService
 from featurebyte.service.query_cache_cleanup_scheduler import QueryCacheCleanupSchedulerService
 from featurebyte.service.session_manager import SessionManagerService
 from featurebyte.storage import Storage
+
+logger = get_logger(__name__)
 
 
 class QueryCacheCleanupService:
@@ -44,22 +47,43 @@ class QueryCacheCleanupService:
         """
         feature_store = await self.feature_store_service.get_document(feature_store_id)
         session = await self.session_manager_service.get_feature_store_session(feature_store)
+
+        logger.info("Query cache cleanup started for feature store %s", feature_store_id)
+        n_success = 0
+        n_failure = 0
+
         async for (
             cache_model
         ) in self.query_cache_document_service.list_stale_documents_as_dict_iterator(
             feature_store_id
         ):
-            await self.query_cache_document_service.delete_document(cache_model["_id"])
             cached_object = cache_model["cached_object"]
-            if cached_object["type"] == QueryCacheType.TEMP_TABLE:
-                await session.drop_table(
-                    table_name=cached_object["table_name"],
-                    schema_name=session.schema_name,
-                    database_name=session.database_name,
+            try:
+                if cached_object["type"] == QueryCacheType.TEMP_TABLE:
+                    await session.drop_table(
+                        table_name=cached_object["table_name"],
+                        schema_name=session.schema_name,
+                        database_name=session.database_name,
+                    )
+                elif cached_object["type"] == QueryCacheType.DATAFRAME:
+                    await self.storage.delete(cached_object["storage_path"])
+                await self.query_cache_document_service.delete_document(cache_model["_id"])
+            except Exception as e:
+                n_failure += 1
+                logger.exception(
+                    "Error occurred while cleaning up query cache %s: %s",
+                    cache_model["_id"],
+                    e,
                 )
-            elif cached_object["type"] == QueryCacheType.DATAFRAME:
-                await self.storage.delete(cached_object["storage_path"])
+                continue
+            n_success += 1
 
+        logger.info(
+            "Query cache cleanup completed for feature store %s. %s documents cleaned up, %s failed",
+            feature_store_id,
+            n_success,
+            n_failure,
+        )
         await self.query_cache_cleanup_scheduler_service.stop_job_if_no_longer_needed(
             feature_store_id
         )
