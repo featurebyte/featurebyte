0c7f31a4a62a181f16c8d033cff3a2af0d9c215a
Yung Siang Liau <yungsiang@featurebyte.com>
2024-11-16 10:29:04 +0800

    Add test_get_historical_feature_query_set__on_demand_tile_tables

---
+++ tests/fixtures/expected_historical_requests_on_demand_tile_tables.sql
@@ -0,0 +1,170 @@
+CREATE TABLE "SOME_HISTORICAL_FEATURE_TABLE" AS
+WITH "REQUEST_TABLE_W1800_F1800_BS600_M300_cust_id" AS (
+  SELECT
+    "POINT_IN_TIME",
+    "cust_id",
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) AS __FB_LAST_TILE_INDEX,
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) - 1 AS __FB_FIRST_TILE_INDEX
+  FROM (
+    SELECT DISTINCT
+      "POINT_IN_TIME",
+      "cust_id"
+    FROM REQUEST_TABLE
+  )
+), "REQUEST_TABLE_W7200_F1800_BS600_M300_cust_id" AS (
+  SELECT
+    "POINT_IN_TIME",
+    "cust_id",
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) AS __FB_LAST_TILE_INDEX,
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) - 4 AS __FB_FIRST_TILE_INDEX
+  FROM (
+    SELECT DISTINCT
+      "POINT_IN_TIME",
+      "cust_id"
+    FROM REQUEST_TABLE
+  )
+), "REQUEST_TABLE_W86400_F1800_BS600_M300_cust_id" AS (
+  SELECT
+    "POINT_IN_TIME",
+    "cust_id",
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) AS __FB_LAST_TILE_INDEX,
+    CAST(FLOOR((
+      DATE_PART(EPOCH_SECOND, "POINT_IN_TIME") - 300
+    ) / 1800) AS BIGINT) - 48 AS __FB_FIRST_TILE_INDEX
+  FROM (
+    SELECT DISTINCT
+      "POINT_IN_TIME",
+      "cust_id"
+    FROM REQUEST_TABLE
+  )
+), _FB_AGGREGATED AS (
+  SELECT
+    REQ."__FB_TABLE_ROW_INDEX",
+    REQ."POINT_IN_TIME",
+    REQ."CUSTOMER_ID",
+    "T0"."_fb_internal_cust_id_window_w1800_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295" AS "_fb_internal_cust_id_window_w1800_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295",
+    "T1"."_fb_internal_cust_id_window_w7200_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295" AS "_fb_internal_cust_id_window_w7200_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295",
+    "T2"."_fb_internal_cust_id_window_w86400_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295" AS "_fb_internal_cust_id_window_w86400_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295"
+  FROM REQUEST_TABLE AS REQ
+  LEFT JOIN (
+    SELECT
+      "POINT_IN_TIME",
+      "cust_id",
+      SUM(value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295) AS "_fb_internal_cust_id_window_w1800_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295"
+    FROM (
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W1800_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 1) = FLOOR(TILE.INDEX / 1)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+      UNION ALL
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W1800_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 1) - 1 = FLOOR(TILE.INDEX / 1)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+    )
+    GROUP BY
+      "POINT_IN_TIME",
+      "cust_id"
+  ) AS T0
+    ON REQ."POINT_IN_TIME" = T0."POINT_IN_TIME" AND REQ."cust_id" = T0."cust_id"
+  LEFT JOIN (
+    SELECT
+      "POINT_IN_TIME",
+      "cust_id",
+      SUM(value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295) AS "_fb_internal_cust_id_window_w7200_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295"
+    FROM (
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W7200_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 4) = FLOOR(TILE.INDEX / 4)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+      UNION ALL
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W7200_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 4) - 1 = FLOOR(TILE.INDEX / 4)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+    )
+    GROUP BY
+      "POINT_IN_TIME",
+      "cust_id"
+  ) AS T1
+    ON REQ."POINT_IN_TIME" = T1."POINT_IN_TIME" AND REQ."cust_id" = T1."cust_id"
+  LEFT JOIN (
+    SELECT
+      "POINT_IN_TIME",
+      "cust_id",
+      SUM(value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295) AS "_fb_internal_cust_id_window_w86400_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295"
+    FROM (
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W86400_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 48) = FLOOR(TILE.INDEX / 48)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+      UNION ALL
+      SELECT
+        REQ."POINT_IN_TIME",
+        REQ."cust_id",
+        TILE.INDEX,
+        TILE.value_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295
+      FROM "REQUEST_TABLE_W86400_F1800_BS600_M300_cust_id" AS REQ
+      INNER JOIN __MY_TEMP_TILE_TABLE AS TILE
+        ON FLOOR(REQ.__FB_LAST_TILE_INDEX / 48) - 1 = FLOOR(TILE.INDEX / 48)
+        AND REQ."cust_id" = TILE."cust_id"
+      WHERE
+        TILE.INDEX >= REQ.__FB_FIRST_TILE_INDEX AND TILE.INDEX < REQ.__FB_LAST_TILE_INDEX
+    )
+    GROUP BY
+      "POINT_IN_TIME",
+      "cust_id"
+  ) AS T2
+    ON REQ."POINT_IN_TIME" = T2."POINT_IN_TIME" AND REQ."cust_id" = T2."cust_id"
+)
+SELECT
+  AGG."__FB_TABLE_ROW_INDEX",
+  AGG."POINT_IN_TIME",
+  AGG."CUSTOMER_ID",
+  CAST("_fb_internal_cust_id_window_w86400_sum_e8c51d7d1ec78e1f35195fc0cf61221b3f830295" AS DOUBLE) AS "sum_1d"
+FROM _FB_AGGREGATED AS AGG
