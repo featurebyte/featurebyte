46ac722f18d43629226c5c6ae48fa827e886b85d
Yung Siang Liau <yungsiang@featurebyte.com>
2024-11-16 13:27:19 +0800

    Use date_format instead of to_varchar to support older runtimes

--- featurebyte/sql/databricks_unity/F_INDEX_TO_TIMESTAMP.sql
+++ featurebyte/sql/databricks_unity/F_INDEX_TO_TIMESTAMP.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION F_INDEX_TO_TIMESTAMP(tile_index INTEGER, time_modulo_frequency_seconds INTEGER, blind_spot_seconds INTEGER, frequency_minute INTEGER)
   RETURNS STRING
   LANGUAGE SQL
-    return to_varchar(
+    return date_format(
       dateadd(
         second,
         time_modulo_frequency_seconds - blind_spot_seconds,
