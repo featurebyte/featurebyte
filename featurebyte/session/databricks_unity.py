4913f08ddeff1dd6cacb72757033c30dbb1c09f7
Yung Siang Liau <yungsiang@featurebyte.com>
2024-11-17 14:21:33 +0800

    [chore] Fix F_INDEX_TO_TIMESTAMP UDF in Databricks (#2675)

--- featurebyte/session/databricks_unity.py
+++ featurebyte/session/databricks_unity.py
@@ -52,7 +52,7 @@ class DatabricksUnitySchemaInitializer(BaseSparkSchemaInitializer):
 
     @property
     def current_working_schema_version(self) -> int:
-        return 19
+        return 20
 
     async def create_schema(self) -> None:
         await super().create_schema()
