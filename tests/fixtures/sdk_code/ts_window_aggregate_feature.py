6cd0b4ff9b2c547fcbb32075eea4a898d134f57b
Yung Siang Liau <yungsiang@featurebyte.com>
2025-01-22 02:58:30 +0800

    Fix missing reference_timezone field in generated code

--- tests/fixtures/sdk_code/ts_window_aggregate_feature.py
+++ tests/fixtures/sdk_code/ts_window_aggregate_feature.py
@@ -20,7 +20,7 @@ grouped = time_series_view.groupby(
     windows=[CalendarWindow(unit="MONTH", size=3)],
     feature_names=["col_float_sum_3month"],
     feature_job_setting=CronFeatureJobSetting(
-        crontab="0 8 1 * *", timezone="Etc/UTC"
+        crontab="0 8 1 * *", timezone="Etc/UTC", reference_timezone=None
     ),
     skip_fill_na=True,
     offset=None,
