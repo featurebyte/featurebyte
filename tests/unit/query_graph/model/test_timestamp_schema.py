2d903e3e08048f856e4ba94f12c4e5d2e5067fe4
Yung Siang Liau <yungsiang@featurebyte.com>
2025-01-21 16:34:14 +0800

    [DEV-3959] Validate compatibility of window and offset unit and format_string (#2784)

--- tests/unit/query_graph/model/test_timestamp_schema.py
+++ tests/unit/query_graph/model/test_timestamp_schema.py
@@ -50,7 +50,7 @@ def test_timezone_name__invalid():
     """
     with pytest.raises(ValueError) as e:
         TimestampSchema(timezone="my_timezone")
-    assert "Invalid timezone name" in str(e)
+    assert "Invalid timezone name" in str(e.value)
 
 
 def test_local_time_without_timezone():
@@ -59,7 +59,7 @@ def test_local_time_without_timezone():
     """
     with pytest.raises(ValueError) as e:
         TimestampSchema(is_utc_time=False, timezone=None)
-    assert "Timezone must be provided for local time" in str(e)
+    assert "Timezone must be provided for local time" in str(e.value)
 
 
 def test_invalid_ts_datetime_types(ts_schema_utc_without_timezone):
@@ -72,12 +72,12 @@ def test_invalid_ts_datetime_types(ts_schema_utc_without_timezone):
             dtype=DBVarType.INT,
             dtype_metadata=DBVarTypeMetadata(timestamp_schema=ts_schema_utc_without_timezone),
         )
-    assert "Column ts is of type INT (expected: [DATE, TIMESTAMP, VARCHAR])" in str(e)
+    assert "Column ts is of type INT (expected: [DATE, TIMESTAMP, VARCHAR])" in str(e.value)
 
 
-def test_invalid_string_datetime_without_format_string(ts_schema_utc_without_timezone):
+def test_string_datetime_missing_format_string(ts_schema_utc_without_timezone):
     """
-    Test invalid timestamp schema datetime types
+    Test string timestamp type missing format_string
     """
     with pytest.raises(ValueError) as e:
         ColumnSpec(
@@ -85,4 +85,21 @@ def test_invalid_string_datetime_without_format_string(ts_schema_utc_without_tim
             dtype=DBVarType.VARCHAR,
             dtype_metadata=DBVarTypeMetadata(timestamp_schema=TimestampSchema()),
         )
-    assert "format_string is required in the timestamp_schema for column ts" in str(e)
+    assert "format_string is required in the timestamp_schema for column ts" in str(e.value)
+
+
+def test_non_string_datetime_with_format_string(ts_schema_utc_without_timezone):
+    """
+    Test non-string timestamp type with unnecessary format_string
+    """
+    with pytest.raises(ValueError) as e:
+        ColumnSpec(
+            name="ts",
+            dtype=DBVarType.TIMESTAMP,
+            dtype_metadata=DBVarTypeMetadata(
+                timestamp_schema=TimestampSchema(format_string="YYYY-MM-DD")
+            ),
+        )
+    assert "format_string is not supported in the timestamp_schema for non-string column ts" in str(
+        e.value
+    )
