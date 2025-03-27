7e5907e28e25fd2a85230bd52cfb0c6b166c9746
Yung Siang Liau <yungsiang@featurebyte.com>
2025-03-27 17:57:25 +0800

    [DEV-4025] Fix grouped materialization for calendar aggregation features (#2897)

--- tests/unit/query_graph/test_time_series_window_aggregator.py
+++ tests/unit/query_graph/test_time_series_window_aggregator.py
@@ -104,6 +104,26 @@ def reference_datetime_is_utc_agg_spec_fixture(base_agg_spec):
     return base_agg_spec
 
 
+@pytest.fixture(name="grouped_agg_specs")
+def grouped_agg_specs_fixture(base_agg_spec):
+    """
+    Fixture of TimeSeriesAggregateSpec objects that can be grouped in sql query
+    """
+    agg_spec_1 = copy.deepcopy(base_agg_spec)
+    agg_spec_1.parameters.agg_func = "sum"
+    agg_spec_1.parameters.names = ["feature_sum"]
+
+    agg_spec_2 = copy.deepcopy(base_agg_spec)
+    agg_spec_2.parameters.agg_func = "min"
+    agg_spec_2.parameters.names = ["feature_min"]
+
+    agg_spec_3 = copy.deepcopy(base_agg_spec)
+    agg_spec_3.parameters.agg_func = "max"
+    agg_spec_3.parameters.names = ["feature_ax"]
+
+    return [agg_spec_1, agg_spec_2, agg_spec_3]
+
+
 @pytest.mark.parametrize(
     "test_case_name",
     [
@@ -114,6 +134,7 @@ def reference_datetime_is_utc_agg_spec_fixture(base_agg_spec):
         "latest",
         "mixed_order_dependency",
         "is_utc_time",
+        "grouped",
     ],
 )
 def test_aggregator(request, test_case_name, update_fixtures, source_info):
@@ -128,6 +149,7 @@ def test_aggregator(request, test_case_name, update_fixtures, source_info):
         "latest": "latest_agg_spec",
         "mixed_order_dependency": "mixed_order_dependency_agg_specs",
         "is_utc_time": "reference_datetime_is_utc_time_agg_spec",
+        "grouped": "grouped_agg_specs",
     }
     fixture_name = test_case_mapping[test_case_name]
     fixture_obj = request.getfixturevalue(fixture_name)
