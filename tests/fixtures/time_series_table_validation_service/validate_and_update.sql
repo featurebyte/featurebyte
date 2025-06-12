dad7de5d4d8d4789bcf0b18d7dfec750c57dadf3
Yung Siang Liau <yungsiang@featurebyte.com>
2025-06-12 18:16:41 +0800

    Validate date format string on more rows during table validation

--- tests/fixtures/time_series_table_validation_service/validate_and_update.sql
+++ tests/fixtures/time_series_table_validation_service/validate_and_update.sql
@@ -1,9 +1,13 @@
 SELECT
-  TO_TIMESTAMP("snapshot_date", '%Y-%m-%d') AS "snapshot_date"
-FROM "my_db"."my_schema"."my_table"
-WHERE
-  "snapshot_date" IS NOT NULL
-LIMIT 10;
+  MAX("snapshot_date") AS "RESULT"
+FROM (
+  SELECT
+    TO_TIMESTAMP("snapshot_date", '%Y-%m-%d') AS "snapshot_date"
+  FROM "my_db"."my_schema"."my_table"
+  WHERE
+    "snapshot_date" IS NOT NULL
+  LIMIT 50000
+);
 
 SELECT
   COUNT(DISTINCT "snapshot_date") AS "snapshot_date"
