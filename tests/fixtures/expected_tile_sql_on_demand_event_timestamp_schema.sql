f8d3f2d13cb75cc9ac76309c452fcc373b6f0f8f
Yung Siang Liau <yungsiang@featurebyte.com>
2025-06-16 23:16:54 +0800

    [DEV-4083] perf: Skip timestamp filtering in entity table when conversion is required (#3011)

---
+++ tests/fixtures/expected_tile_sql_on_demand_event_timestamp_schema.sql
@@ -0,0 +1,36 @@
+WITH __FB_ENTITY_TABLE_NAME AS (
+  __FB_ENTITY_TABLE_SQL_PLACEHOLDER
+), __FB_TILE_COMPUTE_INPUT_TABLE_NAME AS (
+  SELECT
+    R.*
+  FROM __FB_ENTITY_TABLE_NAME
+  INNER JOIN (
+    SELECT
+      "event_timestamp" AS "event_timestamp",
+      "cust_id" AS "cust_id",
+      "col_int" AS "input_col_sum_fefdc3e84902d2f76f04cc746b8bba9256212347"
+    FROM "sf_database"."sf_schema"."sf_table_no_tz"
+  ) AS R
+    ON R."cust_id" = __FB_ENTITY_TABLE_NAME."cust_id"
+)
+SELECT
+  index,
+  "cust_id",
+  SUM("input_col_sum_fefdc3e84902d2f76f04cc746b8bba9256212347") AS value_sum_fefdc3e84902d2f76f04cc746b8bba9256212347
+FROM (
+  SELECT
+    *,
+    F_TIMESTAMP_TO_INDEX(
+      CAST(CONVERT_TIMEZONE(
+        'UTC',
+        TO_TIMESTAMP_TZ(CONCAT(TO_CHAR("event_timestamp", 'YYYY-MM-DD HH24:MI:SS'), ' ', "tz_offset"))
+      ) AS TIMESTAMP),
+      300,
+      600,
+      30
+    ) AS index
+  FROM __FB_TILE_COMPUTE_INPUT_TABLE_NAME
+)
+GROUP BY
+  index,
+  "cust_id"
