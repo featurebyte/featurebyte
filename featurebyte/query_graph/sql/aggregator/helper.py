18f4cf3930cc239f714df3858dab4f72ba75f0d7
Yung Siang Liau <yungsiang@featurebyte.com>
2025-09-02 14:19:24 +0800

    Revert changes to join_aggregated_expr_with_distinct_point_in_time

--- featurebyte/query_graph/sql/aggregator/helper.py
+++ featurebyte/query_graph/sql/aggregator/helper.py
@@ -17,7 +17,6 @@ def join_aggregated_expr_with_distinct_point_in_time(
     serving_names: list[str],
     aggregated_column_names: list[str],
     distinct_by_point_in_time_table_name: str,
-    join_on_serving_names: bool = True,
 ) -> Select:
     """
     Join aggregated result that is distinct by a different key (e.g. scheduled feature job time) and
@@ -40,33 +39,18 @@ def join_aggregated_expr_with_distinct_point_in_time(
     distinct_by_point_in_time_table_name: str
         Table name of the distinct by point in time request table. This table has both the
         POINT_IN_TIME and the distinct_key column.
-    join_on_serving_names: bool
-        Whether to include serving names in the join condition. Default is True. Otherwise, the join
-        condition will only include the distinct key, and the serving names will be selected from the
-        aggregated table as additional output columns. This is useful when the distinct point in
-        time request table is independent of the serving names, e.g. in aggregate_asat for
-        SnapshotsTable.
 
     Returns
     -------
     Select
     """
-    join_key_columns = [distinct_key]
-    if join_on_serving_names:
-        join_key_columns += serving_names
     join_condition = expressions.and_(*[
         expressions.EQ(
             this=get_qualified_column_identifier(column_name, "AGGREGATED"),
             expression=get_qualified_column_identifier(column_name, "DISTINCT_POINT_IN_TIME"),
         )
-        for column_name in join_key_columns
+        for column_name in [distinct_key] + serving_names
     ])
-    columns_from_distinct_point_in_time = [SpecialColumnName.POINT_IN_TIME.value]
-    columns_from_aggregated = aggregated_column_names[:]
-    if join_on_serving_names:
-        columns_from_distinct_point_in_time += serving_names
-    else:
-        columns_from_aggregated += serving_names
     aggregated_expr = (
         select(
             *[
@@ -74,14 +58,14 @@ def join_aggregated_expr_with_distinct_point_in_time(
                     column_name,
                     "DISTINCT_POINT_IN_TIME",
                 )
-                for column_name in columns_from_distinct_point_in_time
+                for column_name in [SpecialColumnName.POINT_IN_TIME.value] + serving_names
             ],
             *[
                 get_qualified_column_identifier(
                     column_name,
                     "AGGREGATED",
                 )
-                for column_name in columns_from_aggregated
+                for column_name in aggregated_column_names
             ],
         )
         .from_(
