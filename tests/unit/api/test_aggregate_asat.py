c61ef3e63b89546cd8ec2de91e2a35fde186541e
Yung Siang Liau <yungsiang@featurebyte.com>
2025-09-02 18:18:06 +0800

    Add unit tests for aggregate_asat with SnapshotsView

--- tests/unit/api/test_aggregate_asat.py
+++ tests/unit/api/test_aggregate_asat.py
@@ -167,3 +167,101 @@ def test_aggregate_asat_with_category(
             "window": None,
         }
     }
+
+
+def test_aggregate_asat_snapshots_view(snowflake_snapshots_table_with_entity, another_entity):
+    """
+    Test aggregate_asat with snapshots view
+    """
+    view = snowflake_snapshots_table_with_entity.get_view()
+    feature = view.groupby("col_binary", category="col_text").aggregate_asat(
+        value_column="col_float",
+        method="sum",
+        feature_name="asat_feature",
+        offset="7d",
+    )
+
+    # Check node and parameters
+    feature_dict = feature.model_dump()
+    graph_dict = feature_dict["graph"]
+    node_dict = get_node(graph_dict, feature_dict["node_name"])
+    assert node_dict == {
+        "name": "project_1",
+        "type": "project",
+        "output_type": "series",
+        "parameters": {"columns": ["asat_feature"]},
+    }
+    asat_node_dict = get_node(graph_dict, "aggregate_as_at_1")
+    assert asat_node_dict == {
+        "name": "aggregate_as_at_1",
+        "output_type": "frame",
+        "parameters": {
+            "agg_func": "sum",
+            "backward": True,
+            "current_flag_column": None,
+            "effective_timestamp_column": "date",
+            "effective_timestamp_metadata": None,
+            "end_timestamp_column": None,
+            "end_timestamp_metadata": None,
+            "entity_ids": [another_entity.id],
+            "keys": ["col_binary"],
+            "name": "asat_feature",
+            "natural_key_column": None,
+            "offset": "7d",
+            "parent": "col_float",
+            "serving_names": ["another_key"],
+            "snapshots_parameters": {
+                "feature_job_setting": None,
+                "offset": None,
+                "snapshot_datetime_column": "date",
+                "snapshot_datetime_metadata": {
+                    "timestamp_schema": {
+                        "format_string": "YYYY-MM-DD " "HH24:MI:SS",
+                        "is_utc_time": None,
+                        "timezone": "Etc/UTC",
+                    },
+                    "timestamp_tuple_schema": None,
+                },
+                "time_interval": {"unit": "DAY", "value": 1},
+            },
+            "value_by": "col_text",
+        },
+        "type": "aggregate_as_at",
+    }
+
+    # check feature can be saved
+    feature.save()
+
+    # check that category is set correctly
+    agg_info = feature.info()["metadata"]["aggregations"]
+    assert agg_info == {
+        "F0": {
+            "aggregation_type": "aggregate_as_at",
+            "category": "col_text",
+            "column": "Input0",
+            "filter": False,
+            "function": "sum",
+            "keys": ["col_binary"],
+            "name": "asat_feature",
+            "offset": "7d",
+            "window": None,
+        }
+    }
+
+
+def test_aggregate_asat_snapshots_view_invalid_key(snowflake_snapshots_table_with_entity):
+    """
+    Test aggregate_asat with snapshots view using an invalid groupby key
+    """
+    view = snowflake_snapshots_table_with_entity.get_view()
+    with pytest.raises(ValueError) as exc_info:
+        _ = view.groupby("col_int", category="col_text").aggregate_asat(
+            value_column="col_float",
+            method="sum",
+            feature_name="asat_feature",
+            offset="7d",
+        )
+    assert (
+        str(exc_info.value)
+        == "Snapshot ID column cannot be used as a groupby key in aggregate_asat"
+    )
