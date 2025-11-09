a1d5f8b3e04a5bc2f569034ccf06cd60f1647abc
Yung Siang Liau <yungsiang@featurebyte.com>
2025-11-09 08:07:30 +0800

    Refactor and add RelationshipInfoUpdateService

--- featurebyte/routes/relationship_info/controller.py
+++ featurebyte/routes/relationship_info/controller.py
@@ -20,6 +20,7 @@ from featurebyte.service.entity import EntityService
 from featurebyte.service.mixin import DEFAULT_PAGE_SIZE
 from featurebyte.service.relationship import EntityRelationshipService
 from featurebyte.service.relationship_info import RelationshipInfoService
+from featurebyte.service.relationship_info_update import RelationshipInfoUpdateService
 from featurebyte.service.table import TableService
 from featurebyte.service.user_service import UserService
 
@@ -39,14 +40,14 @@ class RelationshipInfoController(
         entity_service: EntityService,
         table_service: TableService,
         user_service: UserService,
-        entity_relationship_service: EntityRelationshipService,
+        relationship_info_update_service: RelationshipInfoUpdateService,
     ):
         super().__init__(relationship_info_service)
         self.relationship_info_service = relationship_info_service
         self.entity_service = entity_service
         self.data_service = table_service
         self.user_service = user_service
-        self.entity_relationship_service = entity_relationship_service
+        self.relationship_info_update_service = relationship_info_update_service
 
     async def create_relationship_info(
         self,
@@ -193,35 +194,7 @@ class RelationshipInfoController(
         RelationshipInfoModel
             Updated RelationshipInfo object
         """
-        relationship_info = await self.relationship_info_service.get_document(
-            document_id=relationship_info_id
+        return await self.relationship_info_update_service.update_relationship_info(
+            relationship_info_id=relationship_info_id,
+            data=data,
         )
-
-        if (
-            data.relationship_type is not None
-            and data.relationship_type != relationship_info.relationship_type
-        ):
-            if data.relationship_type == RelationshipType.ONE_TO_ONE:
-                # Changing from CHILD_PARENT to ONE_TO_ONE
-                await self.entity_relationship_service.remove_relationship(
-                    parent_id=relationship_info.related_entity_id,
-                    child_id=relationship_info.entity_id,
-                )
-            else:
-                # Changing from ONE_TO_ONE to CHILD_PARENT
-                table_model = await self.data_service.get_document(
-                    document_id=relationship_info.relation_table_id
-                )
-                await self.entity_relationship_service.add_relationship(
-                    parent=ParentEntity(
-                        id=relationship_info.related_entity_id,
-                        table_id=relationship_info.relation_table_id,
-                        table_type=table_model.type,
-                    ),
-                    child_id=relationship_info.entity_id,
-                )
-
-        updated_relationship_info = await self.relationship_info_service.update_document(
-            document_id=relationship_info_id, data=data
-        )
-        return cast(RelationshipInfoModel, updated_relationship_info)
