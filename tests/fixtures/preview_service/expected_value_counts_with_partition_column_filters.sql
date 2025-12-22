5268e92b1d4de9cfd04be26d3d4f1afaa20fcd20
Kenny Chua <kenny@featurebyte.com>
2025-12-22 13:58:26 +0800

    refactor preview service value counts to reduce sql queries

--- tests/fixtures/preview_service/expected_value_counts_with_partition_column_filters.sql
+++ tests/fixtures/preview_service/expected_value_counts_with_partition_column_filters.sql
@@ -78,35 +78,3 @@ WHERE
 ORDER BY
   "prob"
 LIMIT 50;
-
-CREATE TABLE "__FB_TEMPORARY_TABLE_000000000000000000000001" AS
-SELECT
-  "col_int" AS "col_int",
-  "col_float" AS "col_float",
-  "col_char" AS "col_char",
-  CAST("col_text" AS VARCHAR) AS "col_text",
-  "col_binary" AS "col_binary",
-  "col_boolean" AS "col_boolean",
-  "event_timestamp" AS "event_timestamp",
-  "created_at" AS "created_at",
-  CAST("cust_id" AS VARCHAR) AS "cust_id"
-FROM "__FB_TEMPORARY_TABLE_000000000000000000000000"
-WHERE
-  "event_timestamp" >= CAST('2023-01-01T00:00:00' AS TIMESTAMP)
-  AND "event_timestamp" < CAST('2025-01-01T00:00:00' AS TIMESTAMP)
-LIMIT 50;
-
-SELECT
-  "col_char" AS "key",
-  "__FB_COUNTS" AS "count"
-FROM (
-  SELECT
-    "col_char",
-    COUNT(*) AS "__FB_COUNTS"
-  FROM "__FB_TEMPORARY_TABLE_000000000000000000000001"
-  GROUP BY
-    "col_char"
-  ORDER BY
-    "__FB_COUNTS" DESC NULLS LAST
-  LIMIT 5
-);
