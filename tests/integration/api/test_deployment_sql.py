d8dab225ef2f4f4b7fd1fc2d91eab5570da1d00c
Yung Siang Liau <yungsiang@featurebyte.com>
2026-01-21 16:49:37 +0800

    Add asat features test cases for deployment sql

--- tests/integration/api/test_deployment_sql.py
+++ tests/integration/api/test_deployment_sql.py
@@ -197,6 +197,29 @@ def scd_lookup_feature_test_case(client, scd_table, user_entity):
 
 
 @pytest.fixture
+def aggregate_asat_feature_test_case(client, scd_table, status_entity):
+    """
+    Aggregate asat feature test case
+    """
+    scd_view = scd_table.get_view()
+    feature_name = make_unique("Current Number of Users With This Status")
+    feature = scd_view.groupby("User Status").aggregate_asat(
+        value_column=None, method="count", feature_name=feature_name
+    )
+    feature_list = fb.FeatureList([feature], name=feature_name)
+    feature_list.save()
+    deployment = feature_list.deploy(make_production_ready=True)
+    deployment.enable()
+    deployment_sql = get_deployment_sql(client, deployment)
+    return DeploymentSqlTestCase(
+        feature_list=feature_list,
+        deployment_sql=deployment_sql,
+        point_in_time="2001-01-15 10:00:00",
+        expected_column_names=["POINT_IN_TIME", status_entity.serving_names[0], feature_name],
+    )
+
+
+@pytest.fixture
 def time_since_last_event_feature_test_case(client, event_table, user_entity):
     """
     Time since last event feature
@@ -315,6 +338,7 @@ async def check_deployment_sql(session, test_case):
         "time_series_table_feature_test_case",
         "snapshots_lookup_feature_test_case",
         "scd_lookup_feature_test_case",
+        "aggregate_asat_feature_test_case",
         "time_since_last_event_feature_test_case",
         "user_feature_served_via_transaction_test_case",
         "internal_parent_child_relationship_feature_test_case",
