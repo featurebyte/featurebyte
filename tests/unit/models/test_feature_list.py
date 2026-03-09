b7795d85603bfc51ed19f9dd0f62d97d541aa44e
Gxav <gxav@featurebyte.com>
2026-03-09 13:51:45 +0800

    update test

--- tests/unit/models/test_feature_list.py
+++ tests/unit/models/test_feature_list.py
@@ -71,7 +71,6 @@ def feature_list_namespace_model_dict_fixture():
         "description": None,
         "is_deleted": False,
         "context_id": None,
-        "naive_prediction": None,
     }
 
 
