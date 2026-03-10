6c9d1fe676e9b4900dbe82b0359a3bc6e0c77800
Yung Siang Liau <yungsiang@featurebyte.com>
2026-03-10 22:50:35 +0800

    Fix lint

--- featurebyte/api/source_table.py
+++ featurebyte/api/source_table.py
@@ -921,11 +921,6 @@ class SourceTable(AbstractTableData):
         TimeSeriesTable
             TimeSeriesTable created from the source table.
 
-        Raises
-        ------
-        ValueError
-            If neither or both of series_id_column and series_id_columns are specified.
-
         Examples
         --------
         Create an time series table from a source table.
@@ -1361,11 +1356,6 @@ class SourceTable(AbstractTableData):
         Returns
         -------
         TimeSeriesTable
-
-        Raises
-        ------
-        ValueError
-            If neither or both of series_id_column and series_id_columns are specified.
         """
 
         from featurebyte.api.time_series_table import TimeSeriesTable
